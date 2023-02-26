3b0f30050bbf876166161e160c6b41c3a0db7ef2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-02-26 08:45:04 +0600

    Update C_Ch1_1_10_escape_seq.c

--- CPP_ch_1_intro/C_Ch1_1_10_escape_seq.c
+++ CPP_ch_1_intro/C_Ch1_1_10_escape_seq.c
@@ -16,3 +16,11 @@ These characters can not be entered from keyboard
     \?	    Question  mark
     \N	    Octal constant (N is octal value)
     \xN	    hexdecimal constant (N is hexadecimal value) 
+
+// Example 1: Newline and tab    
+#include <stdio.h>
+int main(void){
+  printf("\n\tThis is a test.");
+  printf("\n\tThis is a test.");
+  return 0;
+}
