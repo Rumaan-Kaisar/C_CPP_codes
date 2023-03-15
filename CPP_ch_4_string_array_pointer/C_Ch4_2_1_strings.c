4517b92b19cbb5fb297af5e1688309633cbb5889
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-15 14:06:28 +0600

    Update C_Ch4_2_1_strings.c

---
+++ CPP_ch_4_string_array_pointer/C_Ch4_2_1_strings.c
@@ -0,0 +1,285 @@
+
+/* 
+    -------   STRINGS as 1D array   -------
+    The most common use of the one-dimensional array in C is the string.
+
+    C has no built-in string data type. Instead, a string is defined as a null-terminated character array.
+        In C, a null is zero.
+        i.e. our array will be one byte larger than the largest string it will be required to hold, to make room for the null.
+        A string constant is null-terminated by the compiler automatically. 
+
+
+    gets(): 
+        To read a string from the keyboard uses the STDIO.H header file.
+        Call it using the name of a character array without any index.
+        Reads characters until press ENTER (carriage return). The ENTER key is not stored, but is replaced by a null, which terminates the string.
+        gets() has no bounds checking, so "array overrun/program-crash" is normal.
+
+        Output string directly using printf():
+                                                gets (str); 
+                                                printf(str); 
+            With format specifier: Display str using the %s format code. For example, to output a newline after str : 	  
+                printf("%s\n", str); 	
+
+
+    
+    4-string functions:
+        strcpy(), 
+        strcat(), 
+        strcmp(),  
+        strlen(),
+        These functions require the header file <STRING.H>. 
+
+    strcpy():
+        General form: 		strcpy(to, from);
+        It copies the contents of "from" to "to".
+            char str[80];
+            strcpy (str, "hello" ); 
+            printf("%s", str);
+
+        No bounds checking.
+
+
+    strcat():
+        General form is :  		strcat(to, from);
+        For Concatenation. It adds the contents of "from" to the contents of "to". 
+        This fragment displays "hello there".
+            char str[80];
+            strcpy (str, "hello" ) ; 
+            strcat (str, "there" ) ; 
+            printf("%s", str);
+
+        No bounds checking.
+
+
+    strcmp():
+        General form:       strcmp(s1, s2); 
+        Compares two strings.
+
+        returns 'zero' if the strings are the same.
+        'less than zero' if s1 is less than s2 and 
+        'greater than zero' if s1 is greater than s2.
+
+        The strings are compared lexicographically; that is, in dictionary order.
+            One string is less than other if it appear before the other in a dictionary.
+            The comparison is not based upon the length of the string.
+            comparison is case-sensitive, lowercase characters being greater than uppercase. 
+
+        This fragment prints 0, because the strings are the same:
+            printf("%d", strcmp("one", "one"));
+
+
+    strlen(): 
+        General form is:  strlen(str);
+        Returns the length, in characters, of a string. 
+        Does not count the null terminator.
+
+        strlen("test"), will return 4.
+
+
+    atoi():
+        converts string of numbers to actal int.
+        Returns the integer equivalent of the number represented by its string argument. 
+        For example, atoi("100") returns the value 100.
+        scanf() is incompatible with gets().
+        atoi()  uses the header file <STDLIB.H>.
+
+
+    Null string:
+        A zero-length string can be created using a strcpy():
+                strcpy(str, "");
+        It contains only one element: the null terminator.
+
+
+    STRING.H & STDLIB.H:
+        atoi()  uses the header file <STDLIB.H>.
+        strcpy(), strcat(), strcmp(), strlen(), require the header file <STRING.H>. 
+*/
+
+
+
+
+
+/* Example 1: For example, this program reads a string entered at the keyboard. It then displays the contents of that string one character at a time.
+                Notice how the program uses the fact that a null (means str[i] is zero/null or false ) is false
+                to control the loop that outputs the string (i.e. until the string reach to its end).*/
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char str[80];
+    int i;
+
+    printf("Enter a string (less than 80 chars): ");
+    gets(str); 	//Reads the string from the keyboard and store at "str" array
+
+    for(i=0; str[i]; i++) printf("%c", str[i]); //when string ends str[i] will be false
+    printf("\n");
+
+    // There is, of course, a much easier way to display a string using printf()
+    printf(str); // Without any specifire
+
+    // With format specifier
+    printf("%s\n", str);
+
+    return 0;
+}
+
+    
+
+
+/* Example 2: This program requests input of two strings, then demonstrates
+the four string functions with them */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char str1[80], str2[80];
+    int i;
+    
+    printf("Enter the first string:");
+    gets(str1);
+    printf("Enter the second string:");
+    gets(str2);
+    
+    //see how long the string are
+    printf("%s is %d chars long\n", str1, strlen(str1));
+    printf("%s is %d chars long\n", str2, strlen(str2));
+    
+    //compare the strings
+    i = strcmp(str1,str2);
+    if(!i) printf("The strings are equal.\n");
+    else if( i < 0 ) printf("%s is less than %s\n", str1, str2);
+    else  printf("%s is less than %s\n", str2, str1);
+    
+    //concatenate str2 to end of str1 if there is enough room
+    if(strlen(str1) + strlen(str2) < 80){
+        strcat(str1, str2);
+        printf("%s\n",str1);
+    }
+    
+    //copy str2 to 1
+    strcpy(str1,str2);
+    printf("%s %s\n", str1, str2);
+
+    return 0;
+}
+
+
+// #include<stdio.h>
+// void printstr(char *p);
+// int main() {
+//     char str[20] ="Hello";
+//     printstr(str);
+
+//     return 0;
+// }
+
+// void printstr(char *p){
+//     p[0] = 'A';
+//     printf("%c",p[0]);
+// }
+
+
+
+/* Example 3: The following program allows the user to add, subtract,
+multiply, or divide, but does not use a menu. Instead, it uses a command-based interface. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char command[80], temp[80];
+    int i, j;
+    
+    for( ; ; ){
+        printf("Operation? ");
+        gets(command);
+        
+        //see if user wants to stop
+        if(!strcmp(command,"quit")) break;
+        
+        printf("Enter the first number:");
+        gets(temp);
+        i = atoi(temp);
+        
+        printf("Enter the second number:");
+        gets(temp);
+        j = atoi(temp);
+        
+        //now perform the opetation
+        if(!strcmp(command, "add")) printf("%d\n",i+j);
+        else if(!strcmp(command, "subtract")) printf("%d\n", i-j);
+        else if(!strcmp(command, "divide")) printf("%d\n", i/j);
+        else if(!strcmp(command, "multiply")) printf("%d\n", i*j);
+        else printf("Unknown command\n");
+    }
+    
+    return 0;
+}
+
+
+
+/* Example 4: Write a program that inputs a string, then displays it backward on the screen. */
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+    char str[80];
+    int i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    // int i= strlen(str);
+    for(i = strlen(str)-1; i >= 0; i--) printf("%c", str[i]);
+
+    return 0;
+}
+
+
+
+/* Example 5: Following program gives err because of string overrun. */
+#include <string.h>
+#include <stdio.h>
+int main(void){
+    char str[5];
+
+    strcpy(str, "this is a test");   //bigger than the available room
+    printf (str);
+
+    return 0:
+}
+
+
+
+/* Example 6: Following repeatedly inputs strings. Each time a string is input, concatenate it with a second string called 'bigstr'.
+                Add newlines to the end of each string. If the user types 'quit', stop inputting and display bigstr (which will contain a record of
+                all strings input). Also stop if bigstr will be overrun by the next concatenation */
+
+#include<stdio.h>
+#include<string.h>
+
+int main(void) {
+    char bigstr[1000]="", str[50];
+
+    for( ; ; ) {
+        printf("Enter a string: ");
+        gets(str);
+
+        if(!strcmp(str, "quit")) break;
+
+        strcat(bigstr, "\n");
+
+        // prevent an array overrun
+        if(strlen(bigstr) + strlen(str) >= 1000) break;
+        strcat(bigstr, str);
+    }
+
+    printf(bigstr);
+
+    return 0;
+}
+
+
