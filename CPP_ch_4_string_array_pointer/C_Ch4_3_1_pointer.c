5d78c429b9246a2366525dd019d449d7dd526331
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-16 10:25:53 +0600

    Update C_Ch4_3_1_pointer.c

---
+++ CPP_ch_4_string_array_pointer/C_Ch4_3_1_pointer.c
@@ -0,0 +1,9 @@
+
+chapter covers one of C's most important and sometimes
+most troublesome features: the pointer. A pointer is basically
+the address of an object. One reason that pointers are so
+important is that much of the power of the C language is
+derived from the unique way in which they are implemented.
+You will learn about the special pointer operators, pointer arithmetic,
+and how arrays and pointers are related. Also, you will be introduced
+to using pointers as parameters to functions.
