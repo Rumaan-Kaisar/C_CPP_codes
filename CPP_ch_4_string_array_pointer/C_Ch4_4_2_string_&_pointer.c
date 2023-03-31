47aa7bbcfd91e2c496d7ba0bc42c98a80966b135
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-31 11:10:47 +0600

    Update C_Ch4_4_2_string_&_pointer.c

---
+++ CPP_ch_4_string_array_pointer/C_Ch4_4_2_string_&_pointer.c
@@ -0,0 +1,103 @@
+
+/* 
+    ---------|    Use pointers to string constants    |---------
+
+    C allows string constants enclosed between double quotes "" to be used in a program.
+        When the compiler encounters such a string, it stores it in the program's string table and generates a pointer to the string.
+
+        char *p;
+        p = "one two three";	 
+        printf(p);
+
+        'char *p;' means that it may 'point' to an "array of characters". When the compiler compiles the line p = "one two three";  
+            it stores the string in the program's string table and assigns to p the address of the string in the table.
+
+        Above code can be written more efficiently:
+            char *P = "one two three";
+            printf(p) ;
+            
+            Here , p is initialized to point to the string.
+
+
+
+    Note: Using "pointers" to string constants can be very helpful when those "constants are quite long". 
+            For example suppose that you had 'a program 'that at various times would prompt the user to insert a diskette into drive A. 
+                Using a pointer has following advantages
+
+        Following uses a 'initialized pointer to the string' and then simply use the pointer when the message needed to be displayed
+            Another advantage to this approach is that to change the prompt, 
+            you only need to change it once, and all references to it will reflect the change.
+
+                char *InsDisk = "Insert disk into drive A then press ENTER ";
+                . . . .
+                printf(InsDisk);
+                . . . .
+                printf(InsDisk);
+*/
+
+
+
+
+// Example 1: folloWing program prints 'one two three' on the screen.
+#include <stdio.h>
+int main(void){
+    char *p;
+    p = "one two three";
+    printf(p);
+
+    return 0;
+}
+
+// Above code can be written more efficiently
+#include <stdio.h>
+int main(void){
+    char *p = "one two three";
+    printf(p);
+
+    return 0;
+}
+
+
+
+
+// Example 2: Following program continues to read strings until you enter "stop":
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char *p="stop";
+    char str[80];
+    
+    do{
+        printf("Enter a string:");
+        gets(str);
+    } while(strcmp(p, str));
+    
+    return 0;
+}
+
+
+
+
+/* Eaxmple 3: Write a program that creates 3 character pointers and initialize them so that 
+                one points to the string "one", the second to the string "two", and the third to the string "three".
+                Next, have the program print all six permutations of these three strings,
+                (For exatnple, one permutation is "one two three', another is "two one three".) */
+#include <stdio.h>
+
+int main (void){
+    char *one = "one";
+    char *two = "two"; 
+    char *three = "three" ;
+    
+    printf("%s %s %s\n", one, two, three);
+    printf("%s %s %s\n", one, three, two);
+    printf("%s %s %s\n", two, three, one);
+    printf("%s %s %s\n", two, one, three);
+    printf("%s %s %s\n", three, one, two);
+    printf("%s %s %s\n", three, two, one);
+
+    return 0;
+}
+
+
