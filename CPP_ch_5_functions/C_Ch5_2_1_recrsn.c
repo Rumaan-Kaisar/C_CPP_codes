c4b1d0da91fd78b02a0ff8a08fde8f16050f07d3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-13 13:23:29 +0600

    Update C_Ch5_2_1_recrsn.c

--- CPP_ch_5_functions/C_Ch5_2_1_recrsn.c
+++ CPP_ch_5_functions/C_Ch5_2_1_recrsn.c
@@ -1,64 +1,101 @@
 
-5.2 Recursion
-Recursion is the process by which something is defined in terms of itself. When applied to computer languages, recursion means that a function can call itself. Not all computer languages support recursive functions, but C does. 
 
-A very simple example of recursion is shown in this program:
+
+/* 
+    ---------|    Recursion    |---------
+    recursion means that a function can call itself.
+
+    Usage of recursion:  Recursion can be quite useful in simplifying certain algorithms. For example,
+    Quick-sort sorting algorithm is difficult to implement without the use of recursion.
+
+    ---------|    Recursion and control statement    |---------
+        Recursion is essentially a new type of program control mechanism (i.e. it can be used as a control statement). 
+
+        Always use "CONDITIONS" with recursion:
+            Every recursive function you write must have a "conditional statement" that controls whether the function will call itself again or return. 
+            Without such a (conditional ) statement, a recursive function will simply run wild and crashes the program.
+*/
+
+
+
+
+/* Example 1: A very simple example of recursion is shown in this program: */
 #include <stdio.h>
 void recurse(int i);
-int main(void){ recurse(0) ;    return 0;}
+
+int main(void){ 
+    recurse(0) ;    
+    
+    return 0;
+}
 
 void recurse(int i){
-    if(i<10){          	/* Condition to control recursion */
-        recurse(i+1);      /* recursive call */
+    if(i<10){          	// Condition to control recursion
+        recurse(i+1);      // recursive call
         printf(" %d ", i);
         }
 }
-This program prints 9 8 7 6 5 4 3 2 1 0  on the screen. 
-Let's see why. 
-	The recurse() function is first called with 0. This is recurse()'s first activation. Since 0 < 10, recurse() ,then calls itself with the value of (i = 0) + 1, 
-	This is the second activation of recurse() and i = 1. Since 1<10 , this causes recurse() to be called again using the value (i=1) + 1. 
-	This process repeats until recurse() is called with the value 10. Since 10≮10 This causes recurse() to return (and stops recursion). Since it returns to the point of its call, it will execute the printf ) statement in its previous activation (i.e. 9th  activation ), print 9, and return. 
-	This, then, returns to the point of its call in the previous activation (8th activation), which causes print 8. 
-	The process continues until all the calls return, and the program terminates. We can assemble this in following stages :
-The recursion starts at i=0, from call recurse(0); The fuction is then activated
-	Stage 0 : condition check : i=0<10. Call recurse() with (i=0)+1 i.e. recurse(1) is called.
-	Stage 1 : condition check : i=1<10. Call recurse() with (i=1)+1 i.e. recurse(2) is called. 
-	Stage 3 : condition check : i=2<10. Call recurse() with (i=2)+1 i.e. recurse(3) is called.
-. . . . . . . . . . . . . . . . . . . . . . . . . .
-. . . . . . . . . . . . . . . . . . . . . . . . . .
-	Stage 9 : condition check : i=9<10. Call recurse() with (i=9)+1 i.e. recurse(10) is called.
-	Stage 10 : condition check : i=10<10 is false and condition fails.  recurse() stopped. Function returns to stage 9.
 
-After returning to stage 9 ,the next statement after the function calls executed i.e. prints i=9 using printf(). The stage 9 is now complete. It then returns to stage 8 and similarly prints i=8. This process repeats until stage 0 is complete. The function then returns  to main() and reach to the end of the program.
+// Above program prints 9 8 7 6 5 4 3 2 1 0  on the screen. 
 
-            i=0<10               i=1<10                   i=2<10               i=3<10                                                                          i=7<10                   i=8<10               i=9<10
-[0]                        [1]                        [2]                        [3]                        [4]          . . . . . .  . . . . .                    [7]                        [8]                        [9]                        [10]
+// if printf() appears 'before'  the "RECURSIVE-CALL-POINT" it could display : 0 1 2 . . . 9. As given below
+/*
+void recurse(int i){
+    if(i<10){          	    // Condition to control recursion-point
+        printf(" %d ", i);  // printf() appears before the recursive-call-point
+        recurse(i+1);       // recursive call-point
+        } 
+} 
+*/
 
-                       return                 return                    return                        return                    return                   return                    return                      return                      return
 
-printf()              printf()              printf()              printf()              printf()    . . . . . .  . . . . .                    printf()              printf()               printf()              printf()
-i = 0                     i = 1                    i = 2                     i = 3                     i = 4         . . . . . .  . . . . .                    i = 7                    i = 8                     i = 9                    
 
-	We need to remember one point that, the value of i in each stage is stored before the recursive call. And the numbers displayed 9 8 7 6 5 …. 0.
-	The reason for that is : the printf() is appeared after the recursive-call-point, in this case all recursive calls need to be done first and then proceed to printf() for each corresponding stage.
-	However, if printf() appears before  the recursive-call-point it could display : 0 1 2 3 …. 9. As given below:
+/* 
+    ---------|    Call-Stack    |---------
+    It is called "The Call-Stack" the execution mechanism of a recursive function. Let's see why. 
+        The recurse() function is first called with 0. This is recurse()'s first activation. Since 0 < 10, recurse() ,then calls itself with the value of (i = 0) + 1, 
+        This is the second activation of recurse() and i = 1. Since 1<10 , this causes recurse() to be called again using the value (i=1) + 1. 
+        This process repeats until recurse() is called with the value 10.
+        
+        Since 10 < 10 is false, this causes recurse() to return (and stops recursion). 
+        
+        Since it returns to the point of its call, it will execute the printf() statement in its previous activation (i.e. 9th  activation ), print 9, and return. 
+        This, then, returns to the point of its call in the previous activation (8th activation), which causes print 8. 
+        The process continues until all the calls return, and the program terminates.
+
+    Note
+        Note that there are not multiple copies of a recursive function. Instead, only one copy exists. 
+        When a function is called, storage for its parameters and local data are allocated on the stack. 
+        Thus, when a function is called recursively, the function begins executing with a new set of parameters and local variables, 
+            but the code that constitutes the function remains the same.
+
+    Stack : A stack is a data structure which is used to store data in a particular way. Two operations can be performed on a stack : 
+        push operation which inserts an element into the stack. 
+        Pop operation which removes the last element that was added into the stack. 
+        It follows last in first out (LIFO) order
+
+
+    ---------|    Before and after RECURSIVE statement    |---------
+
+        We need to remember one point that, the "value of i" in each stage is 'stored before the recursive call'. 
+
+        The appearance of printf() before and after the "RECURSIVE-CALL-POINT"
+            The numbers displayed 9 8 7  . . . 0. because the printf() is appeared 'after' the "RECURSIVE-CALL-POINT", 
+                in this case 'all recursive calls need to be done first' and then proceed to printf() for each corresponding stage.
+            
+            However, if printf() appears 'before'  the "RECURSIVE-CALL-POINT" it could display : 0 1 2 . . . 9. As given below:
+*/
 
 void recurse(int i){
-    if(i<10){          	/* Condition to control recursion */
- printf(" %d ", i); /* printf() appears before the recursive-call-point */
-        recurse(i+1);      /* recursive call */
-        
+    if(i<10){          	    // Condition to control recursion-point
+        printf(" %d ", i);  // printf() appears before the recursive-call-point
+        recurse(i+1);       // recursive call-point
         } 
 }
 
-Recursion and control statement : Recursion is essentially a new type of program control mechanism (i.e. it can be used as a control statement). This is why every recursive function you write will have a conditional statement that controls whether the function will call itself again or return. Without such a (conditional ) statement, a recursive function will simply run wild, using up all the memory allocated to the stack and then crashing the program.
-
-Usage of recursion :  Recursion can be quite useful in simplifying certain algorithms. For example, the Quick-sort sorting algorithm is difficult to implement without the use of recursion.  If you are new to programming in general, you might find yourself uncomfortable with recursion. 
 
 
-Note
-	It is important to understand that there are not multiple copies of a recursive function. Instead, only one copy exists. When a function is called, storage for its parameters and local data are allocated on the stack. Thus, when a function is called recursively, the function begins executing with a new set of parameters and local variables, but the code that constitutes the function remains the same.
-	Stack : A stack is a data structure which is used to store data in a particular way. Two operations can be performed on a stack : push operation which inserts an element into the stack. Pop operation which removes the last element that was added into the stack. It follows last in first out (LIFO) order
+
 
 
 
