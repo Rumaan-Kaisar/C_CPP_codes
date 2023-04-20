e0a44db68cb3e277f6d06bbb1de8fa57801acfa9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-20 14:10:30 +0600

    Update C_Ch5_3_3_old_styl_param.c

--- CPP_ch_5_functions/C_Ch5_3_3_old_styl_param.c
+++ CPP_ch_5_functions/C_Ch5_3_3_old_styl_param.c
@@ -1,71 +1,115 @@
-5.5 Old-Style Parameter Declarations
-The original parameter declaration method is now called the old-style or classic form. The general form of the old-style parameter declaration is shown here:
-type function-name(parameter_1, parameter_2,. . . . . ., parameter_N)
-type parameter_1;
-type parameter_2;
-. . . .
-. . . .
-type parameter_N;
-{
-FUNCTION-CODE
-}
-Notice that the declaration is divided into two parts: 
-	Within the parentheses, only the names of the parameters are specified. 
-	Outside the parentheses, the types and names are specified. 
 
-For example, given the following modern declaration and then old-style
+
+
+/*  ---------------|    Old-Style Parameter Declarations    |---------------
+    The general form of the old-style parameter declaration is shown here:
+
+        type function-name(parameter_1, parameter_2,. . . . . ., parameter_N)
+        type parameter_1;
+        type parameter_2;
+            . . . .
+            . . . .
+        type parameter_N;
+        {
+            FUNCTION-CODE
+        }
+
+
+    Notice that the declaration is divided into two parts: 
+        Within the parentheses, only the 'names of the parameters' are specified. 
+        Outside the parentheses, the "types" and "names" are specified.  
+*/
+
+
+
+
+/* Example 1: For example, following shows modern declaration and then old-style */
+// ---------------|    Modern Style    |---------------
 float f(char ch, long size, double max){ . . function code . . }
-the equivalent old-style declaration is
-float f(ch, size, max)
-char ch;
-long size;
-double max;
-{ . . function code . . }
-Note
-	One advantage of the old-style declaration is that you can specify more than one parameter after the type name (sometimes you find it quite useful). For example, this is perfectly valid:
-myfunc (i. j. k)
+
+
+// ---------------|    OLD-Style    |---------------
+    // The equivalent old-style declaration is
+        float f(ch, size, max)
+        char ch;
+        long size;
+        double max;
+        { 
+            . . function code . . 
+        }
+
+
+
+
+/* Example 2: One advantage of the old-style declaration is that you can specify more than one parameter after the type name.
+                For example, this is perfectly valid: */
+myfunc (i, j, k)
 int i, j, k;
 { . . function code . . }
-	The ANSI C standard specifies that either the old-style or the modern declaration form may be used. The reason for this is to maintain compatibility with older C programs. 
 
 
 
 
+/* Example 3: Following uses old style declaration  */
 #include<stdio.h>
 
-int area(int l, int w)
-{
-    return l*w;
-}
+int area(int l, int w);
 
-int main ()
+int main(void)
 {
     printf("Area is %d",area(10,20));
     return 0;
 }
 
+int area(int l, int w)
+int l, w;
+{
+    return l*w;
+}
 
 
 
+
+/* Example 4: Convert this program so that f_to_m() uses the old-style declaration form. */
+#include <stdio.h>
+
+double f_to_m(double f);
+
+int main(void){
+    double feet;
+
+    printf ("Enter feet: ");
+    scanf("%lf", &feet);
+
+    printf("Meters: %f", f_to_m(feet));
+
+    return 0;
+}
+
+double f_to_m(double f){
+    return f / 3.28;
+}
+
+
+/* old styled */
 #include<stdio.h>
 double ftom(double f);
 
-int main ()
-{
+int main (void){
     double feet;
+
     printf("Enter feet:");
-    scanf("%lf",&feet);
-    printf("Meters: %lf",ftom(feet));
+    scanf("%lf", &feet);
+    printf("Meters: %lf", ftom(feet));
     
     return 0;
 }
 
+// old-style declaration
 double ftom(f)
 double f;
 {
     return f/3.28;
 }
-
-
 
 
