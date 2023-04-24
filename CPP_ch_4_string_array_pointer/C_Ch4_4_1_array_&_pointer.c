1a262fc1ef116d3decada969b1a60113f94c0407
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-24 17:43:14 +0600

    Update C_Ch4_4_1_array_&_pointer.c

--- CPP_ch_4_string_array_pointer/C_Ch4_4_1_array_&_pointer.c
+++ CPP_ch_4_string_array_pointer/C_Ch4_4_1_array_&_pointer.c
@@ -309,3 +309,34 @@ int main(void){
 The fragment is correct. It displays c.
 	
 	
+/* Example 12: Write a function called avg() that averages a list of floating-point values. 
+                The function will have two arguments. 
+                The first is a pointer to the array containing the numbers; 
+                the second is an integer value, which specifies the size of the array. 
+                Demonstrate its use in a program. */
+#include <stdio.h>
+
+void avg(double *d, int num);
+
+int main(void){
+    double nums[] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
+    avg(nums, 10);
+    
+    return 0;
+}
+
+
+void avg(double *d, int num){
+    double sum;
+    int temp;
+
+    temp = num-1;
+    for(sum=0; temp>=0; temp--) sum = sum + d[temp];
+
+    printf("Average is %f", sum/(double)num);
+}
+
+
+
+
+
