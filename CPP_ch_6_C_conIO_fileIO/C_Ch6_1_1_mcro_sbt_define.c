dc90adaf471e32d35a359e4adb5d79890424fc58
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-04 10:34:08 +0600

    Update C_Ch6_1_1_mcro_sbt_define.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_1_1_mcro_sbt_define.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_1_1_mcro_sbt_define.c
@@ -70,6 +70,7 @@ void f(void){
 
 
 
+
 /* Example 3: Since a macro substitution is simply a text replacement, you can use a macro name in place of a quoted string. */
 #include <stdio.h>
 
@@ -80,3 +81,77 @@ int main(void){
 
 	return 0; 
 }
+
+
+
+
+/* Example 4: Once a macro name has been defined, it can be used to help
+				DEFINE another macro name. For example, consider this program: */
+#include <stdio.h>
+
+#define SMALL 1
+#define MEDIUM SMALL+1
+#define LARGE MEDIUM+1
+
+int main(void){
+	printf("%d %d %d", SMALL, MEDIUM, LARGE);
+
+	return 0;
+}
+
+// It prints} 2 3 on the screen.
+
+
+
+
+/* Example 5:  If a MACRO NAME appears "inside a quoted string", no substitution
+				will take place. For example, given this definition */
+#define ERROR "catastrophic error occurred"
+// The following statement will not be affected.
+printf ("ERROR: Try again");	// Here 'ERROR' is only a string, not a macro name
+
+
+
+
+/* Example 6: Create a program that defines two macro names. MAX and COUNTBY. 
+				Have the program count from zero to MAX-1 by whatever value COUNTBY is defined as. 
+				(Give COUNTBY the value 3 for demonstration purposes.) */
+#include <stdio.h>
+#define MAX 100
+#define COUNTBY 3
+
+int main(void){
+	int i;
+
+	for(i=0; i<MAX; i++) if(!(i%COUNTBY)) printf("%d ", i);
+
+	return 0;
+}
+
+
+
+/* Example 7: Following fragment is wrong because a macro cannot be defined in
+				terms of another before the second macro is defined. 
+				Stated differently, MIN is not defined when MAX is being defined. */
+#define MAX MIN+100
+#define MIN 10
+
+
+
+
+/* Example 8: As the macro is used, the following fragment is wrong. The string needs to be within double quotes. */
+#define STR this is a test
+printf(STR) ;
+
+
+
+
+/* Example 9: Give a HEADER file a MACRO nmae !! Following program is correct */
+#define STDIO <stdio.h>
+#include STDIO
+
+int main(void){
+	printf("This is a test.");
+	return 0;
+}
+
