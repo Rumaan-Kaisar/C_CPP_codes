34e226368debc73d0b0745aff4bed9d352bacdf7
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-15 15:38:28 +0600

    Update intro.c

--- CPP_ch_6_C_conIO_fileIO/intro.c
+++ CPP_ch_6_C_conIO_fileIO/intro.c
@@ -1,6 +1,6 @@
-N this chapter you will learn about.C's console I/O functions.
-These are the functions that read or write information to and fro
-the console. You have already been using some ·ofthese functions.
-Here we will look at them in detail. This chapter begins with a
-short but necessary digression that introduces another of C's
-preprocessor directives: #define.
+IN this chapter you will learn about.C's console I/O functions and also File I/O functions.
+These are the functions that read or write information to and from the console/file. 
+
+You have already been using some of these functions.
+  Here we will look at them in detail. 
+ 
