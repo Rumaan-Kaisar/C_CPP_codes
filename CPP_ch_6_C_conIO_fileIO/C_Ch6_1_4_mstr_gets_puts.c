3339d83eda458d7b0d9ec798dadbd9d4b9add9ab
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-18 11:40:57 +0600

    Update C_Ch6_1_4_mstr_gets_puts.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_1_4_mstr_gets_puts.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_1_4_mstr_gets_puts.c
@@ -149,3 +149,8 @@ But, it can only output strings.
 
    
     
+    
+/* Example 7: What is EOF, and where is it defined? */
+EOF is a macro that stands for "end-of-file". It is defined in STOIO.H.
+    
+    
