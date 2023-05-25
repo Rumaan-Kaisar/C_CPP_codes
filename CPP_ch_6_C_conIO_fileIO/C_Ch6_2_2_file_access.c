c25fb05be53dfbe00bd707daec2b1f664e2504e2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-25 09:20:57 +0600

    Update C_Ch6_2_2_file_access.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_2_file_access.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_2_file_access.c
@@ -1,78 +1,157 @@
 
-----------------------    File Access & Modify    ----------------------
 
-            File access: 
-                using fopen(), fclose() and 
+/* 
+    ----------------------    File Access & Modify    ----------------------
 
-            File read/write:
-                using fgetc(), fputc() 
+                File access: 
+                    using fopen(), fclose() and 
 
-
-fopen():
-    Opening a file: To open a file and associate it with a stream, use fopen() it uses  <STDIO.H>. Its prototype:
-
-            FILE *fopen(char *fname, char *mode); 
-
-                Here "FILE" is a Structure (data-type)
-
-    Generally we use the following form 
-
-            FILE *file_pointer;
-            file_pointer = fopen("file_name", "mode");
+                File read/write:
+                    using fgetc(), fputc() 
 
 
-IN the prototype the name of the file to open is pointed to by "fname". It must be a valid file name, as defined by the operating system. The string pointed to by mode determines how the file may be accessed. ANSI C standard values for mode are shown in Table. 
-	The type FILE is defined in STDIO.H. It is a structure that holds various kinds of information about the file, such as its size, the current location of the file, and its access modes. It essentially identifies the file. (A structure is a group of variables accessed under one name.)
 
-	If the open operation is successful, fopen() returns a valid file pointer.  The fopen() function returns a pointer to the structure associated with the file by the open process. This pointer will be used with all other functions that operate on the file. It can't be altered or the object it points to.
+    ----------------------    OPEN a File    ----------------------
+    fopen():
+        Opening a file: To open a file and associate it with a stream, use fopen() it uses  <STDIO.H>. Its prototype:
 
-	If the fopen( ) function fails, it returns a null pointer. The header STDlO.H defines the macro NULL, which is defined to be a null pointer. It is very important to ensure that a valid file pointer has been returned. To do so, check the value returned by fopen( ) (using condition) to make sure that it is not NULL. for example, the proper way to open a file called my file for text input :
+                FILE *fopen(char *fname, char *mode); 
 
-FILE *fp;
-if( (fp = fopen("myfile", "r")) == NULL){ 
-printf(“Error opening file.\n");
-exit(1); /* or substitute your own error handler */ }
+                    Here "FILE" is a Structure (data-type). It is defined in STDIO.H
+                        It holds various information about the file, such as size, current location, access modes. 
+                        It essentially identifies the file. (A structure is a group of variables accessed under one name.)
 
-what actually happening here(CONFUSED !!!) : Here not only the "file error checking " happening inside the condition of "if" statement but also file is opened (if it exists) simultaneously. It is actually equivalent to
+        Generally we use the following form 
 
-fp=fopen("myfile", "r"); /*file opening */
-if(fp===NULL){ /*file error checking*/
-printf("file error\n");
-exit(1); /* less effect of exit() : file error occurs before the conditional statement*/}
+                FILE *file_pointer;
+                file_pointer = fopen("file_name", "mode");
 
-There is no need for a separate comparison step because the assignment and the comparison can be performed at the same time within the if .
+        IN the prototype, the name of the file to open is pointed to by "fname". It must be a valid file name, as defined by the operating system. 
+        The string pointed to by "mode" determines how the file may be accessed. 
 
-	In this case there is no point of using exit(), after the file error occurred.
-	To prevent error of file opening if the file doesn't exist, the fopen() should occur inside the  "if" statement. The whole program will exit (shuts down)  if the file doesn't exist using exit(1) against the "NULL" condition. And no error occurs during file closing (fclose()) or any crash. 
-	Inside the "if" condition the file is opened simultaneously during the "file error checking". But once the file is opened it never close until fclose appeared to corresponding file pointer.
-	The point of using exit() is that when the proper condition occurs for exit(1) the program will shuts down immediately by returning 0 to the operating system. Her the point of using exit() is that : fclose() creates system error if its pointer argument is invalid on a null-pointer. Hence when a null-pointer occurs during file operation the program will shuts down.   
 
-Mode	Meaning	Mode	Meaning
-"r"	Open a text file for reading	"r+"	Open a text file for read/write
-"w"	Create a text file for writing	"w+"	Create a text file for read/write
-"a"	Append(means : edit/add) to a text file	"a+"	Append /create a text file for read/write
-"rb"	Open a binary file for reading	"r+b" or "rb+"	Open a binary file for read/write. Also can use "rb+"
-"wb"	Create a binary file for writing	"w+b" or "wb+"	Create a binary file for read/write. Also can use "wb+"
-"ab"	Append to a binary file	"a+b" or "ab+"	Append/create a binary file for read/write. Also can use "ab+"
+        If the open operation is successful, fopen() returns a valid file pointer.
+            fopen() returns a pointer to the structure associated with the file by the open process. 
+            This pointer will be used with all other functions that operate on the file. 
 
-Although most of the file modes are self-explanatory, a few comments are in order :
-[1]	"r": when opening a file for read-only operations, the file does not exist, fopen() will fail and NULL-pointer will return.
-[2]	"a": When opening a file using append mode, if the file does not exist, it will be created. Further, when a file is opened for append all new data written to the file will be written to the end of the file. The original contents will remain unchanged. 
-[3]	"w": If, when a file is opened for writing, the file does not exist, it will be created. If it does exist, the contents of the original file will be destroyed and a new file created. 
-[4]	"r+": It is similar to "r", "r+" will not create a file if it does not exist. It can both read-write.
-[5]	"a+": It is similar to "a", moreover we can read with this mode. IT can both read-append. 
-[6]	"w+": similar to "w". It can both read-write. Seems like "r+" & "w+" are same , but there are few differences. 
-[7]	Difference between "r+" & "w+": The difference between modes "r+" and "w+" is that "r+" will not create a file if it does not exist; however, "w+" will. Further, if the file already exists, opening it with "w+" destroys its contents; opening it with "r+" does not.
-Note : For general purpose use only "r+" and "a+" for read-write-append but be careful using "w"/"w+"
+        If the fopen() function fails, it returns a null pointer.
 
-Opening a file : To close a file and disassociate it with a stream, use fclose() it uses  STDIO.H. Its prototype is shown here:
 
-int fclose(FILE *fp);
 
-The fclose() function closes the file associated with fp, which must be a valid file pointer previously obtained using fopen(), and disassociates the stream from the file. 
+    ----------------------    Always check the NULL pointer    ----------------------
+        It is very important to ensure that a valid file pointer has been returned. 
+        Use a condition to make sure that it is not NULL. 
+        
+    The proper way to open a file called "myfile" for text input :
 
-	Be carefull with using fclose() : You must never call fclose() with an invalid argument. Doing so will damage the file system and possibly cause irretrievable data loss. Invalid arguments means : you cannot use fclose() with null or empty file pointer or invalid-error causing file pointer (i.e. can't use null-pointer ). Never use fclose before fopen with corresponding file.
-	The fclose() function returns zero if successful. If an error occurs, EOF is returned.
+            FILE *fp;
+            if( (fp = fopen("myfile", "r") ) == NULL){ 
+                printf("Error opening file.\n");
+                exit(1); // or substitute your own error handler 
+            }
+
+    Here not only the "file error checking " happening inside the condition of "if" statement but also file is opened (if it exists) simultaneously. 
+    It is actually equivalent to
+
+            FILE *fp;
+            fp=fopen("myfile", "r"); // file opening
+            if(fp===NULL){      // file error checkin
+                printf("file error\n");
+                exit(1);        //  less effect of exit() : file error occurs before the conditional statement
+            }
+
+            There is no point of using exit(), after the file error occurred.
+            There is no need for a separate comparison step because the assignment and the comparison can be performed at the same time within the if .
+
+
+
+
+    ----------------------    Why use fopen() inside an if    ----------------------
+        In the later case there is no point of using exit(), after the file error occurred.
+        
+        To prevent error of file opening if the file doesn't exist, the fopen() should occur inside the  "if" statement. 
+            The program will stop if the file doesn't exist using exit(1) against the "NULL" condition. 
+            And no error occurs during file closing (fclose()) or any crash. 
+
+    Once the file is opened it never close until "fclose()" appeared to corresponding 'file pointer'.
+
+    The point of using exit() is that: fclose() creates system error if its pointer argument is invalid on a null-pointer.
+
+
+
+
+    ----------------------    CLOSING a File    ----------------------
+
+    Closing a file : 
+        To close a file and disassociate it with a stream, use fclose() it uses  STDIO.H. Its prototype is shown here:
+
+                int fclose(FILE *fp);
+
+        The fclose() function closes the file associated with "fp", which 
+            must be a "valid file pointer" previously obtained using fopen(), and disassociates the stream from the file. 
+            
+        The fclose() function returns zero if successful. If an error occurs, EOF is returned.
+
+
+        Be carefull with using fclose() : 
+            NEVER call fclose() with an invalid argument. Doing so will "DAMAGE THE FILE SYSTEM" and possibly cause irretrievable "DATA LOSS".
+            Invalid arguments means : you cannot use fclose() with "NULL OR EMPTY FILE POINTER" or invalid-error causing file pointer 
+            NEVER use fclose before fopen with corresponding file.
+
+
+
+
+
+    ----------------------    ANSI C standard values for mode are shown in Table.    ----------------------
+
+                "r"     Open a text file for 'reading'	
+                "r+"    Open a text file for 'read/write'
+                "w"     Create a text file for 'writing'	
+                "w+"	Create a text file for 'read/write'
+                "a"     Append(means : edit/add) to a text file	
+                "a+"	Append /create a text file for 'read/write'
+
+                "rb"	Open a binary file for reading	
+                "r+b" or "rb+"	Open a binary file for read/write. Also can use "rb+"
+
+                "wb"	Create a binary file for writing	
+                "w+b" or "wb+"	Create a binary file for read/write. Also can use "wb+"
+
+                "ab"	Append to a binary file	
+                "a+b" or "ab+"	Append/create a binary file for read/write. Also can use "ab+"
+
+
+    Although most of the file modes are self-explanatory, a few comments are in order :
+    [1]	"r": when opening a file for 'read-only' operations, the FILE DOES NOT EXIST, fopen() will "fail" and NULL-pointer will return.
+
+    [2]	"a": When opening a file using append mode, if the FILE DOES NOT EXIST, it will be "created". 
+                Further, append all new data written to the file, and will be written to the EOF. 
+                The original contents will remain unchanged. 
+            SAFE to use.
+
+    [3]	"w": If, when a file is opened for writing, the FILE DOES NOT EXIST, it will be created. 
+                If it does exist, the contents of the ORIGINAL FILE WILL BE DESTROYED and a new file created.
+            Unsafe.
+
+    [4]	"r+": It is similar to "r", "r+" will not create a file if it does not exist. 
+                It can both read-write.
+
+    [5]	"a+": It is similar to "a", moreover we can read with this mode. 
+                IT can both read-append. 
+
+    [6]	"w+": similar to "w". It can both read-write. Seems like "r+" & "w+" are same , but there are few differences. 
+                Difference between "r+" & "w+": "r+" will not create a file if it does not exist; however, "w+" will. 
+                Further, if the file already exists, opening it with "w+" destroys its contents (unsafe); opening it with "r+" does not (safe).
+
+
+    Note: 
+        For GENERAL PURPOSE use only "r+" and "a+" for 'read/write/append' but be careful using "w"/"w+".
+*/
+
+
+
+
+
 	Flushing the buffer : In order to improve efficiency, most file system implementations write data to disk one sector at a time. Therefore, data is buffered until a sector's worth of information has been output before the buffer is physically written to disk. When you call fclose(), it automatically writes any information remaining in a partially full buffer to disk. This is often referred to as flushing the buffer.
 
 Reading and Writing from/to a files : Once a file has been opened, depending upon its mode, you may read and/or write bytes (i.e., characters) using these two functions:
