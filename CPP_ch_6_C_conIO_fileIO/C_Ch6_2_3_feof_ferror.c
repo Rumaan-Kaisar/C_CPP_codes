aa60f02a3ad4a439a1f151d4c6ecd2324f188a2c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-01 11:23:30 +0600

    Update C_Ch6_2_3_feof_ferror.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_3_feof_ferror.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_3_feof_ferror.c
@@ -217,48 +217,51 @@ int main(int argc, char *argv[]){
 
 
 
-/* Example 3: Write a program that counts the number of bytes in a file (text or binary) 
+/* Example 3: Write a program that counts the number of bytes in a file (text or binary)
                 and displays the result. Have the user specify the file to count on the command line. */
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
-int main(int argc, char *argv[])
-{
+// #include <ctype.h>
+// #include <string.h>
+
+int main(int argc, char *argv[]){
     FILE *fp;
     unsigned count;
-    
-    /* see if file name is specified */
+
+    // see if file name is specified
     if(argc!=2){
         printf("File name missing.\n");
-        exit (1);
+        exit(1);
     }
-    
+
     // open  file
     if((fp = fopen(argv[1], "r"))==NULL) {
         printf("Cannot open file.\n");
         exit(1) ;
     }
-    
+
+
+    // count the bites
     count = 0;
-        
+
     while(!feof(fp)){
         fgetc(fp);
-        if(ferror(fp))
-        {
+        if(ferror(fp)){
             printf("File error");
             exit(1);
         }
         count++;
     }
-    
-    
+
+    // we minus 1 because of EOF is also counted
     printf("File has %u bytes", count-1);
     fclose(fp);
-    
+
     return 0;
 }
+// name the file "FL_IO_9bytcount.c"
+//FL_IO_9bytcount swap_2
 
 
 
