8453e24787cd6fea7403a755bf5188b6ca912cc4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-01 13:28:06 +0600

    Update C_Ch6_2_3_feof_ferror.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_3_feof_ferror.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_3_feof_ferror.c
@@ -36,6 +36,7 @@
 
 
 
+
 /* Example 0: Using the feof() function, this code fragment shows how to read to the end of a file: */
 
 FILE *fp;
@@ -130,7 +131,7 @@ int main(int argc, char *argv[]){
     return 0;
 }   
 // name this code-file "FL_IO_7cpy.c" then open CLI inside working folder, run following code in CLI
-// FL_IO_7cpy myfile fileio_cpy 
+// FL_IO_7cpy myfile fileio_cpy    
 
 
 
@@ -222,9 +223,6 @@ int main(int argc, char *argv[]){
 #include <stdio.h>
 #include <stdlib.h>
 
-// #include <ctype.h>
-// #include <string.h>
-
 int main(int argc, char *argv[]){
     FILE *fp;
     unsigned count;
@@ -236,6 +234,7 @@ int main(int argc, char *argv[]){
     }
 
     // open  file
+    // if((fp = fopen(argv[1], "rb"))==NULL) {
     if((fp = fopen(argv[1], "r"))==NULL) {
         printf("Cannot open file.\n");
         exit(1) ;
@@ -266,65 +265,73 @@ int main(int argc, char *argv[]){
 
 
 
-
-
-
-/* Example 4: (Swap the content betn files ) 
+/* Example 4: (Swap / EXCHANGE the content betn files ) 
             Write a program that exchanges the contents of the two files whose names are specified on the command line. 
             That IS , given two files called FILE1 and FILE2, after the program has run ,
             FILE1 will contain the contents that originally were in FILE2,
-            and FILE2 will contam FILE1 's original contents. 
-            (Hint: Use a temporary file to aid in the exchange process. Works like a "temp" swap variable) */
+            and FILE2 will contain FILE1 's original contents. 
+            (Hint: Use a temporary file to aid in the exchange process. Works like a "temp" swap variable) 
 
-// we will exchange two files
+Notice the usage of "rb" for READING and "wb" for WRITING            
+*/
+
+//  EXCHANGE two files
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]){
     FILE *f1,*f2,*temp;
     char ch;
     
-    /* see if correct line of command line arguments */
+    // see if correct line of command line arguments 
     if(argc!=3){
         printf("Usage: exchange <f1> <f2>.\n");
-        exit (1);
+        exit(1);
     }
     
-    // open first file
-    if((f1 = fopen(argv[1], "r"))==NULL) {
+    // open first file for reading
+    if((f1 = fopen(argv[1], "rb"))==NULL) {
         printf("Cannot open first file.\n");
         exit(1) ;
     }
     
-    // open second file
-    if((f2 = fopen(argv[2], "r"))==NULL) {
+    // open second file for reading
+    if((f2 = fopen(argv[2], "rb"))==NULL) {
         printf("Cannot open second file.\n");
         exit(1) ;
     }
     
-    // open temporary file
-    if((temp = fopen("temp.tmp", "r"))==NULL) {
+
+    // open temporary file for writing
+    // following creates a Temporary file "temp.tmp"
+    // if((temp = fopen("temp.tmp", "wb"))==NULL) {
+    //     printf("Cannot open temporary file.\n");
+    //     exit(1) ;
+    // }
+
+    // use this if you already have "temp" file
+    if((temp = fopen("temp", "wb"))==NULL) {
         printf("Cannot open temporary file.\n");
         exit(1) ;
     }
+
     
-    /* copy f1 to temp */
-    while (!feof(f1)){
+    // copy f1 to temp 
+    while(!feof(f1)){
         ch = fgetc(f1);
         if(!feof(f1)) fputc(ch, temp);
     }
     fclose(f1);
     
-    // open first file for output
+
+    // open first file for output/writing
     if((f1 = fopen(argv[1], "wb"))==NULL) {
         printf("Cannot open first file.\n");
         exit(1) ;
     }
     
-    /* copy f2 to f1 */
+    // copy f2 to f1
     while(!feof(f2)){
         ch = fgetc (f2) ;
         if(!feof(f2)) fputc(ch, f1);
@@ -332,14 +339,19 @@ int main(int argc, char *argv[])
     fclose(f2);
     fclose(temp) ;
     
-    // open second file for output
+    // open second file for output/writing
     if((f2 = fopen(argv[2], "wb"))==NULL) {
         printf("Cannot open second file.\n");
         exit(1) ;
     }
     
-    // open temp file for input
-    if((temp = fopen("temp.tmp", "rb"))==NULL) {
+    // open temp file for input/reading
+    // if((temp = fopen("temp.tmp", "rb"))==NULL) {
+    //     printf("Cannot open temporary file.\n");
+    //     exit(1) ;
+    // }
+    // use this if you already have "temp" file
+    if((temp = fopen("temp", "rb"))==NULL) {
         printf("Cannot open temporary file.\n");
         exit(1) ;
     }
@@ -348,8 +360,7 @@ int main(int argc, char *argv[])
     while(!feof(temp))
     {
         ch = fgetc(temp);
-        if(!feof(temp))
-            fputc(ch,f2);
+        if(!feof(temp)) fputc(ch,f2);
     }
     
     fclose(f1);
@@ -358,5 +369,99 @@ int main(int argc, char *argv[])
     
     return 0;
 }
+// name this file "FL_IO_10swap.c"
+// FL_IO_10swap swap_1 swap_2
 
+
+
+// Version 2: In follwing version dont need to create "temp" file manually
+//  EXCHANGE two files
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+    FILE *f1,*f2,*temp;
+    char ch;
+    
+    // see if correct line of command line arguments 
+    if(argc!=3){
+        printf("Usage: exchange <f1> <f2>.\n");
+        exit(1);
+    }
+    
+    // open first file for READING
+    if((f1 = fopen(argv[1], "rb"))==NULL) {
+        printf("Cannot open first file.\n");
+        exit(1) ;
+    }
+    
+    // open second file for READING
+    if((f2 = fopen(argv[2], "rb"))==NULL) {
+        printf("Cannot open second file.\n");
+        exit(1) ;
+    }
+    
+    // open temporary file for WRITING
+    // following creates a Temporary file "temp.tmp"
+    if((temp = fopen("temp.tmp", "wb"))==NULL) {
+        printf("Cannot open temporary file.\n");
+        exit(1) ;
+    }
+
+
+    // copy f1 to temp 
+    while(!feof(f1)){
+        ch = fgetc(f1);
+        if(!feof(f1)) fputc(ch, temp);
+    }
+    
+    fclose(f1);     // close first file (after reading)
+    
+
+    // open first file (again) for OUTPUT/WRITING
+    if((f1 = fopen(argv[1], "wb"))==NULL) {
+        printf("Cannot open first file.\n");
+        exit(1) ;
+    }
+    
+    // copy f2 to f1
+    while(!feof(f2)){
+        ch = fgetc (f2) ;
+        if(!feof(f2)) fputc(ch, f1);
+    }
+
+    fclose(f2);     // close second file (after reading)
+    fclose(temp) ;  // close temp file (after writing)
+    
+
+    // open second file (again) for OUTPUT/WRITING
+    if((f2 = fopen(argv[2], "wb"))==NULL) {
+        printf("Cannot open second file.\n");
+        exit(1) ;
+    }
+    
+    // open temp file (again) for INPUT/READING
+    if((temp = fopen("temp.tmp", "rb"))==NULL) {
+        printf("Cannot open temporary file.\n");
+        exit(1) ;
+    }
+    
+
+    // copy tmep to f2
+    while(!feof(temp)){
+        ch = fgetc(temp);
+        if(!feof(temp)) fputc(ch,f2);
+    }
+    
+    fclose(f1);     // close first file
+    fclose(f2);     // close second file
+    fclose(temp);   // close temp file (after reading)
+    
+    return 0;
+}
+// name this file "FL_IO_10swap_2.c"
+// FL_IO_10swap_2 swap_1 swap_2
+
+// Note: notice the usage of "rb" for READING and "wb" for WRITING
 
