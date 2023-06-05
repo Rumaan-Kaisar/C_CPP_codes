00953294b087075877b84e7a27f1eec3a8fa2e66
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-05 13:35:58 +0600

    Update C_Ch6_2_4_stringIO_filio.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_4_stringIO_filio.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_4_stringIO_filio.c
@@ -1,16 +1,429 @@
+/* 
+    --------------------    String/TEXT I/O in a File    --------------------
 
-6.11 String I/O in a File with fputs() & fgets(). Text I/O with fprintf() & fscanf()
-We use these two functions fputs() and fgets() , which write a string to and read a string from a file, respectively. Their prototypes are
-int fputs(char *str, FILE *fp);
-char *fgets(char *str, int num, FILE *fp);
+        String I/O in a File with fputs() & fgets(). 
 
-fputs() : The fputs() function writes the string pointed to by str  to the file associated with fp. It returns EOF if an error occurs and a non-negative value if successful. The null that terminates str is not written. Also, unlike its related function puts() it does not automatically append a carriage return, linefeed pair. Example : fputs(str_1, f_point);
+        Text I/O with fprintf() & fscanf()
 
-fgets() : The fgets() function reads characters from the file associated with fp into the string pointed to by str until "num-1" (one less than the string length number) characters have been read, a newline character is encountered, or the end of the file is reached. In any case, the string is null-terminated. Unlike its related function gets(), the newline character is retained. The function returns str if successful and a null pointer if an error occurs. Example : fgets(str_1, 79, f_point); /* 0 to 79 = 80 */
 
-fprintf () & fscanf() : The C file system contains two very powerful functions fprintf() and fscanf() similar to printf() and scanf(). These functions operate exactly like printf() and scanf() except that they work with files. Their prototypes are:
 
-int fprintf(FILE *fp, char *control-string, … );
-int fscanf(FILE *fp, char * control-string, … );
 
-Instead of directing their I/O operations to the console, these functions operate on the file specified by fp. Otherwise their operations are the same as their console-based relatives. The advantage to fprintf() and fscanf() is that they make it very easy to write a wide variety of data to a file using a text format.
+    --------------------    String I/O using fputs() and fgets()    --------------------
+
+    fputs() and fgets()
+        Writes a string to and read a string from a file, respectively. Their prototypes are
+
+            int fputs(char *str, FILE *fp);     // writes
+            char *fgets(char *str, int num, FILE *fp);      // reads
+
+
+        fputs() 
+            'fputs()' writes the string pointed to by "str"  to the file associated with "fp". 
+                EOF returned if an error occurs and a 
+                non-negative value returned if successful
+
+            The 'null' that terminates 'str' is not written. 
+            unlike its related function puts() it does not automatically append a carriage return, linefeed pair. 
+                Example : 
+                    fputs(str_1, f_point);
+
+
+        fgets()
+            'fgets()' reads characters from the file associated with "fp" into the string pointed to by "str"
+                    until "num-1" (one less than the string length number) characters have been read, 
+                    or a newline character is encountered, 
+                    or the end of the file (EOF) is reached. 
+                In any case, the string is null-terminated. 
+            
+            Unlike its related function gets(), the newline character is retained. 
+            
+            "str" is returned if successful 
+            'null pointer' returned if an error occurs. 
+                Example : 
+                    fgets(str_1, 79, f_point); // 0 to 79 = 80
+
+
+
+
+    --------------------    TEXT I/O using fputs() and fgets()    --------------------
+
+    fprintf() & fscanf()
+        fprintf() and fscanf() similar to printf() and scanf() except that they work with files. 
+        Their prototypes are:
+
+            int fprintf(FILE *fp, char *control-string, … );
+            int fscanf(FILE *fp, char * control-string, … );
+
+        These functions operate on the file specified by "fp".
+        their operations are the same as printf() and scanf().
+        
+        fprintf() and fscanf() make it very easy to write a wide variety of data to a file using a text format.
+*/
+
+
+
+
+/* Example 1: This program demonstrates fputs() and fgets(). It reads lines entered by the user 
+                and writes them to the file specified on the command line. 
+                When the user enters a blank line, the input phase terminates, and the file is closed. 
+                Next, the file is reopened for input, and the program uses fgets() to display the contents of the file. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    FILE *fp;
+    char str[80];
+    
+    //check for command line argument
+    if(argc != 2){
+        printf("Sepcify file name.\n");
+        exit(1);
+    }
+    
+    // open file for output/write
+    if((fp==fopen(argv[1],"w"))==NULL){
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    
+    printf("Enter a blank line to stop.\n");
+    do{
+        printf(": ");
+        gets(str);
+        strcat(str,"\n"); //add new line
+        if(*str != '\n') fputs(str,fp);
+    }while(*str != '\n');
+    fclose(fp);
+    
+    //open file for input
+    if((fp==fopen(argv[1],"r"))==NULL)
+    {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    
+    //read back the file
+    do{
+        fgets(str,79,fp);
+        if(!feof(fp)) printf(str);
+    }while(!feof(fp));
+    fclose(fp);
+    
+    return 0;
+}
+
+
+
+
+
+/* Example 2: This program demonstrates fprintf() and facanf(). 
+                It first writes a double, an int and a string to the file specified on the command line. 
+                Next, it reads them back and displays their values as verification. 
+                If you examine the file created by this program. you will see that it contains human-readable text. 
+
+                This is because fprintf() writes to a disk file what printf() would write to the screen. 
+                    No internal data formats are used. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    double ld;
+    int d;
+    char str[80];
+    
+    //check for command line argument
+    if(argc != 2)
+    {
+        printf("Sepcify file name.\n");
+        exit(1);
+    }
+    
+    //open file for output
+    if((fp==fopen(argv[1],"w"))==NULL)
+    {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    
+    fprintf(fp,"%f %d %s", 12345.342, 1908, "hello");
+    fclose(fp);
+    
+    
+    
+    //open file for input
+    if((fp==fopen(argv[1],"r"))==NULL)
+    {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+    
+    fscanf(fp," %lf%d%s",&ld,&d,str);
+    printf("%f %d %s",ld,d,str);
+    fclose(fp);
+    
+    return 0;
+}
+
+
+
+
+/* Example 3: wrate a very simple telephone-directory program by allowing the directory to be saved to a disk fIle. 
+                Have the program present a menu that looks like this:
+
+                        1. Enter the names and numbers
+                        2. Find numbers
+                        3. Save directory to disk
+                        4. Load directory from disk
+                        5. Quit
+
+                The program should be capable of storing 100 names and numbers. (Use only first names if you like.) 
+                Use fprintf() to save the directory to disk and fscanf() to read it back into memory. */
+// A simple computerized telephone book.
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char names[100][40], numbers[100][40];
+
+int loc = 0;
+
+int menu(void);
+void enter(void);
+void load(void);
+void save(void);
+void find(void);
+
+int main(void)
+{
+    int choice;
+
+    do{
+        choice = menu();
+        switch(choice){
+            case 1: enter();
+                break;
+            case 2: find();
+                break;
+            case 3: save();
+                break;
+            case 4: load();
+                break;
+        }
+    } while(choice!=5);
+
+    return 0;
+}
+
+
+// Get menu choice
+int menu(void)
+{
+    int i;
+    char str[80];
+
+    printf("1. Enter names and numbers\n");
+    printf("2. Find numbers\n");
+    printf("3. Save directory to disk\n");
+    printf("4. Load directory from disk\n");
+    printf("5. Quit\n");
+
+    do{
+        printf("Enter your choice\n");
+        gets(str);
+        i = atoi(str);
+        printf("\n");
+    } while(i<1 || i>5);
+
+    return i;
+}
+
+void enter(void)
+{
+    for( ;loc<10;loc++)
+    {
+        if(loc<10)
+        {
+            printf("Enter name and phone number:\n");
+            gets(names[loc]);
+            if(!*names[loc])
+                break;
+            gets(numbers[loc]);
+        }
+    }
+}
+
+void find(void)
+{
+    char name[80];
+    int i;
+
+    printf("Enter name: ");
+    gets(name);
+
+    for(i=0;i<100;i++)
+    {
+        if(!strcmp(name,names[i]))
+            printf("%s %s\n",names[i],numbers[i]);
+    }
+}
+
+void load(void)
+{
+    FILE *fp;
+
+    if((fp = fopen("phone","r"))==NULL)
+    {
+        printf("Cannot open file\n");
+        exit(1);
+    }
+
+    loc = 0;
+    while(!feof(fp))
+    {
+        fscanf(fp,"%s%s",names[loc],numbers[loc]);
+        loc++;
+    }
+    fclose(fp);
+}
+
+void save(void)
+{
+    FILE *fp;
+    int i;
+
+    if((fopen("phone","w"))==NULL)
+    {
+        printf("Cannot open file\n");
+        exit(1);
+    }
+
+    for(i=0;i<loc;i++)
+    {
+        fprintf(fp,"%s %s\n",names[i],numbers[i]);
+    }
+    fclose(fp);
+}
+
+
+
+
+
+/* Example 4: Write a program that uses fgets() to display the contents of a text file , one screenful at a time. 
+                After each screen is displayed, have the program prompt the user for more. */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *fp;
+    char ch, str[80];
+    int count;
+
+    // see if correct line of command line arguments
+    if(argc!=2)
+    {
+        printf("Usage: display <file>\n");
+        exit(1);
+    }
+
+    // open the file
+    if((fp=fopen(argv[1],"r"))==NULL)
+    {
+        printf("Cannot open file.\n");
+        exit(1);
+    }
+
+    count = 0;
+    while(!feof(fp))
+    {
+        fgets(str,79,fp);
+        printf("%s", str);
+        count++;
+
+        if(count==23)
+        {
+            printf("More? (y/n) ");
+            gets(str);
+            if(toupper(*str)=='N') break;
+            count = 0;
+        }
+    }
+
+    fclose(fp);
+
+    return 0;
+}
+
+
+
+/* Example 5: Write a program that copies a text file . 
+                Specify both the source and destination file names on the command line. 
+                Use fgets() and fputs() to copy the file. Include full error checking */
+                // Copy a file
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    FILE *from, *to;
+    char str[128];
+
+    // see if correct line of command line arguments
+    if(argc<3)
+    {
+        printf("Usage: copy <source> <destination>\n");
+        exit(1);
+    }
+
+    // open source file
+    if((from=fopen(argv[1],"r"))==NULL)
+    {
+        printf("Cannot open source file.\n");
+        exit(1);
+    }
+
+    // open destination file
+    if((to=fopen(argv[2],"w"))==NULL)
+    {
+        printf("Cannot open destination file.\n");
+        exit(1);
+    }
+
+    // copy the file
+    while(!feof(from))
+    {
+        fgets(str,127,from);
+        if(ferror(from))
+        {
+            printf("Error on input.\n");
+            break;
+        }
+        if(!feof(from)) fputs(str,to);
+        if(ferror(to))
+        {
+            printf("Error on output.\n");
+            break;
+        }
+    }
+
+    if(fclose(from)==EOF)
+    {
+        printf("Error closing source file.\n");
+        exit(1);
+    }
+
+    if(fclose(to)==EOF)
+    {
+        printf("Error closing destination file.\n");
+        exit(1);
+    }
+
+    return 0;
+}
