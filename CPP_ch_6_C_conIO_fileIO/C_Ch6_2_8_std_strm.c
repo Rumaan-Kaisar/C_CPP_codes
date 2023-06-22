9e222ef5f5940c87230e464ed6e043e4f1e234f2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-22 09:49:15 +0600

    Update C_Ch6_2_8_std_strm.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_8_std_strm.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_8_std_strm.c
@@ -1,21 +1,47 @@
-6.15 THE STANDARD STREAMS
-Standard  streams  : When a C program begins execution, three streams are automatically opened and available for use. These streams are called
-	standard input (stdin), 
-	standard output (stdout), and 
-	standard error (stderr). 
 
-stdin inputs from the keyboard;  stdout and stderr write to the screen. By default, they refer to the console, but in environments that Support redirectable I/O, they can be redirected by the operating system to some other device.
+---------------    THE STANDARD STREAMS    ---------------
+Standard  streams: 
+    When a C program 'BEGINS EXECUTION', three streams are automatically opened and available for use.
 
-[1]	These standard streams are FILE pointers and may be used with any function that requires a variable of type FILE *. For example, you can use fprintf() to print formatted output to the screen by specifying stdout as its output stream. The following two statements are functionally the same:
+            1. standard input (stdin), 
+            2. standard output (stdout), and 
+            3. standard error (stderr). 
 
-fprintf(stdout, "%d %c %s", 100, 'c', "this is a string");
-printf("%d %c %s", 100, 'c', "this is a string");
+    "stdin" inputs from the KEYBOARD;  
+    "stdout" and "stderr" write to the SCREEN.
 
-[2]	In actuality, C makes little distinction between console I/O and file I/O. As just shown, it is possible to perform console I/O using several of the file-system functions. 
+    By default, they refer to the "CONSOLE", 
+        but in environments that Support redirectable I/O, they can be redirected by the OS to some other device.
 
-[3]	It is also possible to perform disk file I/O using console I/O functions, such as printf() . Here's why. All of the functions described in 6.1 to 6.7 referred to as "console I/O functions" are actually special-case file-system functions that automatically operate on stdin and stdout. 
 
-[4]	As far as C is concerned, the console is simply another hardware device. You don't actually need the console functions to access the console. Any file -system function can access it. (Of course, non-standard I/O functions like getche() are differentiated from the standard file-system functions and do, in fact, operate only on the console.) 
+
+These 'standard streams' are "FILE pointers" and may be used with any function that requires a variable of type "FILE *". 
+
+ConIO using FileIO:
+    You can use fprintf() to print "formatted output" to the screen by specifying "stdout" as its output stream. 
+        The following two statements are functionally the same:
+    
+            // using FileIO function
+            fprintf(stdout, "%d %c %s", 100, 'c', "this is a string");
+
+            // usning ConIO function
+            printf("%d %c %s", 100, 'c', "this is a string");
+
+
+    In actuality, C makes little distinction between 'console-I/O' and 'file-I/O'. 
+        As just shown, it is possible to perform "console I/O" using several of the 'file-system functions'. 
+
+
+Disk FileIO using ConIO:
+    It is also possible to perform "DISK FILE I/O" using 'CONSOLE I/O' functions, such as printf().
+        Because all "console I/O functions" are actually 'special-case file-system functions' that automatically operate on "stdin" and "stdout". 
+
+    The console is simply another hardware device. 
+        You don't actually need the 'console functions' to access the "console". Any 'file -system function' can access it. 
+        (non-standard I/O functions like getche() are differentiated from the 'standard file-system functions' and operate "only on the console".) 
+
+
+
 
 Redirection of std streams : In environments that allow redirection of I/O, stdin and stdout could refer to devices other than the keyboard and screen. Since the console functions operate on stdin and stdout. if these streams are redirected, the "console" functions can be made to operate on other devices. For example, by redirecting the stdout to a disk file. you can use a "console" I/O function to write to a disk file.
 
