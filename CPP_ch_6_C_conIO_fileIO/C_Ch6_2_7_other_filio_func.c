ee1c7dd2d1cb8a5b7ba50a78a7553e8b404b7376
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-22 09:28:47 +0600

    Update C_Ch6_2_7_other_filio_func.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
@@ -48,4 +48,96 @@
 */
 
 
+// Think of ways that rewind() and fflush() could be useful in real applications.
+
+
+/* Example 1: This program demonstrates remove(). It prompts the user for
+the file to erase and also provides a safety check in case the user
+entered the wrong name. */
+
+#include <stdio.h>
+~inc:ude <stdlib.h>
+_include <ctype.h>
+int main(void)
+{
+)
+char fname[BO};
+printf("Enter name of file to erase: ");
+gets (fname) ;
+printf("Are you sure? (YIN) ");
+if(toupper(getchar(»=='Y') remove (fname) ;
+return 0;
+
+
+
+/* Example 2: The following program demonstrates rewind() by displaying
+the contents of the file specified on the command line twice. */
+
+#include <stdio.h>
+#include cstdlih.h>
+int main(int argc, char *argv(])
+(
+)
+FILE *fp;
+1* see if file name is specified */"
+if(argc!=2) {
+)
+printf (-File name missing. \ n");
+exitll) ;
+if II fp = fopen largv [1J, "r"» ==NULL) (
+printfC"Cannot open file. \ n-);
+exit(1) ;
+)
+/* show it once * 1
+whil,.l! feof Ifp) )
+putcharlgetclfp»;
+rewind ( £p) ;
+1* show it twice • I
+while I! feof Ifp) )
+putchar(getc(fp» ;
+fcloselfp) ;
+return 0;
+
+
+
+/* Example 3: This fragment causes the buffer associated with fp to be flushed */
+to disk.
+FILE *fp;
+fflush(fp) ;
+
+
+/* Example 4: This program renames a file called MYFILE.TXT to
+YOURFILE.TXT. */
+#include <stdio.h>
+int main(void)
+(
+if (rename ( "myfi Ie. txt", ·yourfile. txt") )
+printf ("Rename failed. \n") ;
+else
+printf (" Rename successful. \n ~ ) ;
+return 0;
+}
+
+
+/* Example 5: Improve the erase program (Example 1) so that it notifies the user if he or
+she tries to renlove a nonexistent file */
+#include <stdio.h>
+.include <stdlib.h>
+linclude <ctype.h>
+int main(void)
+(
+char fname[BO];
+printfC-Enter name of file to erase: .);
+gets (fname) j
+printfC-Are you sure? (YIN) .);
+if(toupper(getchar(ll=='Y'}
+if (remove (fname) )
+-878
+9.B EXERCISE
+.,
+printfC-'nFile not found or write' protected.\n-);
+return 0;
+1
+
+
 
