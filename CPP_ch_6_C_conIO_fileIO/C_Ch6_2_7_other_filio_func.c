9fda4b76b953e402556d622095e3a6f1752fdd77
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-22 09:07:08 +0600

    Update C_Ch6_2_7_other_filio_func.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
@@ -1,17 +1,51 @@
-6.14 Some other important File-System functions
-Rename a file : To rename a file use rename(), shown here:
-int rename(char *oldname, char *newname);
-Here, oldname points to the original name of the file and newname points to its new name. The function returns zero if successful and nonzero if an error occurs.
 
-Erase a file : To erase a file use remove(), Its prototype is
-int remove(char *file-name);
-This function will erase the file whose name matches that pointed to by file-name. It returns zero if successful and nonzero if an error occurs.
+/* 
+    ----------------    Other important File-System functions    ----------------
 
-Position change : To position a file's current location to the start of the file use rewind(). Its prototype is
-void rewind(FILE *fp);
-It rewinds the file associated with fp. The rewind() function has no return value, because any file that has been successfully opened can be rewound.
+    rename() - Rename a file : 
+        To rename a file use rename() :
 
-Flush disk buffer : To cause a file's disk buffer to be flushed use fflush(). Its prototype is
-int fflush(FILE *fp);
-It flushes the buffer of the file associated with fp. The function returns zero if successful, EOF if a failure occurs. If you call fflush() using a NULL for fp, all existing disk buffers are flushed.
+            int rename(char *oldname, char *newname);
+
+            "oldname" points to the original name of the file and 
+            "newname" points to its new name. 
+            The function returns 'ZERO' if 'SUCCESSFUL' and 
+            'NONZERO' if an 'ERROR' occurs.
+
+
+
+    remove() - Erase a file : 
+        To erase a file use remove() : 
+
+            int remove(char *file-name);
+
+        will erase the file whose name matches that pointed to by 'file-name'. 
+        It returns 'ZERO' if 'SUCCESSFUL' and 
+        'NONZERO' if an 'ERROR' occurs.
+
+
+
+    rewind() - Position change : 
+        To position a file's current location to the "start of the file" use rewind().
+        
+            void rewind(FILE *fp);
+
+            It rewinds the file associated with fp. 
+            It has no return value, because any file that has been successfully opened can be rewind.
+
+
+
+    fflush() - Flush disk buffer : 
+        To cause a file's disk buffer to be flushed use fflush().
+        
+            int fflush(FILE *fp);
+
+            It flushes the buffer of the file associated with 'fp'. 
+            Returns "ZERO" if "SUCCESSFUL", 
+            "EOF" if a "FAILURE" occurs. 
+            
+            If you call fflush() using a NULL for fp, all 'existing disk buffers' are "flushed".
+*/
+
+
 
