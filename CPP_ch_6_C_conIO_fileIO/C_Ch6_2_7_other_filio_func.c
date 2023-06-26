75825e4676b1cf47e84363dd18f399b12da8ce26
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-26 07:54:30 +0600

    Update C_Ch6_2_7_other_filio_func.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_7_other_filio_func.c
@@ -50,22 +50,28 @@
 // Think of ways that rewind() and fflush() could be useful in real applications.
 
 
+
+
 /* Example 1: This program demonstrates remove(). It prompts the user for
                 the file to erase and also provides a safety check in case the user
                 entered the wrong name. */
 
 #include <stdio.h>
-~inc:ude <stdlib.h>
-_include <ctype.h>
-int main(void)
-{
-)
-char fname[BO};
-printf("Enter name of file to erase: ");
-gets (fname) ;
-printf("Are you sure? (YIN) ");
-if(toupper(getchar(»=='Y') remove (fname) ;
-return 0;
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void){
+    char fname[80];
+
+    printf("Enter name of file to erase: ");
+    gets(fname);
+    printf("Are you sure? (YIN) ");
+
+    if(toupper(getchar())=='Y') remove(fname);
+
+    return 0;
+}
+
 
 
 
@@ -73,68 +79,86 @@ return 0;
                 the contents of the file specified on the command line twice. */
 
 #include <stdio.h>
-#include cstdlih.h>
-int main(int argc, char *argv(])
-(
-)
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+    FILE *fp;
+
+    // see if file name is specified
+    if(argc!=2) {
+        printf("File name missing. \n");
+        exit(1);
+    }
+
+    // open file for reading
+    if((fp = fopen(argv[1], "r")) == NULL) {
+        printf("Cannot open file. \n");
+        exit(1);
+    }
+
+    // show it once
+    while(!feof(fp)) putchar(getc(fp));
+
+    rewind(fp);
+
+    // show it twice.
+    // since in previus statement rewind() is used, location is now set to 'start'
+    while(!feof(fp)) putchar(getc(fp));
+
+    fclose(fp);
+
+    return 0;
+}
+
+// FL_IO_adv_2 myfile.txt
+
+
+
+
+/* Example 3: This fragment causes the buffer associated with 'fp' to be flushed to disk. */
+
 FILE *fp;
-1* see if file name is specified */"
-if(argc!=2) {
-)
-printf (-File name missing. \ n");
-exitll) ;
-if II fp = fopen largv [1J, "r"» ==NULL) (
-printfC"Cannot open file. \ n-);
-exit(1) ;
-)
-/* show it once * 1
-whil,.l! feof Ifp) )
-putcharlgetclfp»;
-rewind ( £p) ;
-1* show it twice • I
-while I! feof Ifp) )
-putchar(getc(fp» ;
-fcloselfp) ;
-return 0;
+.
+.
+fflush(fp);
 
 
-
-/* Example 3: This fragment causes the buffer associated with fp to be flushed to disk. */
-
-FILE *fp;
-fflush(fp) ;
 
 
 /* Example 4: This program renames a file called MYFILE.TXT to YOURFILE.TXT. */
 #include <stdio.h>
-int main(void)
-(
-if (rename ( "myfi Ie. txt", ·yourfile. txt") )
-printf ("Rename failed. \n") ;
-else
-printf (" Rename successful. \n ~ ) ;
-return 0;
+
+int main(void){
+    if(rename("myfile.txt", "yourfile.txt")) printf ("Rename failed. \n");
+    else printf("Rename successful. \n");
+
+    return 0;
 }
 
 
+
+
 /* Example 5: Improve the erase program (Example 1) so that it notifies the user if he or
-                she tries to renlove a nonexistent file */
+                she tries to remove a nonexistent file */
 
 #include <stdio.h>
-.include <stdlib.h>
-linclude <ctype.h>
-int main(void)
-(
-char fname[BO];
-printfC-Enter name of file to erase: .);
-gets (fname) j
-printfC-Are you sure? (YIN) .);
-if(toupper(getchar(ll=='Y'}
-if (remove (fname) )
--878
-9.B EXERCISE
-.,
-printfC-'nFile not found or write' protected.\n-);
-return 0;
-1
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(void){
+    char fname[80];
+
+    printf("Enter name of file to erase: ");
+    gets(fname);
+    printf("Are you sure? (YIN) ");
+
+    if(toupper(getchar())=='Y'){
+        if(remove(fname)){
+            printf("\n File not found or write' protected \n");
+        }
+    } 
+
+    return 0;
+}
+
 
