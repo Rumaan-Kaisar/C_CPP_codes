1b446a23f7c230464361571d4f54da2d1f3c3399
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-07-03 14:20:52 +0600

    Update C_Ch6_2_4_stringIO_filio.c

--- CPP_ch_6_C_conIO_fileIO/C_Ch6_2_4_stringIO_filio.c
+++ CPP_ch_6_C_conIO_fileIO/C_Ch6_2_4_stringIO_filio.c
@@ -472,3 +472,205 @@ int main(int argc, char *argv[]) {
 // FL_IO_ms2 fl_1 fl_3
 
 
+
+
+/* Example 7: Enhance the "electronic Library" card-catalog program (from C_Ch6_1_4_mstr_gets_puts and concept from C_Ch6_2_4_stringio_fileio)
+                that it stores its information in a disk file called "CATALOG".
+
+                When the program begins, have it read the catalog into memory.
+                Also, add an option to save the information to disk.
+
+                Card Catalog:
+                            1. Enter Books
+                            2. Search by author
+                            3. Search by Title
+                            4. Save catalog
+                            5. Quit
+
+
+                If you choose Enter, have the program repeatedly input the name, author, and publisher of a book.
+                    Have this process continue until the user enters a blank line for the name of the book.
+
+                For searches, prompt the user for the specified author or title and
+                then, if a match is found, display the rest of the information.
+
+                The program should be capable of storing 100 book info.
+*/
+
+/* =-=-=-=-=-=-=-=-=-=-=            An electronic card catalog             =-=-=-=-=-=-=-=-=-=-= */
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+int menu(void);
+void enter(void);
+void load(void);
+void save(void);
+
+void display(int i);
+void author_search(void);
+void title_search(void);
+
+char names[MAX][80];    // author name
+char titles[MAX][80];   // titles
+char pubs[MAX][80];     // publisher
+
+int top = 0;    // last location used
+
+
+int main(void){
+    int choice;
+
+    load();  // open to read in catalog
+
+    do{
+        choice = menu();
+        switch(choice){
+            case 1: enter(); // enter books
+                break;
+            case 2: author_search(); // search by author
+                break;
+            case 3: title_search(); // search by, title
+                break;
+            case 4: save();
+                break;
+        }
+    } while(choice!=5);
+
+    return 0;
+}
+
+
+// Get menu choice, select a choice
+int menu(void){
+    int i;
+    char str[80];
+
+    printf("Card Catalog:\n");
+    printf("1. Enter Books\n");
+    printf("2. Search by author\n");
+    printf("3. Search by Title\n");
+    printf("4. Save catalog\n");
+    printf("5. Quit\n");
+
+    do{
+        printf("Choose your selection:\n");
+        gets(str);
+        i = atoi(str);
+        printf("\n");
+    } while(i<1 || i>5);
+
+    return i;
+}
+
+
+// Enter books into Database
+void enter(void){
+    int i;
+
+    for( i=top; i<MAX; i++){
+        printf("Enter author name (ENTER to quit): ");
+        gets(names[i]);
+        if(!*names[i]) break;
+
+        printf("Enter title: ");
+        gets(titles[i]);
+
+        printf("Enter publisher: ");
+        gets(pubs[i]);
+    }
+
+    top = i;
+}
+
+
+// Search by author
+void author_search(void){
+    char name[80];
+    int i, found;
+
+    printf("Name: ");
+    gets(name);
+
+    found= 0;
+
+    for(i=0; i<top; i++){
+        if(!strcmp(name, names[i])){
+            display(i) ;
+            found = 1;
+            printf( "\n" ) ;
+        }
+    }
+
+    if(!found) printf("Not Found\n");
+}
+
+
+// Search by TITLE
+void title_search(void){
+    char title[80];
+    int i, found;
+
+    printf("Title: ");
+    gets(title);
+
+    found= 0;
+
+    for(i=0; i<top; i++){
+        if(!strcmp(title, titles[i])){
+            display(i) ;
+            found = 1;
+            printf( "\n" ) ;
+        }
+    }
+
+    if(!found) printf("Not Found\n");
+}
+
+
+// Display catalog entry.
+void display(int i){
+    printf("%s\n", titles[i]);
+    printf("By %s\n", names[i]);
+    printf("Published by %s \n", pubs[i]);
+}
+
+
+// Load the catalog file
+void load(void){
+    FILE *fp;
+
+    if((fp = fopen("catalog","r"))==NULL){
+        printf("Catalog file not on disk\n");
+        // exit(1); we don't use it because we won't terminate the program
+        return; // we use "return" to continue the "main" function
+    }
+
+    fread(&top, sizeof top, 1, fp); // read count
+    fread(names, sizeof names, 1, fp);
+    fread(titles, sizeof titles, 1, fp);
+    fread(pubs , sizeof pubs, 1, fp);
+
+    fclose(fp);
+}
+
+
+void save(void){
+    FILE *fp;
+
+    // open for writing
+    if((fp = fopen("catalog","w"))==NULL){
+        printf("Cannot open catalog file\n");
+        exit(1);
+    }
+
+    fwrite(&top, sizeof top, 1, fp); // write count
+    fwrite(names, sizeof names, 1, fp);
+    fwrite(titles, sizeof titles, 1, fp);
+    fwrite(pubs, sizeof pubs, 1, fp);
+
+    fclose(fp);
+}
