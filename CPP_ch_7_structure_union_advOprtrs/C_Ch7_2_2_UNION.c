e615c9b8b0fcfea221cb50fba0178e32001df0f6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-24 16:24:52 +0600

    Update C_Ch7_2_2_UNION.c

--- CPP_ch_7_structure_union_advOprtrs/C_Ch7_2_2_UNION.c
+++ CPP_ch_7_structure_union_advOprtrs/C_Ch7_2_2_UNION.c
@@ -472,3 +472,25 @@ int main(void){
 }
 
 
+
+
+/* Example 9: Write a program that displays individually the values of the 'HIGH and LOW-order bytes' of a short integer.
+                (Hint: Use a 'union' that contains as its two elements a short integer and a 'two-byte character array'.)
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    union u_type {
+        short int i;
+        unsigned char c[2];
+    } uvar;
+
+    uvar.i = 99;
+
+    printf("High order byte: %u\n", uvar.c[1]);
+    printf("Low order byte: %u\n", uvar.c[0]);
+
+    return 0;
+}
+
