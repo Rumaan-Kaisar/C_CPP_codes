b35ce3da0f9f9717426288ecd16fa01946294344
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-25 14:45:47 +0600

    Update null.cpp

--- CPP_ch_7_structure_union_advOprtrs/null.cpp
+++ CPP_ch_7_structure_union_advOprtrs/null.cpp
@@ -2,62 +2,6 @@
 
 // ===============    DEBUGED    ===============
 
-/* Example 1: Write a program that contains two structure variables defined as:
-
-                    struct s_type {
-                        int i;
-                        char ch;
-                        double d;
-                    } var1, var2;
-
-                Have the program give each member of both structures initial values,
-                but make sure that the values differ between the two structures.
-
-                Using a function called struct_swap(), have the program swap the contents of var1 and var2.
-*/
-
-#include <stdio.h>
-
-struct s_type {
-    int i;
-    char ch;
-    double d;
-} var1, var2;
-
-void struct_swap(struct s_type *i, struct s_type *j);
-
-int main(void) {
-    var1.i = 100;
-    var2.i = 99;
-
-    var1.ch = 'a';
-    var2.ch = 'b';
-
-    var1.d = 1.0;
-    var2.d = 2.0;
-
-    printf("var1: %d %c %f \n", var1.i, var1.ch, var1.d);
-    printf("var2: %d %c %f \n", var2.i, var2.ch, var2.d);
-
-    struct_swap(&var1, &var2);
-
-    printf("After swap: \n");
-    printf("var1: %d %c %f \n", var1.i, var1.ch, var1.d);
-    printf("var2: %d %c %f \n", var2.i, var2.ch, var2.d);
-
-    return 0;
-}
-
-
-void struct_swap(struct s_type *i, struct s_type *j){
-    struct s_type temp;
-    temp = *i;
-    *i = *j;
-    *j = temp;
-}
-
-
-
 
 
 
