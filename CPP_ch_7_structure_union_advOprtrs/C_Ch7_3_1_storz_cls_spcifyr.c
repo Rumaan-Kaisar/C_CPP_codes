2eff353f8645e012743a649450a8c5275e589b9d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-29 01:01:41 +0600

    Update C_Ch7_3_1_storz_cls_spcifyr.c

--- CPP_ch_7_structure_union_advOprtrs/C_Ch7_3_1_storz_cls_spcifyr.c
+++ CPP_ch_7_structure_union_advOprtrs/C_Ch7_3_1_storz_cls_spcifyr.c
@@ -77,31 +77,74 @@ void f1(void){
 
 
 /* 
-    By placing extern in front of count's declaration in FILE #2, you are telling the compiler that 'count' is an integer defined elsewhere. 
+    By placing 'extern' in front of count's declaration in FILE #2, you are telling the compiler that 'count' is an integer defined ELSEWHERE. 
         In other words, using 'extern' informs the compiler about the existence and the type of the variable it precedes, 
             but it does not cause storage for that variable. 
+
+
+    If we didn't use 'extern' two errors would occur: 
+
+    Error 1: 
+        If we directly use : 
+
+            // FILE #2 
+            void f1(void){
+                count = 17 ;
+            } 
+            
+            An ERROR will be reported because 'count' is not defined. 
+
+
+    Error 2:   
+        If we even define count as :
+            
+            // FILE #2 
+            void f1(void){ 
+                int count; // re-declaring the variable i
+
+                count = 17;
+            } 
+            
+            many linkers will report a DUPLICATE-symbol ERROR, which means that 'count' is defined twice, and the linker doesn't know which to use.
+
+    The solution to these problem is C's 'extern' specifier. 
+
 */
 
-// rev 1 start
-If we didn't use extern two errors would occur : 
-Error 1 : If we directly use : /* FILE #2 */ void f1(void){count = 17 ;} an error will be reported because count is not defined. 
-Error 2  :   If we even define count as /* FILE #2 */ void f1(void){ int count; count = 17 ;} many linkers will report a duplicate-symbol error, which means that count is defined twice, and the linker doesn't know which to use.
 
-The solution to these problem is C's extern specifier. 
-	Use extern in same file to mention the global variable : We can use  extern inside a function to declare a global variable defined elsewhere in the same file (i.e.  to mention that it is global variable so that it won't be treated as a local variable ). But it is rarely used. Because, whenever the compiler encounters a variable name not defined by the function as a local variable, it assumes that it is global. For example, the following is valid:
 
+
+
+/* 
+    // another use of "extern"
+    Use 'extern' in SAME FILE to MENTION the global variable (not to treat as LOCAL variable): 
+        We can use  'extern' inside a 'function' to declare a global variable defined ELSEWHERE in the same file 
+        (i.e.  to mention that it is GLOBAL variable so that it won't be treated as a LOCAL variable ). 
+
+        But it is rarely used. 
+            Because, whenever the compiler encounters a 'variable name' not defined by the function as a LOCAL variable, it assumes that it is GLOBAL. 
+            For example, the following is valid:
+*/
+
+// Use 'extern' in SAME FILE 
 #include <stdio.h>
+
 int count;
-int main(void){ 	extern int count; /* this refers to global count *1
-count = 10;
-printf("%d", count);
-return 0;}
 
-// rev 1 end
+int main(void){ 	
+    extern int count; // this refers to global count 
+
+    count = 10;
+    printf("%d", count);
+
+    return 0;
+}
 
 
 
-//-------------------------    Linking multple source files    -------------------------
+
+
+//-------------------------    Linking multiple source files    -------------------------
 
 // How to Link Multiple C++ Files with G++:
     // first install "GCC compiler" or "codeBlocks with GCC"
@@ -136,36 +179,79 @@ return 0;}
 
 
 
+/* 
+    register (access AS FAST AS POSSIBLE): 
+        When you specify a 'register variable' you are telling the compiler that you want ACCESS to that variable to be AS FAST AS POSSIBLE. 
+        
+        In 'early version of C 'it causes the variables to be held in a register of the CPU. (This is how the name register came about.)
+            By using a register of the CPU, "EXTREMELY FAST ACCESS" times are achieved.
 
-[3]	register : When you specify a register variable you are telling the compiler that you want access to that variable to be as fast as possible. In early version of C it causes the variables to be held in a register of the CPU. (This is how the name register came about.) By using a register of the CPU, extremely fast access times are achieved.
-	In modern versions of C, the requirement that register variables must be held in a CPU register was removed. Instead, the ANSI C standard stipulates that a register variable will be stored in such a way as to minimize access time. This means that,
-	 register variables of type int and char continue to be held in a CPU register.
-	No matter what storage method is used, only so many variables can be granted the fastest possible access time. 
 
-For example, the CPU has a limited number of registers. When fast-access locations are exhausted, the compiler is free to make register variables into regular variables. 
-You must choose carefully which variables you modify with register.  One good choice is to make a frequently used variable, such as the variable that controls a loop, into a register variable.  The more times a variable is accessed, the greater the increase in performance when its access time is decreased. Generally, you can assume that at least two variables per function can be truly optimized for access speed.
-	Restriction of using pointes : Because a register variable may be stored in a register of the CPU, it may not have a memory address. This means that you cannot use the & to find the address of a register variable and cannot use pointers.
+        In 'modern versions of C', the requirement that register variables must be held in a CPU register was 'removed'. 
+            Now a register variable will be stored in such a way as to minimize access time. This means:
 
-[4]	static : Recall 3.2 how a local variable act : a local variable is created upon entry into its function and destroyed upon exit. And a global variable is active in whole program.
+                register variables of type 'int' and 'char' continue to be held in a CPU register.
+                
+                No matter what storage method is used, only so many variables can be granted the fastest possible access time. 
+                    e.g. the CPU has a limited number of registers. When fast-access locations are exhausted, 
+                        the compiler is free to make register variables into regular variables.
 
-	Use of static modifier on local variables :  When you use the static modifier, you cause the contents of a local variable to be preserved between function calls (That is it will not destroy when function exit and it act like a global variable inside its own function). 
 
-Unlike normal local variables, which are initialized each time a function is entered, a static local variable is initialized only once. 
-For example, take a look at this program,
+        You must choose carefully which variables you modify with register.  
+            use it for a frequently used variable, such as the variable that CONTROLS a LOOP, into a register variable.  
+                The more times a variable is accessed, the greater the increase in performance when its access time is decreased.
+                
+            Generally, you can assume that 'at least two variables per function' can be truly optimized for access speed.
+
+        pointers vs register:
+            Restriction of using pointers: 
+            Because a register variable may be stored in a register of the CPU, it may not have a 'memory address'. 
+                This means that you cannot use the "&" to find the ADDRESS of a 'register variable' and cannot use POINTERS.
+
+
+
+
+    static : 
+        Recall how a local variable act: a local variable is created upon entry into its function and destroyed upon exit. And a global variable is active in whole program.
+
+        Use of static modifier on local variables:  
+            When you use the static modifier, you cause the contents of a local variable to be preserved between function calls 
+                (That is it will not destroy when function exit and it act like a global variable inside its own function). 
+
+        Unlike normal local variables, which are 'initialized each time a function is entered', a static local variable is "INITIALIZED only ONCE". 
+*/
+
+
+
+/* Example 2: take a look at this program, notice how static is used. */
 
 #include <stdio.h>
+
 void f(void);
-int main(void){ int i;
-                for(i=0; i<10; i++) f();
-                return 0;}
 
-void f(void){   static int count = 0;
-                count++;
-                printf("count is : %d  ", count);}
-which displays the following output:
-count is : 1 count is : 2 . . . .  count is : 10 . Here count retains its value between function calls. 
-but without static modifier int count = 0; gives the result : count is : 1 count is : 1 . . . .  count is : 1. Which is obvious and we explained in 3.2 in chapter 3.
+int main(void){ 
+    int i;
+    for(i=0; i<10; i++) f();
+    return 0;
+}
 
+void f(void){   
+    static int count = 0;
+    // int count = 0;
+    count++;
+    printf("count is : %d  ", count);
+}
+
+
+// which displays the following output, . Here count retains its value between function calls. :
+count is : 1 count is : 2 . . . .  count is : 10 
+
+// but without static modifier int count = 0; gives the result : 
+count is : 1 count is : 1 . . . .  count is : 1. Which is obvious and we explained in 3.2 in chapter 3.
+
+
+
+---------29-aug-2023---------
 So why didn’t we use a global variable in the previous example? The answer is : The advantage to using a static local variable over a global one is that the static local variable is still known to and accessible by only the function (global variables active outside any function) in which it is declared.
 
 	Use of static modifier on global variables :  The static modifier may also be used on global variables ( more like a local variable for its own file). It causes the global variable to be known to and accessible by only the functions in the same file in which it is declared. This static global variable has no effect on other file (when we work with different files) which use the same named global variable.
