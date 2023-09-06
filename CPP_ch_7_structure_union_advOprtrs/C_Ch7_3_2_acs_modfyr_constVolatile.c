578c3246623dde4823cee52c460845731b2075d5
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-07 00:21:11 +0600

    Update C_Ch7_3_2_acs_modfyr_constVolatile.c

--- CPP_ch_7_structure_union_advOprtrs/C_Ch7_3_2_acs_modfyr_constVolatile.c
+++ CPP_ch_7_structure_union_advOprtrs/C_Ch7_3_2_acs_modfyr_constVolatile.c
@@ -60,9 +60,12 @@ int main(void) {
         'no statement in the function' can modify the variable pointed to by that parameter.
 
     The most important feature of 'const pointer parameters' is that 
-        they guarantee that many StL functions will not modify the variables pointed to by their parameters.
-*/
+        they guarantee that many Standared Library functions will not modify the variables pointed to by their parameters.
 
+    For example, here is the actual prototype to strlen() specified by the ANSI standard:
+                    size_t sll1en(const char *str);
+        Since 'str' is specified as conat, 'the string it points to' cannot be changed.
+*/
 
 
 
@@ -121,12 +124,71 @@ void pr_str(const char *p){
 
 
 
----------    rev    ---------
-For example, a variable's ADDRESS might be given to an 'interrupt service routine', and 
-        its value changed each time an interrupt occurs. 
+
+/* Example 3: When you precede a variable's type with volatile, you are telling the COMPILER 
+                that the 'value' may be changed in ways not 'explicitly defined' in the program.
+
+                For example, a variable's ADDRESS might be given to an 'interrupt service routine', and 
+                    its value changed each time an interrupt occurs. 
+*/ 
+
 volatile unsigned u;
+
 give_address_to_some_interrupt(&u);
-for(;;) { 		/* watch value of u */
-printf ("%d", u);. . . . . . . 
-In this example, if u had not been declared as volatile, the compiler could have optimized the repeated calls to printf() in such a way that u was not reexamined each time. The use of volatile forces the compiler to actually obtain the value of u whenever it is used.
- 
+
+for(;;) { 		
+    // watch value of u 
+    printf ("%d", u);
+    . . . . . . . 
+    . . . . . . . 
+
+/* In this example, if u had not been declared as volatile, 
+        the compiler could have optimized the repeated calls to printf() in such a way that 'u was not re-examined each time'. 
+        The use of volatile forces the compiler to actually obtain the value of u whenever it is used. 
+*/
+
+
+
+/* Example 4: One good time to use const is when you want to embed a 'version control number' into a program. 
+                By using a const variable to hold the version, you prevent it from accidentally being changed. 
+
+                Write a short program that illustrates how this can be done. 
+                    Use 6.01 as the version number. 
+*/
+
+#include <stdio.h>
+
+const double version = 6.01;
+
+int main(void)
+(
+printf(-Version '.2f-, version):
+return 0;
+}
+
+
+
+/* Example 5: Write your own version of 'strcpy()' called 'mystrcpy()', which has the prototype
+                                char *mystrcpy (char *to, canst char *from);
+                The function returns a pointer to "to". Demonstrate your version of mystrcpy() in a program. 
+*/
+
+'include <stdio.h>
+char *mystrcpy(char *to, const char *from);
+int main(void)
+{
+    chlr 'p, str(80],
+
+    P = mystrcpy(STR, *TESTING);
+printfC-'. 'S·, P. str};
+return 0:
+)
+char *mystrcpy(char *to. const char *from)
+{
+char -temp ;
+temp == to;
+while (*from) *to++ == -from++;
+*to = '\0' ; /* null termdnator */
+return temp:
+)
+
