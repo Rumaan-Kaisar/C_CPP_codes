adec0a1aec2aa4a6409ab709b7788110771f8bb8
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-15 10:10:58 +0600

    Update C_Ch7_5_1_Bitwise_OpRTR.c

--- CPP_ch_7_structure_union_advOprtrs/C_Ch7_5_1_Bitwise_OpRTR.c
+++ CPP_ch_7_structure_union_advOprtrs/C_Ch7_5_1_Bitwise_OpRTR.c
@@ -58,99 +58,141 @@
 
 
 
-I. The XOR operation has one interesting property. Given two
-values A and B, when the outcome of A XOR B is XORed with B
-a second time, A is produced. For example, thi& output
-initial value of i: 100
-i after first XOR: 21895
-i after second XOR: 100
-is produced by the following program:
+
+/* Example 1: The XOR operation has one interesting property. Given two values A and B,
+                when the outcome of (A XOR B) is XORed with B a second time, A is produced.
+
+                            i.e.    A = (A XOR B) XOR B
+                
+                Notice in the follwoing example.
+
+                        initial value of i: 100
+                        i after first XOR: 21895
+                        i after second XOR: 100
+
+                    is produced by the following program.   */
+
 #include <stdio.h>
-int main(voidl
-(
-int i;
-i = 101);
-printf("initial value of i: %d\n-, il;
-i = i ;, 21987;
-printf(-i after first XOR: %d\n", i);
-i = i A 21987;
-printf{"i after second XOR: %d\n", il;
-return 0;
-)
+int main(void) {
+    int i;
 
+    i = 100;
+    printf("initial value of i: %d\n", i);
 
-2. The following program uses a bitwise AND to display, in binary,
-the ASCII value of a character typed at the keyboard:
-#include <stdio.h>
-#include <conio.h>
+    i = i ^ 21987;
+    printf("i after first XOR: %d\n", i);
 
-int main (void)
-(
-)
-char chi
-lnt i;
-printf(MEnter a character: ");
-ch = getche();
-printf{"\n") ;
-j* display bin~ry representation *;
-for(i=128; i>oi .i=i/2)
-if(l & chI printf("l ");
-else printf("O ");
-return 0;
-'f.he program works by adjusting the value of i so that only one
-bit is set each time a comparison is made. Since the high-order
-bit in a byte represents 12B. this value is used as a starting point.
-Each time through the loop. i is halved. This causes the next
-bit position to be set and all others cleared. Thus. each time
-through the loop. a bit in eh is tested. If it is I. the comparison
-produces a true result and a I is output. Otherwise a 0 is
-displayed. This process continues until all bits have been tested
+    i = i ^ 21987;
+    printf("i after second XOR: %d\n", i);
 
-
-
-3. By modifying the program from Example 2. it can be used to
-show the effect of the 1's complement operator.
-#include <stdio.h>
-#include <conio.h>
-int main (void)
-(
-char Chi
-int i;
-ch = 'a';
-/*. ?isplay binary representation ~ I
-for!i=128; i>O; i=i/2)
-ifli & ch) printfl"l ");
-else printf("Q ");
-j * reverse bit patt~rn */
-
-ch = -Chi
-printf("\n") ;
-/ * display binary representation */
-for(i=128: i>Oi i=i/2)
-if(i & chI printf("l .);
-else printf{"O .);
-return 0;
-, 1.5 USE C'S BITWISE OPERATORS
-When you run this program, you will see that the state of bits in
-ch are reversed after the N operation has occurred.
-4. The following program shows how to use the [5 operator to
-determine if a signed integer is positive or negative. (The
-program assumes short integers are 16 bits long.) Since negative
-numbers are represented with their high-order bit set, the
-comparison will be true only if i is negative. (The value 32768 is
-the value of an unsigned short integer when only its high-order
-bit is set. This value is \000 0000 in binary.)
-#include <stdio.h>
-iot main (void)
-(
-short i;
-printf("Enter a number: "I;
-scanf("%hd", &i);
-•
-if(i & 32768) printf("Number is negative.\n M ):
-return 0;
+    return 0;
 }
-5. The following program makes i into a negative number
+
+
+
+
+/* Example 2: The following program uses a 'bitwise AND' to display, in binary,
+                the ASCII value of a character typed at the keyboard:                 
+
+                The program works by 'adjusting the value of i' so that only one bit is set each time a comparison is made.
+                    Since the high-order bit in a byte represents 128, this value is used as a starting point.
+                    Each time through the loop, i is halved.
+                    This causes the next bit position to be set and all others cleared.
+
+                128 in Binary is 1000 0000
+                    ASCII uses seven-digit binary numbers—i.e., numbers consisting of various sequences of 0's and 1's. 
+                    Since there are 128 different possible combinations of seven 0's and 1's, the code can represent 128 different characters
+
+                Thus, each time through the loop, a bit in 'ch' is tested. 
+                    If it is 1, the comparison produces a true result and a 1 is output. 
+                    Otherwise a 0 is displayed. 
+
+                This process continues until all bits have been tested                */
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(void) {
+    char ch;
+    int i;
+
+    printf("Enter a character: ");
+    ch = getche();
+
+    printf("\n");
+
+    // display binary representation
+    for(i=128; i>0; i=i/2){
+        if(i & ch) printf("1 ");    // using Bitwise AND
+        else printf("O ");
+    }
+
+    return 0;
+}
+
+
+
+
+/* Example 3: By modifying the program from Example 2. it can be used to show the effect of the 1's complement operator. 
+                When you run this program, you will see that the state of bits in
+                ch are reversed after the '~' (1's compliment) operation has occurred.*/
+
+#include <stdio.h>
+#include <conio.h>
+
+int main(void){
+    char ch, rvch;
+    int i;
+
+    ch = 'a';
+
+    // display binary representation
+    for(i=128; i>0; i=i/2){
+        if(i & ch) printf("1 ");    // using Bitwise AND
+        else printf("O ");
+    }
+
+    // reverse bit pattern: use of '~'
+    rvch = ~ch;
+    printf("\n");
+
+    // display binary representation of reverse bits
+    for(i=128; i>0; i=i/2){
+        if(i & rvch) printf("1 ");    // using Bitwise AND
+        else printf("O ");
+    }
+
+    return 0;
+}
+
+
+
+
+/* Example 4: The following program shows how to use the '&' operator to determine if a signed integer is positive or negative. 
+                (The program assumes short integers are 16 bits long.) 
+                
+                Since negative numbers are represented with their high-order bit set, the comparison will be true only if 'i' is negative. 
+                    (The value 32768 is the value of an 'unsigned short integer' when only its high-order bit is set. 
+                        This value is 1000 0000 0000 0000 in binary.) */
+
+#include <stdio.h>
+
+iot main(void){
+    short i;
+    printf("Enter a number: ");
+    scanf("%hd", &i);
+
+    if(i & 32768) printf("Number is negative.\n " );
+
+    return 0;
+}
+
+
+
+
+
+
+Example 5: The following program makes i into a negative number
 by setting its high-order bit. (Again, 16-bit short integers
 are assumed.)
 #include <stdio.h>
@@ -167,7 +209,9 @@ It displays - 32 ,767
 
 
 
-1. One very easy way to encode a file is to reverse the state of each
+
+
+Example 1: One very easy way to encode a file is to reverse the state of each
 bit using the N operator. Write a program that encodes a file
 using this method. (To decode the file, simply run the program
 a second time.) Have the user specify the name of the file on
@@ -203,7 +247,9 @@ return 0;
 
 
 
-2. A better method of coding a file uses the XOR operation
+
+
+Example 2: A better method of coding a file uses the XOR operation
 combmed wah a user-defined key. Write a program that
 encodes a file using this method. Have the user speCJfy the file
 to code as "'ell as a single character key on the command line.
@@ -240,7 +286,9 @@ return 0;
 
 
 
-3. What is the outcome of these operations?
+
+
+Example 3: What is the outcome of these operations?
 A 1010 001 J [< OIOJ J JOJ
 B OIOJ 1I0J I J Il J lOll
 C. OIOJ 01 JO A IOJO 101 J
@@ -251,7 +299,9 @@ c. lUI 1101
 
 
 
-4. Sometimes, the high-order bit of a byte is used as a panty bIt by
+
+
+Example 4: Sometimes, the high-order bit of a byte is used as a panty bIt by
 modem programs. It is used to verify the integrity of each byte
 transferred. There are two types of parity: even and odd. If even
 parity is used, the parity bit is used to ensure that each byte has
