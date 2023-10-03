b0ae11ce44003ac4ff98e9df2af7dcdfaef41f54
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-03 15:08:53 +0600

    Update C_Ch8_4_bultin_MACROS.c

---
+++ CPP_ch_8_preprocessor_advanced/C_Ch8_4_bultin_MACROS.c
@@ -0,0 +1,19 @@
+
+-----------------    predefined MACROS    -----------------
+
+ANSI C standard has at least five predefined macro names. They are
+ 
+__LINE __
+__FILE __
+__ DATE __
+__ TIME __
+__ STDC __
+ 
+	The __LINE__ macro defines an integer value that is equivalent to the line number of the source line currently being compiled.
+	The __FILE__ macro defines a string that is the name of the file currently being compiled,
+	The __DATE _ macro defines a string that holds the current system date, The string has this general form:
+month/day/year
+	The __TIME__ macro defines a string that contains the time the compilation of a program began. The string has this general form: 
+hours:minutes:seconds
+	The __ STDC__ macro is defined as the value 1 if the compiler conforms to the ANSI standard.
+
