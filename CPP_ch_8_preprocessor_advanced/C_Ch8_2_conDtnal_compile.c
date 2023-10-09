3dc46952ca3d06d8816abef8894bc145f8f1b518
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-09 16:11:56 +0600

    Update C_Ch8_2_conDtnal_compile.c

--- CPP_ch_8_preprocessor_advanced/C_Ch8_2_conDtnal_compile.c
+++ CPP_ch_8_preprocessor_advanced/C_Ch8_2_conDtnal_compile.c
@@ -102,3 +102,248 @@
 
 */
 
+
+
+Example 1: Sometimes you will want a program's behavior to depend on
+a value ciefined within the program. Although examples that
+are both short and meaningful are hard to find, the following
+program gives the flavor of it. This program can be compiled
+to display either the ASCII character set by itself, or the full
+extended set, depending on the value of CHAR_SET. As you
+know, the ASCII character set defines characters for the values 0
+through 127. However, most computers reserve the values 128
+through 255 for foreign-language characters and mathematical
+and other special symbols. (You might want to try this program
+with CHAR_SET set to 256. You will see some very interesting
+characters')
+#include <stdio.h>
+/* define CHAR_SET as either 256 or 128 */
+y'define CHAR-SET 256
+int main (void)
+I
+int i;
+.if CHAR-SET ==256
+printf I·Displaying ASCII character set plus extensions. \n·) ;
+'else
+printfl·Displaying only ASCII character set.\n·):
+.endif
+I
+for(i=O: i<CHAR-SET: i++)
+printf(-'c·, i);
+
+return 0:
+
+
+
+
+Example 2: A good use ofjJjfclefis for imbedding debugging information
+into your programs. For example, here is a program that copies
+the contents ofone file into another:
+/* Copy a file. */
+'include <stdio.h>
+'include <stdlib.h>
+'define DEBUG
+int main(int argc, char ""argv[])
+I
+FILE ·from. ""to:
+char Chi
+'* see if correct number of command line arguments */
+iflargc!=3) I
+I
+printf(-Usage: copy <source> <destination>\n-);
+exitt1):
+'* open source file *'
+if«from = fopen(argv[11. ·rb·»==NULL) I
+printf(·Cannot open source fi1e.\n:):
+exit(1):
+I
+
+/*open destination file */
+if I Ito = fopen largv[2], "wb"» ==NULL) {
+printE (~Cannot open destination file. \n");
+exit(!);
+)
+/* copy the file */
+while ( ! feaf (from» (
+ch = fgetc (from) ;
+if (ferror (from» (
+)
+princf("Error reading source file.\n");
+exit(!);
+if(!feof(from» {
+fpute (ch, to);
+#ifdef DEBUG
+putchar (ch);
+#endif
+)
+,
+,
+)
+if(ferror(to»)- {
+)
+printE ("Error writing destination file. \0") ;
+exit(!):
+fclose (trom) ;
+fclose(to) ;
+return 0;
+If DEBUG is defined, the program displays each byte as it is
+transferred. This can be helpful during the development phase.
+Once the program is finished, the statement defining DEBUG is
+removed, and the output is not displayed. However, if the
+program ever misbehaves in the future, DEBUG can be defin~d
+again, and output will again be shown on the screen. While this
+might seem like a lot of work for such a simple program, in
+actual practice programs may have many debugging statements,
+and this procedure can greatly facilitate the development and
+testing cycle.
+
+As shown in this program, to simply define a macro name,
+you do not have /0 associate any character sequence with it
+
+
+
+
+
+Example 3: Continuing with the debugging th~me, it is possible to use the
+Hif to allow several levels ofdebugging code to be easily managed.
+For·example, here is one of the encryption programs from the
+answers to Chapter II that supports three debugging levels:
+#include <stdio.h>
+#include <std!ib.h>
+/ * DEBUG leve ls:
+0/
+0: no debug
+1. d".splay byte read flom source file
+2. d~.s play byte wr~tten to destination file
+3: display bytr::s read and hyt.es written
+ijdefin-::) DEBUG ::
+int mainline arqc, char *argv[] I
+(
+FILE *in, "o~r:;
+unsigned char chi
+j* see if correct number of command line arguments */
+if(argc!.::4) [
+)
+printf (·Usage: code <in> <out> <key> ~) ;
+exit (1);
+I~ open input file */
+if{(in = fopen{arg'l[l] "rb"»==NULL) {
+)
+printf("Cannot open ~pput file.\n")i
+exit(!);
+/w open output file */
+if«out = fopen(argv[2}, "wh") )==NULL) (
+printf("Cannot open output file.\n~ ) ;
+exit(l);
+)
+while(!feof(in» {
+
+ch = fgetc (in) ;
+THE C PREPROCESSOR AND SOME ADVANCED TOPICS
+12,2 UNDERSTAND CONDITIONAL COMPILAnON
+DEBUG == 1 I I DEBUG == 3
+putchar feb) ;
+Itendif
+ch = *argv(31 A chi
+/tif DEBUG >:: 2
+putchar (eh) ;
+#endif
+)
+if(!feof(in» fputc(ch, out);
+)
+fclose (in) ;
+fclose(outl;
+return 0;
+
+
+
+
+
+Example 4: The following fragment illustrates the "elif. It displays NUM I S
+2 on the screen.
+#define NUM 2
+tiE NOM == 1
+printf("NUM is 1");
+#ellf NUM == 2
+printf ("NUM is 2");
+#elif NOM == 3
+printf{"NUM is )N);
+#eliE NOM == 4
+printf(~NUM is 4");
+#endif
+
+
+
+
+Example 5: Here, the defined operator is used to determine if
+TESTPROJECT is defined.
+'include <stdio.h>
+#define TESTPROJECT 29
+iif defined TESTPROJECT
+int main (void)
+(
+printf(~This is a test.\n");
+
+I'"eturn .0 :- __
+}
+#endi f
+
+
+
+
+Example 6: Write a program that defines three macros called INT, FLOAT,
+and PWR_TYPE. Define INT as 0, FLOAT as J, and
+PWR_TYPE as either INT or FLOAT. Have the program
+request two numbers from the user and display the result of
+the first number raised to the second number. Using #if and
+depending upon the value of PWR_TYPE, have both numbers
+be integers, or allow the first number to be a double.
+
+
+1. #include <stdio.h>
+#define INT 0
+#define FLOAT 1
+_define PWR_TYPE INT
+int main(void)
+(
+int e:
+#if PWR_TYPE==FLOAT
+double base. result;
+#elif PWR_TYPE==INT
+int base, result;
+lendif
+.if PWR_TYPE==FLOAT
+printf("Enter floating point base: ");
+scanf('%lf', &base);
+
+#elif PWR_TYPE==INT
+printf{·Enter integer base: .):
+scanf ("'d", &base);
+iendif
+printf("Enter integer exponent (greater than 0): .);
+scanf ("'d", &e);
+result = 1;
+fore; e; e--)
+result = result • base:
+#if PWR_TYPE==FLOAT
+printf("Result: %f-, result);
+#elif PWR_TYPE==INT
+printf(-Result: %d-, result);
+tendif
+return 0;
+}
+
+
+
+Example 7: Is this fragment correct' If not, show one ,;'ay to fix it.
+
+#define MIKE
+#ifdef !MIKE
+#endif
+No. You cannot use an expression like IMIKE with #ifdef. Here
+are two possible solutions.
+*i fndef MIKE
+#endif
+/* or */
+#if !defined MIKE
+tend!f
