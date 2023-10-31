aaa6712f74925f4ad028635b625ce587e2b7b17a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-31 15:05:06 +0600

    Update null.cpp

--- CPP_ch_8_preprocessor_advanced/null.cpp
+++ CPP_ch_8_preprocessor_advanced/null.cpp
@@ -1,6 +1,112 @@
-#include <iostream>
 
-int main(){
 
-    return 0;
-}
+
+
+// -------  rev  ----------
+/* --------    mastery    -------- */
+
+At this point yoti should be able to answer these qttestions and
+perform these exercises:
+
+
+Example 1: What is the difference between using quotes and angle brackets
+\vith the #include directive?
+
+
+Example 2: Using an #ifdef, show how to conditionally compile this
+fragment of code hased upon whether DEBUG is defined or not.
+if l ! (j %2)) {
+)
+print.f("J = %d\n", J);
+J = 0;
+
+
+Example 3:L'smg the tragmcm from ExerCIse 2, show how you can
+conditionallv compile the cncie when DEBUG is defined as 1.
+(Hint: Use #if).
+-I. Hov,' do VOll undefine a macro?
+
+
+Example 5: \"lhat is __FILE__ and what does it repre!:.f'nt:'
+
+
+6. What do the # and ## preprocessor operators do:'
+
+
+Example 7: Write a prugr<lm that sorts the string "this is a test ofqsort".
+Display the sorted output.
+
+
+Example 8: Write a program that dynamically allocates mem(lfY for one
+double. Have the program assign that locanon the value ~9.01,
+display the value, and then free the memory
+
+
+
+
+
+"
+/* --------    cumul    -------- */
+
+This section checks how well you have integrated the material in
+this ch<lptcr with that from "<lrlier ch<lpters.
+
+Example 1: Section 10.1, Example 3, presents .1 computerized card-catalog
+program tllat liSt'S an array \)f structures to hold infornlation on
+books. Change this program so that only an array of structure
+
+pointers is created, and use dynamically allocated memory to
+actually hold the information for each book as it is entered. This
+way, less memory is used when information on only a few
+books is stored.
+
+
+
+Example 2: Show the macro eqUivalent of this function;
+char code_it(char c)
+(
+return -Ci
+)
+Demonstrate that your macro version works in a program.
+,
+
+
+
+Example 3: On your own, look over the programs that you have written in
+the course of working through this book. Try to find places
+where you can;
+.. Use conditional compilation.
+• Replace a short function with a f~tion.like macro.
+.. Replace statically allocated arrays with dynamic arrays.
+... Use function pointers.
+
+
+
+Example 4: On your own, study the user's manual or online documentation
+for your C compiler, paying special attention to the description
+of its standard library functions. The C standard library contains
+several hundred library functions that can make your
+programming tasks easier. Also, Appendix A in this book
+discusses some of the most common library functions.
+
+
+
+Example 5: Now that you have finished this book, go back and skim through
+each chapter, thinking about how each aspect of C relates to the
+rest of it. As you will see, C is a highly integrated language, in
+which one feature complements another. The connection
+between pointers ahd arrays, for example, is pure elegance.
+
+
+
+Example 6: C is a language best learned by doing! Continue to write
+programs in C and to study other programmers' programs. You
+will be surprised at how quickly C will become second nature!
+
+
+
+7. Finally, you now have the necessary foundation in C to a1\ow
+you to move on to C++;C'sobject-orientedextension. If C++ .
+programming is in your future, proceed to Teach Yoursel{C++,
+(Berkeley, CA, Osborne/McGraw-Hill). It picks up where this
+book leaves off.
