6d70c88e9073c57bb8cd9be7920ddc5f0ae90ef9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-14 03:02:00 +0600

    Update C_Ch0_5_dyn_alloc.c

--- CPP_ch_0_useful_lib_func/C_Ch0_5_dyn_alloc.c
+++ CPP_ch_0_useful_lib_func/C_Ch0_5_dyn_alloc.c
@@ -30,25 +30,64 @@ void *calloc(size_t num, size_t size);
 
             p = calloc(100, sizeof(float));
             if(!p) {
-
+                printf("Allocation error - aborting.\n");
+                exit(1);
             }
-            printf("Allocation error - aborting.\n");
-            exit(1);
+
 
             return p;
         }
 */
 
 
-void free( void *ptr)
-free() de-allocates the memory pointed to by ptr. It is called only with a pointer that was previously allocated using malloc() or calloc() etc. Invalid pointer destroy the memory management mechanism and cause a system crash.	for(i=0; i<100; i++) 
-free(str[i]);
-void *malloc(size_t size);	malloc() returns a pointer to the first byte of a region of memory of size size that has been allocated from the heap. (Remember, the heap is a region of free memory managed by C‘s dynamic allocation subsystem.) A null pointer is returned if there is insufficient memory in the heap.  Always verify that the return value is not a null pointer before attempting to use it. null pointer will usually result in a system crash.	if((p = malloc(sizeof(struct addr)))==NULL)
+
+void free(void *ptr)
+/* free() de-allocates the memory pointed to by 'ptr'. 
+    It is called only with a pointer that was previously allocated using malloc() or calloc() etc. 
+    "Invalid pointer" destroy the memory management mechanism and cause a 'SYSTEM CRASH'.
+
+    Example: Notice we need two for-loop to allocate and  de-allocate the memory.
+                It first allocates room for 100 user-entered strings and then frees them.
+
+        #include <stdlib.h>
+        #include <stdio.h>
+
+        int main(void) {
+            char *str[100];
+            int i;
+
+            for(i=0; i<100; i++) {
+                // allocating and cheking simultaniously
+                if((str[i] = malloc(128))==NULL) {
+                    printf("Allocation error - aborting.\n");
+                    exit(1);
+                }
+
+                gets(str[i]) ;
+            }
+
+            // now free the memory
+            for(i=0; i<100; i++) free(str[i]);
+
+            return 0;
+        }
+*/
+
+
+
+
+
+void *malloc(size_t size);
+malloc() returns a pointer to the first byte of a region of memory of size size that has been allocated from the heap. (Remember, the heap is a region of free memory managed by C‘s dynamic allocation subsystem.) A null pointer is returned if there is insufficient memory in the heap.  Always verify that the return value is not a null pointer before attempting to use it. null pointer will usually result in a system crash.	if((p = malloc(sizeof(struct addr)))==NULL)
     {
           printf("Allocation error - aborting.\n");
           exit(0); 
      }
-void *realloc(void *ptrt size_t size);	realloc() changes the size of the allocated memory pointed to by ptr to that specified by size. size may greater or less than the original. A pointer to the memory block is returned since it may be necessary for realloc() to move the block to increase its size. Contents of the old block are copied into the new block—no information is lost. A null pointer is returned if there is not enough free memory in the heap. Verify the success of real1oc().	char *p;     p = malloc(17);
+
+
+
+void *realloc(void *ptrt size_t size);
+realloc() changes the size of the allocated memory pointed to by ptr to that specified by size. size may greater or less than the original. A pointer to the memory block is returned since it may be necessary for realloc() to move the block to increase its size. Contents of the old block are copied into the new block—no information is lost. A null pointer is returned if there is not enough free memory in the heap. Verify the success of real1oc().	char *p;     p = malloc(17);
 if(!p) {   printf("Alloc error");   exit(1);
             }
 strcpy(p, "this is 16 chars');
