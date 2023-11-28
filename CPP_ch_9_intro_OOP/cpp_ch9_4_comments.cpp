3e39b709dde38422e0b494ab09ad207322f62e1d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-11-28 11:34:50 +0600

    Update cpp_ch9_4_comments.cpp

---
+++ CPP_ch_9_intro_OOP/cpp_ch9_4_comments.cpp
@@ -0,0 +1,6 @@
+9.4 C++ comments
+In C++, you can include comments in your program two different ways. 
+	First, you can use the standard, C-like comment mechanism. That is, begin a comment with /* and end it with */. As with C, this type of comment cannot be nested in C++.
+	The second way that you can add a remark to your C++ program is to use the single-line comment. A single-line comment begins with a // and stops at the end of the line. 
+	Typically, C++ programmers use C-like comments for multiline commentaries and reserve C++ style single line comments for short remarks.
+
