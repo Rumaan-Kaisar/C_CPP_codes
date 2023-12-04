3592aec2ecc508281e65cf40b3320e24d57d4264
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-04 17:49:18 +0600

    Update cpp_ch9_3_func_ovrld.cpp

--- CPP_ch_9_intro_OOP/cpp_ch9_3_func_ovrld.cpp
+++ CPP_ch_9_intro_OOP/cpp_ch9_3_func_ovrld.cpp
@@ -1,29 +1,40 @@
 
-// ----     rev    ----
-
 /* ----------------------     Function overloading (intro)     ----------------------
 
+    Function overloading:
+        In C++ function overloading is one type of POLYMORPHISM (compile-time polymorphism). 
+            When two or more functions share the same name, they are said to be overloaded.
 
-Function overloading:
-    In C++ function overloading is one type of POLYMORPHISM (compile-time polymorphism). 
-        When two or more functions share the same name, they are said to be overloaded.
+        In C++, two or more functions can share the 'same name' as long as either:
+            the "TYPE of their arguments" differs or 
+            the "number of their arguments" differs
+            or both
+    
+        Overloaded functions can help reduce the complexity of a program by 
+            allowing 'related operations' to be referred to by the same name.
 
-    In C++, two or more functions can share the 'same name' as long as either:
-        the "TYPE of their arguments" differs or 
-        the "number of their arguments" differs
-        or both
-    
-    
 
-    
-	Overloaded functions can help reduce the complexity of a program by allowing related operations to be referred to by the same name.
-	It is very easy to overload a function: simply declare and define all required versions. The compiler will automatically select the correct version based upon the number and/or type of the arguments used to call the function.
-	It is also possible in C++ to overload operators. 
-	The return type alone is not a sufficient difference to allow function overloading. If two functions differ only in the type of data they return, the compiler will not always be able to select the proper one to call. For example, this fragment is incorrect because it is inherently ambiguous. 		int f1(int a);
-double f1( int a);
-. . . . . 
-f1(10) ; 	/* which function does the compiler call ???*/ 
-As the comment indicates, the compiler has no way of knowing which version of f1() to call.
+
+    How to overload a function: 
+        simply DECLARE and DEFINE all required versions. 
+        the 'compiler' will 'automatically select' the correct version based 
+            upon the 'NUMBER' and/or 'TYPE' of the arguments used to call the function.
+        
+        it's the ARGUMENTs that make the difference. 
+
+    It is also possible in C++ to overload OPERATORS. 
+
+    return type not used to overload: The 'return type of a function'  alone is not a sufficient difference to allow function overloading. 
+        If two functions differ only in the "type of data they return", the compiler will not always be able to select the proper one to call. 
+        For example, this fragment is incorrect because it is inherently ambiguous. 		
+        
+            int f1(int a);
+            double f1( int a);
+            . . . 
+            . . .
+            f1(10) ; 	// which function does the compiler call ??? 
+
+        As the comment indicates, the compiler has no way of knowing which version of f1() to call.
 */
 
 
@@ -46,6 +57,8 @@ int main(){
     std::cout<<"Absolute of -10 : "<<abs(-10)<<"\n";
     std::cout<<"Absolute of -10L : "<<abs(-10L)<<"\n";
     std::cout<<"Absolute of -10.01 : "<<abs(-10.01)<<"\n";
+
+    return 0;
 }
 
 // abs() for ints
@@ -65,4 +78,8 @@ double abs(double n){
     cout<< "\n In double abs() = ";
     return n<0 ? -n : n;
 }
+
+// As you can see, this program defines three functions called abs()-one for each data type.
+// Inside main(), abs() is called using 'three different types of arguments'. 
+// The compiler automatically calls the correct version of abs() based upon the type of data used as an argument.
 
