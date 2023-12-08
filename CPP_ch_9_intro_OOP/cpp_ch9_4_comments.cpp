b81a085877e08afe7d1077c5608a3e6b18714610
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-12-08 14:48:44 +0600

    Update cpp_ch9_4_comments.cpp

--- CPP_ch_9_intro_OOP/cpp_ch9_4_comments.cpp
+++ CPP_ch_9_intro_OOP/cpp_ch9_4_comments.cpp
@@ -11,3 +11,48 @@ The second way that you can add a remark to your C++ program is to use the singl
 Typically, C++ programmers use 'C-like comments' for "multiline commentaries" and 
     reserve C++ style single line comments for "short remarks".
 
+
+
+
+/* Example 1: Here is a program that contains both C and C++-style comments:
+ */
+/*
+This is a C- like comment .
+This program determines whether
+an integer is odd or even
+*/
+# include <iostream >
+using namespace std ;
+int main ()
+{
+int num ; // this is a C++ single - line comment
+// read the number
+cout << " Enter number to be tested : ";
+cin >> num ;
+// see if even or odd
+if (( num %2) ==0)
+cout << " Number is even \n";
+else
+cout << " Number is odd \n";
+return 0;
+}
+
+
+
+
+/* Example 2: While multiline comments cannot be nested, it is possible to nest a single-line comment
+within a multiline comment. For example, this is perfectly valid: */
+/*
+This is a multiline comment
+inside of which // is nested a single - line comment .
+Here is the end of the multiline comment .
+*/
+
+
+
+
+/* Example 3: As an experiment, determine whether following comment (which nests a C-like comment within
+                a C++-style, single-line comment) is valid: */
+
+// This is a strange /* way to do a comment */
+
