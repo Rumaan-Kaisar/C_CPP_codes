abad98c93717a2038c18fb2fa61d208f4d445279
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-09 15:03:07 +0600

    Update ch10_01_2_constructor_destructor.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_01_2_constructor_destructor.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_01_2_constructor_destructor.cpp
@@ -198,80 +198,93 @@ void myclass :: show(){
 /* Example 3: Recall the "stack class" we created in previous section required an initialization function to set the stack index variable.
                 Constructor function was designed to perform this sort of operation. 
 
-                Following is an improved version of the 'stack class' 
+                Following is an improved version of the 'stack class' that can be used to store characters
                     that uses a constructor to 'automatically initialize' a stack object when it is created 
 */
 
-# include <iostream >
-using namespace std ;
-# define SIZE 10
+#include <iostream>
+// using namespace std;
+
+#define SIZE 10
+
+
 // Declare a stack class for characters .
-class stack
-{
-char stck [ SIZE ]; // holds the stack
-int tos ; // index of top of stack
-public :
-stack (); // constructor
-void push ( char ch); // push character on stack
-char pop (); // pop character from stack
+class stack {
+        char stck[ SIZE];   // holds the stack
+        int tos;            // index of top of stack
+    
+    public:
+        stack();            // constructor. Notice no 'void init();' required
+        void push(char ch); // push character on stack
+        char pop();         // pop character from stack
 };
-// Initialize the stack .
-stack :: stack ()
-{
-cout << " Constructing a stack \n";
-tos = 0;
-}
-// Push a character
-void stack :: push ( char ch)
-{
-if( tos == SIZE )
-{
-cout << " Stack is full \n";
-return ;
-}
-stck [ tos ] = ch;
-tos ++;
-}
-// Pop a character
-char stack :: pop ()
-{
-if( tos ==0)
-{
-cout << " Stack is empty \n";
-return 0; // return null on empty stack
-}
-tos --;
-return stck [ tos ];
-}
-int main ()
-{
-// Create two stacks that are automatically initialized .
-stack s1 , s2;
-
-int i;
-s1. push (’a’);
-s2. push (’x’);
-s1. push (’b’);
-s2. push (’y’);
-s1. push (’c’);
-s2. push (’z’);
-for (i =0; i <3; i ++)
-cout << " Pop s1: " << s1.pop () << "\n";
-for (i =0; i <3; i ++)
-cout << " Pop s2: " << s2.pop () << "\n";
-return 0;
-}
-As you can see, now the initialization task is performed automatically by the constructor
-function rather than by a separate function that must be explicitly called by the program.
-This is an important point. When an initialization is performed automatically when an
-object is created, it eliminates any prospect that, by error, the initialization will not be
-performed. This is another way that objects help reduce program complexity. You, as the
-programmer, don’t need to worry about initialization-it is performed automatically when
-the object is brought into existence.
 
 
+// -=-=-=-=-=-    implementing member function    -=-=-=-=-=-
+
+// stack(): Initialize the stack. No 'void init();' required
+    // implementing constructor function
+stack::stack(){
+    std::cout << " Constructing a stack \n";
+    tos = 0;
+}
+
+// push(): Push a character
+void stack::push(char ch){
+    if(tos == SIZE){
+        std::cout << " Stack is full \n";
+        return;
+    }
+    stck[tos] = ch;
+    tos++;
+}
+
+// pop(): Pop or remove a character
+char stack::pop(){
+    if(tos == 0){
+        std::cout << " Stack is empty \n";
+        return 0; // return null on empty stack
+    }
+    tos--;
+    return stck[tos];
+}
 
 
+int main(){
+    stack s1, s2; // create two stacks that are automatically initialized.
+    int i;
+
+    // No need to initialize the stacks
+    // s1.init();  // no need 
+    // s2.init();  // no need
+
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+
+    for(i =0; i<3; i ++) std::cout << " Pop s1: " << s1.pop() << "\n";
+    for(i =0; i<3; i ++) std::cout << " Pop s2: " << s2.pop() << "\n";
+
+    return 0;
+}
+
+
+/* 
+    Notice, 'the initialization task is performed automatically' by the constructor function 
+        rather than by a separate function that must be explicitly called by the program.
+
+    Constructor function makes sure that the inilialization is performed.
+    The programmer, don’t need to worry about initialization-it is performed automatically when the object is brought into existence.
+ */
+
+
+
+
+
+// ----  rev  ----
 
 /* Example 4: Following program need both "constructor" and "destructor" function.
                 It creates a simple string class, called 'strtype', that contains a 'string and its length'. 
