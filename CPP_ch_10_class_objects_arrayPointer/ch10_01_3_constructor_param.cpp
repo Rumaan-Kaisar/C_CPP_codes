301ccb8e81be0de7a3be2fbb8158412881fe347f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-18 15:08:40 +0600

    Update ch10_01_3_constructor_param.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_01_3_constructor_param.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_01_3_constructor_param.cpp
@@ -124,91 +124,91 @@ int main(){
 
 
 
+/* Example 3: Here is another version of the 'stack class' that uses a parameterized constructor to pass a "name" to a stack. 
+                This single-character name is used to 'identify the stack' that is being referred to when an error occurs. 
 
+                Tips for DEBUGGING: Giving objects a "name," as shown in this example, is especially useful during DEBUGGING,
+                    when it is important to know which object generates an error.                
+*/
+#include <iostream>
 
+#define SIZE 10
 
----- 
-
-
-Example 3: Here is another version of the stack class that uses a parameterized constructor to pass a
-"name" to a stack. This single-character name is used to identify the stack that is being
-referred to when an error occurs.
-# include <iostream >
-using namespace std ;
-# define SIZE 10
 // Declare a stack class for characters .
-class stack
-{
-char stck [ SIZE ]; // holds the stack
-int tos ; // index of top of stack
-char who ; // identifies stack
-public :
-stack ( char c); // constructor
-void push ( char ch); // push character on stack
-char pop (); // pop character from stack
+class stack {
+        char stck[SIZE];    // holds the stack
+        int tos;            // index of top of stack
+        char who;           // identifies stack
+    
+    public:
+        stack(char c);       // constructor. Notice the parameter
+        void push(char ch); // push character on stack
+        char pop();         // pop character from stack
 };
-// Initialize the stack .
-stack :: stack ( char c)
-{
-tos = 0;
-who = c;
-cout << " Constructing stack " << who << "\n";
+
+
+// Initialize the stack. Use of parameter
+stack::stack(char c){
+    tos = 0;
+    who = c;    // Use of parameter
+    std::cout << " Constructing stack " << who << "\n";
 }
-// Push a character .
-void stack :: push ( char ch)
-{
-if( tos == SIZE )
-{
-cout << " Stack " << who << " is full \n";
-return ;
+
+// push(): Push a character
+void stack::push(char ch){
+    if(tos == SIZE){
+        // Error massage
+        std::cout << " Stack " << who << " is full \n";
+        return;
+    }
+    stck[tos] = ch;
+    tos++;
 }
-stck [ tos ] = ch;
-tos ++;
+
+// pop(): Pop or remove a character
+char stack::pop(){
+    if(tos == 0){
+        // Error massage
+        std::cout << " Stack " << who << " is empty \n";
+        return 0; // return null on empty stack
+    }
+    tos--;
+    return stck[tos];
 }
-// Pop a character .
-char stack :: pop ()
-{
-if( tos ==0)
-{
-cout << " Stack " << who << " is empty \n";
-return 0; // return null on empty stack
+
+
+// -=-=-  main function  -=-=-
+int main(){
+    stack s1('A'), s2('B'); // create two stacks that are automatically initialized. Notice parameters
+    int i;
+
+    s1.push('a');
+    s2.push('x');
+    s1.push('b');
+    s2.push('y');
+    s1.push('c');
+    s2.push('z');
+
+    // This will generate some error messages. Used 'i=5' instead of 'i=3'
+    for(i =0; i<5; i ++) std::cout << " Pop s1: " << s1.pop() << "\n";
+    for(i =0; i<5; i ++) std::cout << " Pop s2: " << s2.pop() << "\n";
+
+    return 0;
 }
-39TEACH YOURSELF
-C++
-tos --;
-return stck [ tos ];
-}
-int main ()
-{
-// Create two stacks that are automatically initialized .
-stack s1(’A’), s2(’B’);
-int i;
-s1. push (’a’);
-s2. push (’x’);
-s1. push (’b’);
-s2. push (’y’);
-s1. push (’c’);
-s2. push (’z’);
-// This will generate some error messages .
-for (i =0; i <5; i ++)
-cout << " Pop s1: " << s1.pop () << "\n";
-for (i =0; i <5; i ++)
-cout << " Pop s2: " << s2.pop () << "\n";
-return 0;
-}
-Giving objects a "name," as shown in this example, is especially useful during debugging,
-when it is important to know which object generates an error.
 
 
 
 
 
-Example 4: Here is a different way to implement the strtype class (developed earlier) that uses a
-parameterized constructor function:
+
+// ----  rev  ----
+/* Example 4: Here is a different way to implement the 'strtype class' (developed earlier) 
+                that now uses a parameterized constructor function. */
 # include <iostream >
 # include <cstring >
 # include <cstdlib >
-using namespace std ;
+
+
 class strtype
 {
 char *p;
