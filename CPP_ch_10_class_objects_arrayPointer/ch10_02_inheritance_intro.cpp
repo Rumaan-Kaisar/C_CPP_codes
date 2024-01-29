bcb682ddf55e165e31f2872bea1b9e2054ceab43
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-01-29 14:39:22 +0600

    Update ch10_02_inheritance_intro.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_02_inheritance_intro.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_02_inheritance_intro.cpp
@@ -15,37 +15,131 @@
             BASE class defines all qualities that will be 'common' to any DERIVED classes.
             BASE class represents the 'most general description' of a set of traits. 
         A DERIVED class inherits those general traits from BASE class and 'adds properties' that are 'specific' to that class.
+
+    
+    GENERAL FORM:
+        The general form used to inherit a base class is shown here:
+
+                class derived_class_name : access_specifier base_class_name {
+                            . . . 
+                            . . .  
+                };
+
+        Here "access-specifier" is one of these three keywords: 
+                public, 
+                private, or 
+                protected. 
 */
 
 
-// ----  rev (23-jan-24)  ----
 
-/* Example 1: Example of base class and derived class 
 
-*/
+/* Example 1: Example of base class and derived class  */
 // The declaration & definition for the BASE class	
-class Bs {	int i;
-		public : void set_i(int n);
-		int get_i();  };		
+class Bs {	
+        int i;
+	public : 
+        void set_i(int n);
+	    int get_i();  
+};		
 
 // Using base class, here is a derived class that inherits it:        
         // Define derived class .
-class Drv : public Bs { int j;
-			public : void set_k(int n);
-				  int mul();   };
-
-
+class Drv : public Bs { 
+        int j;
+    public : 
+        void set_k(int n);
+        int mul();   
+};
 
 /*
-	Notice that after the class name Drv there is a colon followed by the keyword public and the class name Bs. This tells the compiler that class Drv will inherit all components of class Bs. 
-	They keyword public tells the compiler that Bs will be inherited such that all public elements of the base class will also be public elements of the derived class. However, all private elements of the base class remain private to it and are not directly accessible by the derived class.
-	Derived class can call base class public member functions directly. For example consider the following function. 
-int :: mul(){	return j*get_i(); }
-Notice that it calls get_i(), which is a member of the base class Bs, not of Drv, without linking it to any specific object (without base class it won't be possible). 
-However, the reason that mul() must call get_i() instead of accessing i directly is that the private members of a base class (in this case,i) remain private (due to encapsulation) to it and not accessible by any derived class. 
-	The general form used to inherit a base class is shown here:
-class derived_class_name : access_specifier base_class_name {
-. . . . . . . };
-Here access-specifier is one of these three keywords: public, private, or protected. 
+    Notice that after the class name 'Drv' there is a colon ':' bofore the keyword 'public' and the class name 'Bs'. 
+        This tells the compiler that class 'Drv' will INHERIT all components of class 'Bs'. 
+
+    They keyword public tells the compiler that Bs will be inherited such that all public elements of the base class will also be public elements of the derived class. However, all private elements of the base class remain private to it and are not directly accessible by the derived class.
+
+*/
+
+
+
+
+/*  ----------------------     Inheritance ACCESS MODES: C++ Public, Protected and Private Inheritance     ----------------------
+    In C++ inheritance, we can derive a child class from the base class in different 'ACCESS MODES'. For example,
+
+        class Base {
+            .... 
+            .... 
+            ....
+        };
+
+        class Derived : public Base {
+            .... 
+            .... 
+            ....
+        };
+
+
+        Notice the keyword 'public' in the code:
+
+                class Derived : public Base
+
+            This means that we have created a derived class from the base class in 'public mode'. 
+                Alternatively, we can also derive classes in 'protected' or 'private' modes.
+
+            These 3 keywords (public, protected, and private) are known as access specifiers in C++ inheritance.
+
+
+
+    public, protected, and private inheritance have the following features:
+
+        public:
+            public inheritance makes 'PUBLIC members' of the base class "public" in the derived class, and 
+                the PROTECTED members of the base class "remain protected" in the derived class.
+
+        protected:
+            protected inheritance makes the 'PUBLIC and PROTECTED members' of the base class "protected" in the derived class.
+
+        private:
+            private inheritance makes the 'public and protected members' of the base class "private" in the derived class.
+
+    Note: 'private members' of the base class are inaccessible to the derived class.
 
  */
+
+// Inheritance ACCESS MODES: C++ Public, Protected and Private Inheritance
+class Base {
+    public:
+        int x;
+    protected:
+        int y;
+    private:
+        int z;
+};
+
+class PublicDerived: public Base {
+    // x is public
+    // y is protected
+    // z is not accessible from PublicDerived
+};
+
+class ProtectedDerived: protected Base {
+    // x is protected
+    // y is protected
+    // z is not accessible from ProtectedDerived
+};
+
+class PrivateDerived: private Base {
+    // x is private
+    // y is private
+    // z is not accessible from PrivateDerived
+};
+
+
+
+
+----  rev  ----
+
+/* 	Derived class can call base class public member functions directly. For example consider the following function. 
+int :: mul(){	return j*get_i(); }
+Notice that it calls get_i(), which is a member of the base class Bs, not of Drv, without linking it to any specific object (without base class it won't be possible). 
+However, the reason that mul() must call get_i() instead of accessing i directly is that the private members of a base class (in this case,i) remain private (due to encapsulation) to it and not accessible by any derived class.  */
