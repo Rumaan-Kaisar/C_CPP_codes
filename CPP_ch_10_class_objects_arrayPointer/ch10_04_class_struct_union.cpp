3e4753d684aceddc174830ca355175338e5ae273
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-08 07:37:08 +0600

    Update ch10_04_class_struct_union.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_04_class_struct_union.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_04_class_struct_union.cpp
@@ -109,3 +109,303 @@
 
 
 
+Example 1: Here is a short program that uses struct to create a class:
+# include <iostream >
+# include <cstring >
+using namespace std ;
+// use struct to define a class type
+struct st_type
+{
+st_type ( double b, char *n);
+void show ();
+private :
+double balance ;
+char name [40];
+};
+st_type :: st_type ( double b, char *n)
+{
+balance = b;
+strcpy (name , n);
+}
+void st_type :: show ()
+{
+cout << " Name : " << name ;
+cout << ": $" << balance ;
+if( balance <0.0)
+cout << "**";
+cout << "\n";
+}
+int main ()
+{
+st_type acc1 (100.12 , " Johnson ");
+st_type acc2 ( -12.34 , " Hedricks ");
+acc1 . show ();
+acc2 . show ();
+return 0;
+}
+Notice that, as stated, the members of a structure are public by default. The private
+keyword must be used to declare private members.
+Also, notice one difference between C-like structures and C++-like structures. In C++,
+the structure tag-name also becomes a complete type name that can be used to declare
+objects. In C, the tag-name requires that the keyword struct precede it before it becomes
+a complete type.
+
+
+
+
+Example 2: Here is the same program, rewritten using a class:
+
+# include <cstring >
+using namespace std ;
+class cl_type
+{
+double balance ;
+char name [40];
+public :
+cl_type ( double b, char *n);
+void show ();
+};
+cl_type :: cl_type ( double b, char *n)
+{
+balance = b;
+strcpy (name , n);
+}
+void cl_type :: show ()
+{
+cout << " Name : " << name ;
+cout << ": $" << balance ;
+if( balance <0.0)
+cout << "**";
+cout << "\n";
+}
+int main ()
+{
+cl_type acc1 (100.12 , " Johnson ");
+cl_type acc2 ( -12.34 , " Hedricks ");
+acc1 . show ();
+acc2 . show ();
+return 0;
+}
+
+
+
+Example 3: Here is an example that used a union to display the binary bit pattern, byte by byte,
+contained within a double value.
+# include <iostream >
+using namespace std ;
+union bits
+{
+bits ( double n);
+void show_bits ();
+double d;
+unsigned char c[ sizeof ( double )];
+};
+52INTRODUCING CLASSES
+2.5. CLASSES, STRUCTURES, AND UNIONS ARE RELATED
+bits :: bits ( double n)
+{
+d = n;
+}
+void bits :: show_bits ()
+{
+int i, j;
+for (j = sizeof ( double ) -1; j >=0; j --)
+{
+cout << " Bit pattern in byte " << j << ": ";
+for (i = 128; i; i >>= 1)
+if(i & c[j])
+cout << "1";
+else
+cout << "0";
+cout << "\n";
+}
+}
+int main ()
+{
+bits ob (1991.829) ;
+ob. show_bits ();
+return 0;
+}
+
+
+
+Example 4: Both structures and unions can have constructors and destructors. The following example
+shows the strtype class reworked as a structure. It contains both a constructor and a
+destructor function.
+# include <iostream >
+# include <cstring >
+# include <cstdlib >
+
+using namespace std ;
+struct strtype
+{
+strtype ( char * ptr );
+~ strtype ();
+void show ();
+private :
+char *p;
+int len ;
+};
+strype :: strype ( char * ptr )
+{
+len = strlen ( ptr );
+p = ( char *) malloc (len +1);
+if (!p)
+{
+cout << " Allocation error \n";
+exit (1) ;
+}
+strcpy (p, ptr );
+}
+strtype ::~ strtype ()
+{
+cout << " Freeing p\n";
+free (p);
+}
+void strtype :: show ()
+{
+cout << p << " - length : " << len ;
+cout << "\n";
+}
+int main ()
+{
+strtype s1(" This is a test ."), s2("I like C++");
+s1. show ();
+s2. show ();
+return 0;
+}
+
+
+Example 5: This program uses an anonymous union to display the individual bytes that comprise a
+double. (This program assumes that doubles are 8 bytes long.)
+// Using an anonymous union .
+# include <iostream >
+using namespace std ;
+
+int main ()
+{
+union
+{
+unsigned char bytes [8];
+double value ;
+};
+int i;
+value = 859345.324;
+// display the bytes within a double
+for (i =0; i <8; i ++)
+cout << ( int ) bytes [i] << " ";
+return 0;
+}
+As you can see, both value and bytes are accessed as if they were normal variables and
+not part of a union. Even though they are declared as being part of an anonymous union,
+their names are at the same scope level as any other local variable declared at the same
+point. This is why a member of an anonymous union cannot have the same name as any
+other variable known to its scope.
+
+
+
+
+Example 6: Rewrite the stack class presented in Section 2.1 so it uses a structure rather than a class.
+
+1. // Stack class using a structure .
+# include <iostream >
+using namespace std ;
+# define SIZE 10
+// Declare a stack class for characters using a structure .
+struct stack
+{
+stack (); // constructor
+void push ( char ch); // push character on stack
+char pop (); // pop character from stack
+private :
+char stck [ SIZE ]; // holds the stack
+int tos ; // index of top of stack
+};
+// Initialize the stack
+stack :: stack ()
+{
+cout << " Constructing a stack \n";
+tos = 0;
+}
+// Push a character .
+void stack :: push ( char ch)
+{
+if( tos == SIZE )
+{
+cout << " Stack is full \n";
+return ;
+}
+stck [ tos ] = ch;
+tos ++;
+}
+// Pop a character .
+char stack :: pop ()
+{
+    if( tos ==0)
+{
+cout << " Stack is empty \n";
+return 0; // return null on empty stack
+}
+tos --;
+return stck [ tos ];
+}
+int main ()
+{
+// Create two stacks that are automatically initialized .
+stack s1 , s2;
+int i;
+s1. push (’a’);
+s2. push (’x’);
+s1. push (’b’);
+s2. push (’y’);
+s1. push (’c’);
+s2. push (’z’);
+for (i =0; i <3; i ++)
+cout << " Pop s1: " << s1.pop () << "\n";
+for (i =0; i <3; i ++)
+cout << " Pop s2: " << s2.pop () << "\n";
+return 0;
+}
+
+
+
+
+Example 7: Use a union class to swap the low- and high-order bytes of an integer (assuming 16-bit
+integers; if your computer uses 32-bit integers, swap the bytes of a short int).
+# include <iostream >
+using namespace std ;
+union swapbytes
+{
+unsigned char c [2];
+unsigned i;
+swapbytes ( unsigned x);
+void swp ();
+};
+swapbytes :: swapbytes ( unsigned x)
+{
+i = x;
+}
+void swapbytes :: swp ()
+{
+unsigned char temp ;
+temp = c [0];
+428ANSWERS
+2.6 EXERCISES
+c [0] = c [1];
+c [1] = temp ;
+}
+int main ()
+{
+swapbytes ob (1) ;
+ob. swp ();
+cout << ob.i;
+return 0;
+}
+
+
+
+Example 8: Explain what an anonymous union is and how it differs from a normal 'union'.
+An anonymous union is the syntactic mechanism that allows two variables to share the
+same memory space. The members of an anonymous union are accessed directly, without
+reference to an object. They are at the same scope level as the union itself.
+
