dea4b490d4abe67f5e67b883cc6f0e9949cd607a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-23 16:25:35 +0600

    Update ch10_05_2_auto_inLine.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_05_2_auto_inLine.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_05_2_auto_inLine.cpp
@@ -31,7 +31,71 @@
             This format is very common in C++ programs when a function is declared within a class declaration. 
             It allows the declaration to be more compact. 
 
-
 */
+
+
+
+For example, the divisible()
+function from the preceding section can be automatically in-lined as shown here:
+# include <iostream >
+using namespace std ;
+58INTRODUCING CLASSES
+2.7. AUTOMATIC IN-LINING
+class samp
+{
+int i, j;
+public :
+samp ( int a, int b);
+/*
+divisible () is defined here and automatically
+in - lined .
+*/
+int divisible () { return !(i%j);}
+};
+samp :: samp ( int a, int b)
+{
+i = a;
+j = b;
+}
+int main ()
+{
+samp ob1 (10 , 2) , ob2 (10 , 3);
+// this is true
+if( ob1. divisible ())
+cout << "10 divisible by 2\n";
+// this is false
+if( ob2. divisible ())
+cout << "10 divisible by 3\n";
+return 0;
+}
+As you can see, the code associated with divisible() occurs inside the declaration for the class
+samp. Further notice that no other definition of divisible() is needed-or permitted. Defining
+divisible() inside samp causes it to be made into an in-line function automatically.
+When a function defined inside a class declaration cannot be made into an in-line function
+(because a restriction has been violated), it is automatically made into a regular function.
+Notice how divisible() is defined within samp, paying particular attention to the body. It
+occurs all on one line. This format is very common in C++ programs when a function is
+declared within a class declaration. It allows the declaration to be more compact. However, the
+samp class could have been written like this:
+class samp
+{
+int i, j;
+public :
+samp ( int a, int b);
+/*
+divisible () is defined here and automatically in - lined .
+*/
+59TEACH YOURSELF
+C++
+int divisible ()
+{
+return !(i%j);
+}
+};
+In this version, the layout of divisible() uses the more-or-less standard indentation style. From
+the compiler’s point of view, there is no difference between the compact style is commonly found
+in C++ programs when short functions are defined inside a class definition.
+The same restrictions that apply to "normal" in-line functions apply to automatic in-line functions within a class declaration
+
 
 
