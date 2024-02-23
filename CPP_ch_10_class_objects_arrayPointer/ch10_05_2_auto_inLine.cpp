2aa2854e68cbcd61e3646bb7a07583636ef5f6f6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-23 16:22:52 +0600

    Update ch10_05_2_auto_inLine.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_05_2_auto_inLine.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_05_2_auto_inLine.cpp
@@ -1,16 +1,37 @@
 
+/*  ----------------    AUTOMATIC IN-LINING    ----------------
 
-	AUTOMATIC IN-LINING: If a member function's definition is short enough, the definition can be included inside the class declaration. Doing so causes the function to automatically become an in-line function, if possible. 
-	When a function is defined within a class declaration, the inline keyword is no longer necessary. 
-	Of course if any in-line restriction is violated, the compiler is free to generate a normal function.
-	For example, the  divisible()  is automatically in-lined as shown here:
-class samp {	int i, j;
-public:
-samp (int a, int b);
-/* divisible() is defined here and automatically in - lined . */
-int divisible() { return !(i%j);}
-};
-samp :: samp(int a, int b) { i = a;  j = b; }
-	As you can see, the code associated with divisible() occurs inside the declaration for the class samp. Further notice that no other definition of divisible() is needed-or permitted.
-	Notice how divisible()  occurs all on one line. This format is very common in C++ programs when a function is declared within a class declaration. It allows the declaration to be more compact. 
+    If a member function's definition is 'short enough', 
+        the DEFINITION can be included inside the "class declaration". 
+        Then the function autometically become an "in-line function", if possible. 
+
+    When a function is defined "within a class declaration", the 'inline' keyword is no longer necessary. 
+        if any 'in-line restriction' is violated, the compiler is free to generate a normal function.
+    
+    For example, the  divisible()  is automatically in-lined as shown here:
+
+
+            class samp {	
+                    int i, j;
+                public:
+                    samp(int a, int b);
+                    int divisible(){ return !(i%j); }   // divisible() is defined inside class-declaration and automatically 'in-lined'.
+            };
+
+            samp::samp(int a, int b) {
+                i = a;
+                j = b; 
+            }
+
+
+        Notice, the code associated with divisible() occurs inside the declaration for the class 'samp'. 
+            Further notice that no other definition of divisible() is 'needed-or permitted'.
+
+        Notice how divisible()  occurs all on one line. 
+            This format is very common in C++ programs when a function is declared within a class declaration. 
+            It allows the declaration to be more compact. 
+
+
+*/
+
 
