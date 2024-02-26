5e232556d375ceae2a6e5892c704a3839bcd80d3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-02-26 12:16:50 +0600

    Update null.cpp

--- CPP_ch_10_class_objects_arrayPointer/null.cpp
+++ CPP_ch_10_class_objects_arrayPointer/null.cpp
@@ -6,13 +6,19 @@
 // -=-=-=-=-    Mastery Skills Check    -=-=-=-=-
 
 At this point you should be able to perform the following exercises and answer the questions.
-1. What is a constructor? What is a destructor? When are they executed?
-2. Create a class called line that draws a line on the screen. Store the line length in a private
+
+
+// Example 1: What is a constructor? What is a destructor? When are they executed?
+
+
+// Example 2: Create a class called line that draws a line on the screen. Store the line length in a private
 integer variable called len. Have line’s constructor take one parameter: the line length.
 Have the constructor store the length and actually draw the line. If your system does not
 support graphics, display the line by using *.
 Optional: Give linea destructor that erases the line.
-3. What does the following program display?
+
+
+// Example 3: What does the following program display?
 # include <iostream >
 using namespace std ;
 int main ()
@@ -24,11 +30,17 @@ cout << i << ’ ’ << l << ’ ’ << d;
 cout << "\n";
 return 0;
 }
-4. Add another derived class that inherits area cl from Section 2.3, Exercise 1. Call this
+
+
+// Example 4: Add another derived class that inherits area cl from Section 2.3, Exercise 1. Call this
 class cylinder and have it compute the surface area of a cylinder. Hint: The surface area
 of a cylinder is: 2 ∗ π ∗ R2 + π ∗ D ∗ height:
-5. What is an in-line function? What are its advantages and disadvantages?
-6. Modify the following program so that all member functions are automatically in-lined:
+
+
+// Example 5: What is an in-line function? What are its advantages and disadvantages?
+
+
+// Example 6: Modify the following program so that all member functions are automatically in-lined:
 # include <iostream >
 using namespace std ;
 class myclass
@@ -53,8 +65,12 @@ myclass count (2, 3);
 count . show ();
 return 0;
 }
-7. What is the difference between a class and a structure?
-8. Is the following fragment valid?
+
+
+// Example 7: What is the difference between a class and a structure?
+
+
+// Example 8: Is the following fragment valid?
 union
 {
 float f;
@@ -69,12 +85,16 @@ unsigned int bits ;
 
 This section checks how well you have integrated material in this chapter with that from the
 preceding chapter.
-1. Create a class called prompt. Pass its constructor function a prompting string of your
+
+
+// Example 1: Create a class called prompt. Pass its constructor function a prompting string of your
 own choosing. Have the constructor display the string and then input an integer. Have
 the constructor display the string and then input an integer. Store this value in a private
 variable called count. When an object of type prompt is destroyed, ring the bell on the
 terminal as many times as the user entered.
-2. In Chapter 1 you created a program that converted feet to inches. Now create a class that
+
+
+// Example 2: In Chapter 1 you created a program that converted feet to inches. Now create a class that
 does the same thing. Have the class store the number of feet and its equivalent number of
 inches. Pass to the class’s constructor the number of feet and have the constructor display
 the number of inches.
@@ -90,15 +110,21 @@ number between 1 and 6. Then have roll() display that value.
 
 Before proceeding, you should be able to correctly answer the following questions and do the
 exercises.
-1. Given the following class, what are the names of its constructor and destructor functions?
+
+
+// Example 1: Given the following class, what are the names of its constructor and destructor functions?
 class widgit
 {
 int x, y;
 public :
 // ... fill in constructor and destructor functions
 };
-2. When is a constructor function called? When is a destructor function called?
-3. Given the following base class, show how it can be inherited by a derived class called
+
+
+// Example 2: When is a constructor function called? When is a destructor function called?
+
+
+// Example 3: Given the following base class, show how it can be inherited by a derived class called
 Mars.
 class planet
 {
@@ -109,9 +135,15 @@ double mass ;
 public :
 // ...
 };
-4. There are two ways to cause a function to be expanded in line. What are they?
-5. Give two possible restrictions to in-line functions.
-6. Given the following class, show how an object called ob that passes the value 100 to a
+
+
+// Example 4: There are two ways to cause a function to be expanded in line. What are they?
+
+
+// Example 5: Give two possible restrictions to in-line functions.
+
+
+// Example 6: Given the following class, show how an object called ob that passes the value 100 to a
 and X to c would be declared.
 class sample
 {
