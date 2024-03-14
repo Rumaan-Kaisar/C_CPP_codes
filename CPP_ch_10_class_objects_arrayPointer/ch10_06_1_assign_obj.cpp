6afd688595fecfbeb59569433114b9e448922a2b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-14 08:59:22 +0600

    Update ch10_06_1_assign_obj.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_06_1_assign_obj.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_06_1_assign_obj.cpp
@@ -8,27 +8,41 @@
     Important fact:
         C++ is different in this case from Java, JavaScript & Python
 
-In C++, when you assign one object to another, a memberwise copy is made by default. 
-    This means that each data member of the source object is copied to the corresponding data member of the destination object. For built-in types, this copy operation is essentially a bitwise copy. For user-defined types, the copy operation may involve calling the copy constructor or assignment operator, depending on how they are implemented.
+        In C++, when you assign one object to another, a 'MEMBERWISE COPY' is made by default. 
+            i.e. each data member of the SOURCE OBJECT is copied to the corresponding data member of the destination object. 
+                For 'built-in' types, this copy operation is essentially a "bitwise copy". 
+                For 'user-defined' types, the copy operation may involve calling the 'copy constructor' or "assignment operator", 
+                    depending on how they are implemented.
 
 
-In Java or JavaScript or Python, on the other hand, when you assign one object reference to another, you are copying the reference, not the object itself. Both the original and the copied reference point to the same object in memory. This means that modifications made to the properties of one object will affect the other object if they are referring to the same underlying data.
+        In Java / JavaScript / Python, on the other hand, when you assign one object reference to another, 
+            you are "copying the REFERENCE", not the object itself. 
+            Both the 'original' and the 'copied reference' point to the "same object" in MEMORY. 
+            i.e. modifications made to the properties of one object will affect the other object 
+                if they are referring to the same underlying data.
 
-To create a copy of an object with its own separate memory space in Java, you typically need to perform a deep copy manually by implementing the Cloneable interface and overriding the clone() method, or by using serialization/deserialization techniques.
+            To create a copy of an object with its 'own separate memory space' in Java/JavaScript/Python, 
+                you typically need to perform a deep copy manually.
+
+        Java:
+            To create a deep copy of an object in Java, implement the 'Cloneable' interface and overriding the clone() method,
+                This involves creating a new instance of the object and copying the state of all its fields to the new instance.  
+            Alternatively use serialization/deserialization techniques.
+
+        JavaScript:
+            iterate through its properties and creating 'new copies of nested objects' recursively. 
+            Alternatively, use built-in methods such as: 
+                spread syntax ({...obj}) or Object.assign() to create shallow copies, 
+            or you can use external libraries like lodash for deep copying.
+
+        Python:
+            Use the copy() method or the 'copy module' for shallow copies, 
+            Or the deepcopy() function from the 'copy module' for deep copies. 
+            These functions create new objects with their own memory space, recursively copying all nested objects.
 
 
-
-Java:
-To create a deep copy of an object in Java, you can implement the Cloneable interface and override the clone() method. This involves creating a new instance of the object and copying the state of all its fields to the new instance. Alternatively, you can use serialization/deserialization techniques to achieve deep copying.
-
-JavaScript:
-In JavaScript, you can create a deep copy of an object by iterating through its properties and creating new copies of nested objects recursively. Alternatively, you can use built-in methods such as spread syntax ({...obj}) or Object.assign() to create shallow copies, or you can use external libraries like lodash for deep copying.
-
-Python:
-In Python, you can create deep copies of objects using the copy() method or the copy module for shallow copies, or the deepcopy() function from the copy module for deep copies. These functions create new objects with their own memory space, recursively copying all nested objects.
-
-
-
+    
+    ----------------    rev[14-mar-24]    ----------------
 
 
 
