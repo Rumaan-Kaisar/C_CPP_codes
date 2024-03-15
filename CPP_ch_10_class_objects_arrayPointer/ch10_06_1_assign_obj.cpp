02e96569d897f3b85ffeaacc8074c1f6a0dd43e4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-03-15 14:41:01 +0600

    Update ch10_06_1_assign_obj.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_06_1_assign_obj.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_06_1_assign_obj.cpp
@@ -39,42 +39,56 @@
             Use the copy() method or the 'copy module' for shallow copies, 
             Or the deepcopy() function from the 'copy module' for deep copies. 
             These functions create new objects with their own memory space, recursively copying all nested objects.
-
-
-    
-    ----------------    rev[14-mar-24]    ----------------
+*/
 
 
 
 
+/* Example 1: The contents of object called 'obj_1' is assigned to another object called 'obj_2', 
+                the contents of all of obj_1's data are copied into the equivalent members of obj_2.
+*/
 
-
-
-
-Example 1: the contents of object called obj_1 is assigned to another object called obj_2, the contents of all of obj_1's data are copied into the equivalent members of obj_2. 
- 
-#include <iostream >
-using namespace std;
+#include <iostream>
 
 class myclass {
-int a, b;
-public :
-void set(int i, int j) {a = i; b = j; }
-void show() {cout << a << ' ' << b << "\n";}
+        int a, b;
+    public:
+        void set(int i, int j) {a = i; b = j; }
+        void show() {std::cout << a << ' ' << b << "\n";}
 };
 
-int main(){	myclass obj_1 , obj_2;
-obj_1.set(10 , 4);
 
-// assign obj_1 to obj_2 
+int main(){	
+    myclass obj_1 , obj_2;
 
-obj_2 = obj_1;
-obj_1.show();
-obj_2.show();
-return 0;}
- 
-	Here, object obj_1 has its member variables a and b set to the values 10 and 4, respectively. Next, obj_1 is assigned to obj_2. This causes the current value of obj_1.a to be assigned to obj_2.a and  obj_1.b to be assigned to obj_2.b. 
-	An assignment between two objects simply makes the data in those objects identical. The two objects are still completely separate, For example, after the assignment, calling obj_1.set() to set the value of obj_1.a has no effect on obj_2 or its a value.
+    obj_1.set(10 , 4);
+    
+    // assign obj_1 to obj_2 
+    obj_2 = obj_1;
+    obj_1.show();
+    obj_2.show();   // obj_2 has copied members from obj_1
+    return 0;
+}
+
+
+
+
+/*  obj_1's member variables a and b set to the values 10 and 4, respectively. 
+    Next, obj_1 is assigned to obj_2. 
+        This causes the current value of obj_1.a to be assigned to obj_2.a and  obj_1.b to be assigned to obj_2.b. 
+
+    An assignment between two objects simply makes the data in those objects identical. 
+        The two objects are still completely separate, 
+        For example, after the assignment, calling obj_1.set() to set the value of obj_1.a has no effect on obj_2 or its a value.
+
+    Remember: 
+        For Java / JavaScript / Python this is not true.
+
+    Restrictions: 
+        same type: Only objects of the 'same type' can be used in an assignment statement. 
+            If the objects are not of the same type, a "COMPILE-TIME ERROR" is reported. 
+            It is not sufficient that the types just be physically similar-their type names must be the same. 
+*/
 
 
 	Only objects of the same type can be used in an assignment statement. If the objects are not of the same type, a compile-time error is reported. It is not sufficient that the types just be physically similar-their type names must be the same. 
