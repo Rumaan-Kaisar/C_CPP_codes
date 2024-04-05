4b4d2fd13d7cc385b9e93ad2ee156c78be6ba12a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-04-05 15:35:29 +0600

    Update ch10_06_3_return_obj_from_func.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_06_3_return_obj_from_func.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_06_3_return_obj_from_func.cpp
@@ -1,37 +1,57 @@
+
 /*  ----------------------    RETURN objects from function    ---------------------- 
-    RETURNING OBJECTS FROM FUNCTIONS: 
-    To return objects from a function,
-    [1]	First declare the function as returning a class type. 
-    [2]	Second, return an object of that type using the normal return statement.
-    ret_class_type function_name( any_type obj_1, any_type obj_2, . . . . );    
+    RETURNING objects from functions: 
+        Declare the function as returning a class type. 
+        Then return an object of that type using the normal return statement.
+
+                return_class_type function_name( any_type obj_1, any_type obj_2, . . . . );    
 */
 
 
 
-Example: Here is an example of a function that returns an object:
-#include <iostream >
-#include <cstring >
+/* Example 1: Here is an example of a function that returns an object: 
+                In this example, input() creates a "local object" called "str" and then reads a string from the keyboard. 
+                This string is copied into "str.s" and then str is returned by the function. 
+                This object is then assigned to "ob" inside main() when it is returned by the call to input(). 
+*/
+#include <iostream>
+#include <cstring>
 
-using namespace std;
-class samp {char s[80];
-        public :
-            void show() { cout << s << "\n"; }
-            void set( char *str ) { strcpy(s, str ); }  };
-/* Function input() return an object of type samp */
-samp input(){ char g[80];     /* it is a local variable. However s[80] used in book. Which have no 
-connection with the private variable s[80] in the class samp */
-samp str ; 				/* declaring local object str */
-cout << " Enter a string : ";  cin >> g;
-str.set(g); 				/* copying g to str.s */
-return str; } 			/* returning object */
+class samp {
+        char s[80];
+    public:
+        void show(){ std::cout << s << "\n"; }
+        void set(char *str){ strcpy(s, str ); }  
+};
 
-int main(){	samp ob;
-ob = input(); 			/* assign returned object to ob */
-ob.show();
-return 0;}
+// Function input() return an object of type samp
+samp input(){ 
+    char g[80];     // it is a local variable. However s[80] used in book. Which have no connection with the private variable s[80] in the class samp
+    samp str;       // declaring local object str
 
-In this example, input() creates a local object called str and then reads a string from the keyboard. This string is copied into str.s and then str is returned by the function. This object is then assigned to ob inside main() when it is returned by the call to input(). Notice in the book a private variable s[80] used in the function input().
+    std::cout << " Enter a string : ";  
+    std::cin >> g;
+    str.set(g);     // copying g to str.s
+    return str;     // returning object
+}
+
+
+int main(){	
+    samp ob;
+    ob = input();   // assign returned object to ob
+    ob.show();
+
+    return 0;
+}
+
+
+
+
+// ----  rev  ----
+
 	One important point is : When an object is returned by a function, a temporary object is automatically created which holds the return value. It is this object that is actually returned by the function. After the value has been returned, this object is destroyed. The destruction of this temporary object might cause unexpected side effects in some situations (This problem can be resolved in two ways : one, using reference. Two, using copy-constructor.).
+
+
 	You must be careful about returning objects from functions if those objects contain destructor functions because the returned object goes out of scope as soon as the value is returned to the calling routine. For example, if the object returned by the function has a destructor that frees dynamically allocated memory, that memory will be freed even though the object that is assigned the return value is still using it. Consider the program above with newly defined class, constructor and set() : 
 
 #include <cstdlib> 					/* we'll use allocation function malloc() and free() */
