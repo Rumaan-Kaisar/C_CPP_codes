de40002fae2dd6caf20d280e34059a941ee4f518
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-04-25 11:40:17 +0600

    Update ch10_01_2_constructor_destructor.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_01_2_constructor_destructor.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_01_2_constructor_destructor.cpp
@@ -631,3 +631,17 @@ class sample{
 */
 
 
+
+
+/* Example 10: Given the following class, what are the names of its 'constructor' and 'destructor' functions? */
+
+class widgit{
+        int x, y;
+    public:
+        // ... fill in constructor and destructor functions
+};
+
+// Constructor is called widgit() and 
+// Destructor is called ~widgit().
+
+
