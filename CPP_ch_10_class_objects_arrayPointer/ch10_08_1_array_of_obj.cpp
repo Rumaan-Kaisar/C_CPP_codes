274fd2b944fb74f5eeeab1f1d4e5b620f315513a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-17 19:48:27 +0600

    Update ch10_08_1_array_of_obj.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_08_1_array_of_obj.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_08_1_array_of_obj.cpp
@@ -145,75 +145,63 @@ int main(){
 }
 
 
-// ----    REV[17-MAY-24]    ----
 
-/* Example 4: As you know, a constructor can take more than one argument. When initializing an array
-of objects whose constructor takes more than one argument, you must use the alternative
-form of initialization mentioned earlier. Let’s begin with an example: */
 
-# include <iostream >
-using namespace std ;
-class samp
-{
-int a, b;
-public :
-samp ( int n, int m) { a = n; b = m; }
-int get_a () { return a; }
-int get_b () { return b; }
+// -=-=-=-    Initialization for a 2-D array of objects with a 2-parameter constructor    -=-=-=-
+
+/* Example 4: A constructor can take more than one argument. 
+                When initializing an array of objects whose constructor takes 
+                more than one argument, you must use the "longer form" of initialization.  
+*/
+
+#include <iostream>
+
+class samp{
+        int a, b;
+    public:
+        samp( int n, int m) { a = n; b = m; }   // 2-parameter constructor
+        int get_a() { return a; }
+        int get_b() { return b; }
 };
-int main ()
-{
-samp ob [4][2] = { samp (1, 2) , samp (3, 4) ,
-samp (5, 6) , samp (7, 8) ,
-samp (9, 10) , samp (11 , 12) ,
-samp (13 , 14) , samp (15 , 16)
-};
-int i;
-91TEACH YOURSELF
-C++
-for (i =0; i <4; i ++)
-{
-cout << ob[i ][0]. get_a () << ’ ’;
-cout << ob[i ][0]. get_b () << "\n";
-cout << ob[i ][1]. get_a () << ’ ’;
-cout << ob[i ][1]. get_b () << "\n";
+
+
+int main(){
+    // following initalize 8 objects using "longer form"
+    samp ob [4][2] = { 
+        samp(1, 2),     samp(3, 4),
+        samp(5, 6),     samp(7, 8),
+        samp(9, 10),    samp(11, 12),
+        samp(13 , 14),  samp(15, 16)
+    };
+
+    int i;
+
+    for(i =0; i <4; i ++){
+        std::cout << ob[i][0].get_a() << ' ';
+        std::cout << ob[i][0].get_b() << "\n";
+        std::cout << ob[i][1].get_a() << ' ';
+        std::cout << ob[i][1].get_b() << "\n";
+    }
+    std::cout << "\n";
+
+    return 0;
 }
-cout << "\n";
-return 0;
-}
-In this example, samp’s constructor takes two arguments. Here, they array ob is declared
-and initialized in main() by using direct calls to samp’s constructor. This is necessary
-because the formal C++ syntax allows only one argument at a time in a comma-separated
-list. There is no way, for example, to specify two (o more) arguments per entry in the
-list. Therefore, when you initialize arrays of objects that have constructors that take more
-than one argument, you must use the "long form" initialization syntax rather than the
-"shorthand form."
-Note: You can always use the long form of initialization even if the object takes only one
-argument. It’s just that the short form is more convenient in this case.
-The preceding program displays
-1 2
-3 4
-5 6
-7 8
-9 10
-11 12
-13 14
-15 16
+
+/*  In this example, samp's constructor takes two arguments.
+
+    Here, they array "ob" is declared and initialized in main() by using "direct calls" to samp's constructor. 
+
+    This is necessary because the formal C++ syntax allows only one argument at a time in a comma-separated list.
+    There is no way, for example, to specify two (or more) arguments per entry in the list.
+
+    Therefore, when you initialize arrays of objects that have constructors that take more
+        than one argument, you must use the "long form" initialization syntax rather than the "shorthand form."
+*/
 
 
 
-class samp { 	int a, b;
-public : samp(int n, int m) { a = n; b = m; } /* initialization for 2-D array */
-int get_a() { return a; }
-int get_b() { return b; }	};
 
-int main() { 	samp ob[4][2] = {	samp(1, 2), 		samp(3, 4),
-samp(5, 6), 		samp(7, 8),
-samp(9, 10), 		samp(11 , 12),
-samp(13 , 14), 	samp(15 , 16)   };
-. . . . . }
-
-
+// ----  rev[17-may-2024]  ----
 
 EXERCISES
 1. Using the following class declaration, create a ten-element array and initialize the ch
