1c9b0538271c0bfc6f6608a5d4387904957aa42e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-05-23 15:54:59 +0600

    Update ch10_08_2_pointer_to_obj.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_08_2_pointer_to_obj.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_08_2_pointer_to_obj.cpp
@@ -1,11 +1,42 @@
-10.7 Object Pointers
-It is possible to access a member of an object via pointer to that object. When a pointer is used, the arrow operator (->) rather than the dot operator is employed. (This is exactly the same way the arrow operator is used when given a pointer to a structure.)
-However using the dot operator to access members of an object is the correct method.
-	You declare an object pointer just like you declare a pointer to any other variable. Specify its class name, and then precede the variable name with an asterisk. 
-	To obtain the address of an object, precede the object with & operator, just as you do when taking the address of any other type of variable.
-	Just like pointers to other types, an object pointer, when incremented, will point to the next object of its type.
-	It is important to understand that creation of an object pointer does not create an object-it creates just a pointer to one. 
-For example 
+
+/*  ------------    Object Pointers    ------------
+    We've already discussed this topic in : ch10_03_obj_pointer.cpp
+        However, let's review it again
+
+
+    We can 'access' a member of an object via "pointer" to that object. 
+    
+    When a "pointer" is used, the "arrow operator" (->) rather than the dot (.) operator is employed.
+        (it is exactly the same way "->" is used when given a "pointer to a structure")
+
+    However using the dot operator "." to access members of an object is the correct method
+
+
+
+    object pointer declaration is same as any other variable. 
+        Specify its class name, and then precede the variable name with an asterisk "*"
+
+    Note that, creation of an object pointer does not create an object- 
+        it creates just a pointer to one. 
+
+
+
+    address:
+        To obtain the address of an object, precede the object with "&" operator
+
+    pointer arithmetic:    
+        like other types, when object pointer incremented, it will point to the next object of its type.
+
+*/
+
+
+// ----  rev[23-may-24]  ----
+
+/* Example 1: following use two ways to access a class member of an object of that class
+                    use "." operator 
+                    use "->" operator (pointer)
+*/
+
 int myclass :: get() { return a; }
 int main() {	myclass ob (120) ; 	/*  create object  */
 myclass *p; 		/*  create pointer to object  */ 
@@ -14,6 +45,51 @@ cout << " Value using object : " << ob.get();
 cout << "\n";
 cout << " Value using pointer : " << p->get();
 return 0; }
+
+
+
+
+
+/* Example 2: (Pointers to objects) Here is an example of object pointer arithmetic */
+
+#include <iostream>
+
+class samp{
+        int a, b;
+    public:
+        samp(int n, int m){ a = n; b = m; }
+        int get_a(){ return a; }
+        int get_b(){ return b; }
+};
+
+
+int main(){
+    // initialization
+    samp ob[4] = {
+        samp(1, 2),
+        samp(3, 4),
+        samp(5, 6),
+        samp(7, 8)
+    };
+
+    int i;
+    samp *p;    // object pointer
+
+    p = ob;     // get starting address of array
+    for(i =0; i<4; i++){
+        std::cout << p -> get_a() << ' ';
+        std::cout << p -> get_b() << "\n";
+        p++;    // advance to next object
+    }
+
+    std::cout << "\n";
+
+    return 0;
+}
+
+
+
+
 	Notice how the declaration          myclass *p; 	creates a pointer to an object of myclass. 
 	The address of ob is put into p by using this statement:       p = &ob;
 	Finally, the program shows how the members of an object can be accessed through a Pointer as using :      p->get()  .
