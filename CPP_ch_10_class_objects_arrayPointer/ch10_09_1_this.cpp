14e2cd733958ab0da5fa1e2dc84523d2d0a862c7
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-03 10:37:40 +0600

    Update ch10_09_1_this.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_09_1_this.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_09_1_this.cpp
@@ -11,13 +11,56 @@
         The function f1() is automatically passed a pointer to "ob" -which is the object that invokes the call. 
             That pointer is referred to as "this"
 
+
     Restrictions:
         Only member functions are passed a "this" pointer. 
         A friend does NOT have a "this" pointer
 
 */
 
-/* When a member function refers to another member of a class, it does so directly without qualifying the member with either a class or an object specification. For example,  */
+
+/* Example 1: When a member function "refers" to another member of a class, 
+                it does so DIRECTLY without qualifying the member with either a "class or an object specification"
+
+                Examine following short program, which creates a simple "inventory" class
+*/
+
+// Demonstrate the this pointer .
+# include <iostream >
+# include <cstring >
+using namespace std ;
+class inventory
+{
+char item [20];
+double cost ;
+int on_hand ;
+public :
+inventory ( char *i, double c, int o)
+{
+strcpy (item , i);
+cost = c;
+on_hand = o;
+}
+void show ();
+};
+void inventory :: show ()
+{
+cout << item ;
+cout << ": $" << cost ;
+cout << " On hand : " << on_hand << "\n";
+}
+int main ()
+{
+inventory ob(" wrench ", 4.95 , 4);
+ob. show ();
+return 0;
+}
+As you can see, within the constructor inventory() and the member function show(),
+the member variables item, cost, and on hand are referred to directly. This is because a
+member function can be called only in conjunction with an object. Therefore, the compiler
+knows which object’s data is being referred to.
+
+
 
 
 class inventory {	char item[20];
