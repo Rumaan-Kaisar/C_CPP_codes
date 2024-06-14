9d86baef2d4db18502aab28fbcb6105c2d72ab6f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-14 16:28:22 +0600

    Update ch10_09_3_more_new_delete.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_09_3_more_new_delete.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_09_3_more_new_delete.cpp
@@ -26,10 +26,12 @@
 
 
 
+// ----  rev[14-jun-2024]  ----
+
 /* Example 3 : to allocate an array of integers  */
 int *p;
-p = new int[5]; 	/* allocate room for 5 integers */ 
-if(!p) { cout << " Allocation error \n"; return 1; } 	/*allocation check*/
+p = new int[5]; 	// allocate room for 5 integers
+if(!p) { cout << " Allocation error \n"; return 1; } 	// allocation check
 . . . . . . STATEMENTS. . . . . .
 delete [] p;
 
@@ -38,20 +40,26 @@ delete [] p;
 
 /* Example 4 : to allocate object array named "samp", */
 samp *p;
-p = new samp[5]; 	/* allocate object array named "samp" */ 
-if(!p) { cout << " Allocation error \n"; return 1; } 	/*allocation check*/
+p = new samp[5]; 	// allocate object array named "samp" 
+if(!p) { cout << " Allocation error \n"; return 1; } 	// allocation check
 . . . . . . STATEMENTS. . . . . .
 delete [] p;
 
 
 /* 
 Note
-[1]	new automatically allocates enough memory to hold an object of the specified type. You do not need to use sizeof, for example, to compute the number of bytes required. This reduces the possibility for error. 
-[2]	new automatically returns a pointer of the specified type. You do not need to use an explicit type cast.  
-	In C, no type cast is required when you are assigning the return value of malloc() to a pointer because the void * returned by malloc() is automatically converted into a pointer compatible with the type of pointer on the left side of the assignment. 
-	However, this is not the case in C++, which requires an explicit type cast when you use malloc(). The reason for this difference is that it allows C++ to enforce more rigorous type checking.
-[3]	Both new and delete can be overloaded, enabling you to easily implement your own custom allocation system. 
-[4]	It is possible to initialize a dynamically allocated object. 
-[5]	You no longer need to include <cstdlib> with your programs.
+    new automatically allocates enough memory to hold an object of the specified type. You do not need to use sizeof, for example, to compute the number of bytes required. This reduces the possibility for error. 
 
- */
+    new automatically returns a pointer of the specified type. You do not need to use an explicit type cast.  
+
+    In C, no type cast is required when you are assigning the return value of malloc() to a pointer because the void * returned by malloc() is automatically converted into a pointer compatible with the type of pointer on the left side of the assignment. 
+
+    However, this is not the case in C++, which requires an explicit type cast when you use malloc(). The reason for this difference is that it allows C++ to enforce more rigorous type checking.
+    
+    Both new and delete can be overloaded, enabling you to easily implement your own custom allocation system. 
+    
+    It is possible to initialize a dynamically allocated object. 
+
+    You no longer need to include <cstdlib> with your programs.
+
+*/
