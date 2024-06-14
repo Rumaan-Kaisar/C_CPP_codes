f1000aa76598aa0e6e43f2783902be8420d785e2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-06-14 16:23:46 +0600

    Update ch10_09_3_more_new_delete.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_09_3_more_new_delete.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_09_3_more_new_delete.cpp
@@ -1,26 +1,50 @@
 
-	Dynamically allocate a one-dimensional array : To dynamically allocate a one-dimensional array, use this form of new:
-p_var = new type[ size ];
-After this statement has executed, p_var will point to the start of an array of size elements of the type specified. 
-	For various technical reasons, it is not possible to initialize an array that is dynamically allocated.
-	To delete a dynamically allocated array, use this form of delete:
-delete [] p_var ;
-This syntax causes the compiler to call the destructor function for each element in the array. It does not cause p_var to be freed multiple times. p_var is still freed only once.
+/*  ------------    More new, delete :    dynamically allocate array    ------------ 
+    
+    Dynamically allocate a 1D array : 
+        
+                p_var = new type[ size ];
 
-Example 3 : to allocate an array of integers 
+        After this statement has executed, "p_var" will point 
+            to the start of an array of "size" elements of the type specified. 
+
+
+
+    initialization:
+        It is not possible to initialize an array that is dynamically allocated.
+
+
+
+    delete:
+                delete [] p_var;
+
+        This syntax causes the "compiler" to call the DESTRUCTOR function for "each element" in the array. 
+        It does not cause "p_var" to be freed MULTIPLE times. 
+        p_var is still freed only once.
+
+*/
+
+
+
+/* Example 3 : to allocate an array of integers  */
 int *p;
 p = new int[5]; 	/* allocate room for 5 integers */ 
 if(!p) { cout << " Allocation error \n"; return 1; } 	/*allocation check*/
 . . . . . . STATEMENTS. . . . . .
 delete [] p;
 
-Example 4 : to allocate object array named "samp",
+
+
+
+/* Example 4 : to allocate object array named "samp", */
 samp *p;
 p = new samp[5]; 	/* allocate object array named "samp" */ 
 if(!p) { cout << " Allocation error \n"; return 1; } 	/*allocation check*/
 . . . . . . STATEMENTS. . . . . .
 delete [] p;
 
+
+/* 
 Note
 [1]	new automatically allocates enough memory to hold an object of the specified type. You do not need to use sizeof, for example, to compute the number of bytes required. This reduces the possibility for error. 
 [2]	new automatically returns a pointer of the specified type. You do not need to use an explicit type cast.  
@@ -30,4 +54,4 @@ Note
 [4]	It is possible to initialize a dynamically allocated object. 
 [5]	You no longer need to include <cstdlib> with your programs.
 
-
+ */
