fcf25dbe3c05f128aaaabd0a94dce4b1ddeca89a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-11 14:36:39 +0600

    Update ch10_10_2_pass_ref_to_obj.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_10_2_pass_ref_to_obj.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_10_2_pass_ref_to_obj.cpp
@@ -1,12 +1,39 @@
 
-	PASSING REFERENCES TO OBJECTS :  As you learned in 10.10, when an object is passed to a function by use of the default call-by-value parameter-passing mechanism, a copy of that object is made. Although the parameter's constructor function is not called, its destructor function is called when the function returns.
-	As you should recall, this can cause serious problems in some instances-when the destructor frees dynamic memory, for example. There is two way to solve this : 
-	One solution to this problem is to pass an object by reference.
-	The other solution involves the use of copy constructors, which are discussed in Next chapter: Function overloading.
-	When you pass the object by reference, no copy is made, and therefore its destructor function is not called when the function returns. Remember, however, that changes made to the object inside the function affect the object used as the argument.
-	Note: It is critical to understand that a reference is not a pointer. Therefore, when an object is passed by reference, the member access operator remains the dot (.), not the arrow (->).
-	Example:   Lets first define class with constructor and destructor 
-#include <iostream >
+/*  ----------------    pass the object by reference    ----------------
+
+    We know when an object is passed to a function by 
+        use of the default "call-by-value parameter-passing" mechanism, a copy of that object is made.
+
+    Although the parameter's constructor function is not called, 
+        its destructor function is called when the function returns
+
+        this can cause serious problems in some instances-
+            For example: when the destructor frees dynamic memory.
+
+        There is two way to solve this: 
+            1. pass an object by reference
+            2. use of copy constructors (discussed in Next chapter: Function overloading)
+
+
+
+    Resolving problem of execution of "copy's destructor function" during function return
+            Recall "ch10_06_2_pass_obj_to_func.cpp" and "ch10_06_3_return_obj_from_func.cpp"
+
+
+    When you pass the object by reference, no copy is made, and therefore its destructor function is not called when the function returns. 
+        Remember, however, that changes made to the object inside the function affect the object used as the argument.
+
+    Note: It is critical to understand that a reference is not a pointer. 
+        Therefore, when an object is passed by reference, the member access operator remains the dot (.), not the arrow (->).
+
+    	Example:   Lets first define class with constructor and destructor 
+
+
+
+*/
+
+
+#include <iostream>
 using namespace std;
 class myclass {	int who ;
 public : myclass (int n) { who = n;
