d35409e65d51869234cfc024d6895d7a600c6aaf
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-18 09:07:03 +0600

    Update ch10_10_3_return_ref.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_10_3_return_ref.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_10_3_return_ref.cpp
@@ -11,24 +11,10 @@
 */
 
 
-// A simple example of a function returning a reference .
-
-
-Here function f() is declared as returning a reference to an integer. Inside the body of the
-function, the statement
-return x;
-does not return the value of the global variable x, but rather, it automatically returns x’s
-address (int the form of a reference). Thus, inside main(), the statement
-f() = 100;
-puts the value 100 into x because f() has returned a reference to it.
-To review, function f() returns a reference. Thus when f() is used on the left side of the
-assignment statement, it is this reference, returned by f(), that is being assigned to. Since
-f() returns a reference to x (in this example), it is x that receives the value 100.
-2.
 
 /* Example 1 : here is a very simple program that contains a function that "returns a reference" */
 
-#include <iostream >
+#include <iostream>
 
 int &f(); // function that return a reference
 int x;  // without global variable  returning reference is meaningless
@@ -42,31 +28,48 @@ int main(){
     return 0; 
 }
 
-
-int &f() {		 // Return an int reference
-return x; } 	// returns a reference to x
-
-
-int main ()
-
-{
-f() = 100; // assign 100 to reference returned by f()
-cout << x << "\n";
-return 0;
-}
 // Return an int reference
-int &f()
-{
-return x; // returns a reference to x
-}
+int &f() { return x; } 	// returns a reference to x
 
-	Here function f() returns a reference. So, f() is declared as returning a reference to an integer. 
-	Inside the body of the function, the statement "  return x;  "  does not return the value of the global variable x, but rather, it automatically returns x's address (int the form of a reference). 
-	When f() is used on the left side of the assignment statement, it is this reference, returned by f(), that is being assigned to.  Thus, inside main(), the statement  "  f() = 100;  " puts the value 100 into x because f() has returned a reference to x. thus x recives the value 100.
-	Be careful on not go out of scope. For example, here the reference returned by f() is useless:
+
+/*  --------    f() returns a reference:    -------- 
+    So, f() is declared as returning a reference to an integer. 
+
+    inside f() the statement "return x;"  does not return the "value of the global variable x", 
+        but rather, it automatically returns "x's address" (int the form of a reference). 
+
+    When f() is used on the "left" side of the assignment statement, 
+        it is this reference, returned by f(), that is being assigned to.  
+        
+        Thus, inside main(), the statement  " f() = 100; " puts the value 100 into x 
+        because f() has returned a reference to x. 
+        thus x recives the value 100.
+
+
+    ----   don't lose the SCOPE   ----
+    Be careful on not go "out of scope", that's why we used "global x"
+    For example, here the reference returned by f() is useless:
+
 int &f() {	int x;  	/* x is now a local variable */ 
 return x; }  	/* returns a reference to x */ 
 In this case, x is now local to f() and will go out of scope when f() returns. This effectively means that the reference returned by f() is useless.
+
+*/
+
+Here function f() is declared as returning a reference to an integer. Inside the body of the
+function, the statement
+return x;
+does not return the value of the global variable x, but rather, it automatically returns x’s
+address (int the form of a reference). Thus, inside main(), the statement
+f() = 100;
+puts the value 100 into x because f() has returned a reference to it.
+To review, function f() returns a reference. Thus when f() is used on the left side of the
+assignment statement, it is this reference, returned by f(), that is being assigned to. Since
+f() returns a reference to x (in this example), it is x that receives the value 100.
+2.
+
+
+
 	Example 2 : One very good use of returning a reference is found when a bounded array type is created. In C++, you can create an array class that performs automatic bounds checking. An array class contains two core functions-one that stores information into the array and one that retrieves information. These functions can check, at run time, that the array boundaries are not overrun. The following program implements a bounds-checking array for characters:
  
 
