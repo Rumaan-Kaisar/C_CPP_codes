ad164c515c8ffef03d07a97f06e48bc1a032a959
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-07-30 09:23:05 +0600

    Update ch10_10_4_indpdnt_ref_&_rules.cpp

--- CPP_ch_10_class_objects_arrayPointer/ch10_10_4_indpdnt_ref_&_rules.cpp
+++ CPP_ch_10_class_objects_arrayPointer/ch10_10_4_indpdnt_ref_&_rules.cpp
@@ -13,27 +13,22 @@
     An independent reference can refer to a "constant". For example, following is valid.
 
             const int &ref = 10; 
-*/
 
 
 
-// ----  rev[29-JUL-2024]  ----
+    ----------------    RULES    ----------------
+    RESTRICTIONS on using Reference : There are a number of restrictions that apply to all types of references. 
 
-/*  ----------------    RULES    ----------------
-
-RESTRICTIONS on using Reference : There are a number of restrictions that apply to all types of references. 
-
-
-[1]	You cannot reference another reference. 
-[2]	You cannot obtain the address of a reference. 
-[3]	You cannot create arrays of references.
-[4]	You cannot reference a bit-field. 
-[5]	References must be initialized unless they are members of a class, are return values, or are function parameters.
-[6]	Remember, references are similar to pointers, but they are not pointers.
+        [1]	You cannot reference another reference. 
+        [2]	You cannot obtain the "ADDRESS" of a reference. 
+        [3]	You cannot create "ARRAYS" of references.
+        [4]	You cannot reference a "bit-field". 
+        [5]	References must be "INITIALIZED" unless they are members of a class, are return values, or are function parameters.
+        [6]	Remember, references are similar to pointers, but they are not pointers.
  
 
-Note
-[1]	Reference parameters offer several advantages over their (more or less) equivalent pointer alternatives. 
+Note:
+Reference parameters offer several advantages over their (more or less) equivalent pointer alternatives. 
 a.	No longer need to remember to pass the address of an argument. When a reference parameter is used, the address is automatically passed. 
 b.	Reference parameters offer a cleaner, more elegant interface than the rather clumsy explicit pointer mechanism. 
 c.	When an object is passed to a function as a reference, no copy is made. This is one way to eliminate the troubles associated with the copy of an argument damaging something needed elsewhere in the program (recall change : destructor problem while passing object to a function) when its destructor function is called.
