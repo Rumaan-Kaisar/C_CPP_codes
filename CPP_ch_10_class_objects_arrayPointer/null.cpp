92f4c98ec648c21f47cd099298b7245e91061788
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-02 02:18:37 +0600

    Update null.cpp

--- CPP_ch_10_class_objects_arrayPointer/null.cpp
+++ CPP_ch_10_class_objects_arrayPointer/null.cpp
@@ -57,13 +57,48 @@ int main(){
 
 
 
-
 /* Example 2: Modify your solution to the preceding problem so it accesses the array by using a pointer. */
 
-2. The this pointer is a pointer that is automatically passed to a member function and that
-points to the object that generated the call.
+// Version 2: modified version using object pointer
+#include <iostream>
+
+class a_type{
+        double a, b;
+    public:
+        a_type(double x, double y){
+            a = x;
+            b = y;
+        }
+        void show(){ 
+            std::cout << a << ' ' << b << "\n"; 
+        }
+};
 
 
+int main(){
+    int i, j;
+    // array of objects, and its initailaization (using "longer form")
+    a_type ob[2][5] = {
+        a_type(1, 1), a_type(2, 2),
+        a_type(3, 3), a_type(4, 4),
+        a_type(5, 5), a_type(6, 6),
+        a_type(7, 7), a_type(8, 8),
+        a_type(9, 9), a_type(10 , 10),
+    };
+
+    // use pointer to show the array content
+    a_type *p;
+    p = (a_type *) ob;
+
+    for(i=0; i<2; i++)
+        for(j=0; j<5; j++) {
+            p-> show();
+            p++;
+        }
+    std::cout << '\n';
+
+    return 0;
+}
 
 
 
