adb137ce8fcc9fab3411fba6c9f6c8b6a14eb78c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-02 02:14:05 +0600

    Update null.cpp

--- CPP_ch_10_class_objects_arrayPointer/null.cpp
+++ CPP_ch_10_class_objects_arrayPointer/null.cpp
@@ -20,6 +20,8 @@
                 };
 */
 
+#include <iostream>
+
 class a_type{
         double a, b;
     public:
@@ -32,70 +34,24 @@ class a_type{
         }
 };
 
-1. # include <iostream >
-using namespace std ;
-class a_type
-{
-double a, b;
-public :
-a_type ( double x, double y)
-{
-a = x;
-b = y;
-}
-void show () { cout << a << ’ ’ << b << ’\n’; }
-};
-int main ()
-{
-a_type ob [2][5] = {
-a_type (1, 1) , a_type (2, 2) ,
-a_type (3, 3) , a_type (4, 4) ,
-a_type (5, 5) , a_type (6, 6) ,
-a_type (7, 7) , a_type (8, 8) ,
-a_type (9, 9) , a_type (10 , 10) ,
-};
-int i, j;
-for (i =0; i <2; i ++)
-for (j =0; j <5; j++)
-ob[i][j]. show ();
-cout << ’\n’;
-return 0;
-} #
-include <iostream >
-using namespace std ;
-class a_type
-{
-double a, b;
-public :
-a_type ( double x, double y)
-{
-a = x;
-b = y;
-}
-void show () { cout << a << ’ ’ << b << ’\n’; }
-};
-int main ()
-{
-a_type ob [2][5] = {
-a_type (1, 1) , a_type (2, 2) ,
-a_type (3, 3) , a_type (4, 4) ,
-a_type (5, 5) , a_type (6, 6) ,
-a_type (7, 7) , a_type (8, 8) ,
-a_type (9, 9) , a_type (10 , 10) ,
-};
-a_type *p;
-p = ( a_type *) ob;
-int i, j;
-for (i =0; i <2; i ++)
-{
-for (j =0; j <5; j++)
-{
-p-> show ();
-p ++;
-}
-}
-cout << ’\n’;
-return 0;
+
+int main(){
+    int i, j;
+    // array of objects, and its initailaization (using "longer form")
+    a_type ob[2][5] = {
+        a_type(1, 1), a_type(2, 2),
+        a_type(3, 3), a_type(4, 4),
+        a_type(5, 5), a_type(6, 6),
+        a_type(7, 7), a_type(8, 8),
+        a_type(9, 9), a_type(10 , 10),
+    };
+
+    // show the array content
+    for(i=0; i<2; i++)
+        for(j=0; j<5; j++)  ob[i][j].show();
+    std::cout << '\n';
+
+    return 0;
 }
 
 
