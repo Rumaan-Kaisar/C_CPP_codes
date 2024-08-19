6910af7aae45cc5011c27b0fa8751b354ac3740b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-19 08:51:37 +0600

    Update null.cpp

--- CPP_ch_10_class_objects_arrayPointer/null.cpp
+++ CPP_ch_10_class_objects_arrayPointer/null.cpp
@@ -9,30 +9,6 @@
 
 
 
-/* Example 7: Create a function called mag() using the following prototype that raises num to the order
-of magnitude specified by order: */
-void mag ( long &num , long order );
-For example, if num is 4 and order is 2, when mag() returns, num will be 400. Demonstrate in a program that the function works.
-
-7. # include <iostream >
-using namespace std ;
-void mag ( long &num , long order );
-int main ()
-{
-long n = 4;
-long o = 2;
-cout << "4 raised to the 2nd order of magnitude is ";
-mag (n, o);
-cout << n << ’\n’;
-return 0;
-}
-void mag ( long &num , long order )
-{
-for ( ; order ; order --)
-num = num * 10;
-}
-
-
 
 
 
