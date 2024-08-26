8d46b07940313ea1749f04f8746d8bd0b5e32c04
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-08-26 11:07:08 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -1,6 +1,8 @@
 
-/*  ------------------------    chapter    ------------------------
+/*  ------------------------    Base class access control    ------------------------
 
-Base class access control
+INHERITANCE basics: 
+    previously discussed in "ch10_02_inheritance_intro.cpp"
+
 
 */  
