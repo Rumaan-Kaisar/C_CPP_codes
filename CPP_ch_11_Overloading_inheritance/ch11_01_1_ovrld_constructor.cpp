85fdf6778ad5370ddb257c1ec6fb3afa93e7b841
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-12 16:40:31 +0600

    Update ch11_01_1_ovrld_constructor.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_01_1_ovrld_constructor.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_01_1_ovrld_constructor.cpp
@@ -272,6 +272,61 @@ int main(){
 
 
 
+1. # include <iostream >
+# include <cstring >
+# include <cstdlib >
+using namespace std ;
+class strtype
+{
+char *p;
+int len ;
+public :
+strtype ();
+strtype ( char *s, int l);
+char * getstring () { return p; }
+int getlength () { return len; }
+};
+strtype :: strtype ()
+{
+p = new char [255];
+if (!p)
+{
+cout << " Allocation error \n";
+exit (1) ;
+} *
+p = '\0'; // null string
+len = 255;
+}
+strtype :: strtype ( char *s, int l)
+{
+if( strlen (s) >= l)
+{
+cout << " Allocating too little memory !\n";
+exit (1) ;
+} p
+= new char [l];
+if (!p)
+{
+cout << " Allocation error \n";
+exit (1) ;
+}
+strcpy (p, s);
+len = l;
+}
+465TEACH YOURSELF
+C++
+int main ()
+{
+strtype s1;
+strtype s2(" This is a test ", 100) ;
+cout << "s1: " << s1. getstring () << " - Length : ";
+cout << s1. getlength () << ’\n’;
+cout << "s2: " << s2. getstring () << " - Length : ";
+cout << s2. getlength () << ’\n’;
+return 0;
+}
+
+
 /* Example 6: In Exercise 2 of Chapter 2, Section 2.1, you created a stopwatch emulation. Expand your
 solution so that the stopwatch class provides both a parameterless constructor (as it does
 already) and an overloaded version that accepts the system time in the form returned by
