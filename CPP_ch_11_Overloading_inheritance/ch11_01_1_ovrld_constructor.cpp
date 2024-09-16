d5b7a04dfa2cb7dd2e0f444495cadc85697e3588
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-09-16 15:05:16 +0600

    Update ch11_01_1_ovrld_constructor.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_01_1_ovrld_constructor.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_01_1_ovrld_constructor.cpp
@@ -330,8 +330,6 @@ int main(){
 
 
 
-// ----  rev[13-Sep-2024]  ----
-
 /* Example 6: In "Example 7" of "ch10_01_2_constructor_destructor.cpp", we created a stopwatch emulation. 
 
                 Now we expand our solution so that the stopwatch class provides both:
@@ -343,7 +341,7 @@ int main(){
                 
                 Use a CONSTRUCTOR to initially set the elapsed time to 0. 
                 
-                Provide two member functions called start() and stop() that turn on and off the timer, respectively. 
+                Provide two member functions called start() and stop() that turn 'on' and 'off' the timer, respectively. 
                 Include a member function called show() that displays the elapsed time. 
                 
                 Also, have the DESTRUCTOR function automatically display elapsed time when a stopwatch object is destroyed. 
@@ -351,15 +349,15 @@ int main(){
 
 
 
---------  clock_t clock(void);  --------
+                --------  clock_t clock(void);  --------
 
-clock() returns the 'number of system clock cycles' that have occurred since the PROGRAM began EXECUTION. 
-    To compute the number of seconds, divide this value by the CLOCKS_PER_SEC macro. 
+                clock() returns the 'number of system clock cycles' that have occurred since the PROGRAM began EXECUTION. 
+                    To compute the number of seconds, divide this value by the CLOCKS_PER_SEC macro. 
+                    Recall: CPP_ch_0_useful_lib_func -> C_Ch0_4_Time.c
 
-Recall: CPP_ch_0_useful_lib_func -> C_Ch0_4_Time .c
-
-CLOCKS_PER_SEC is a constant in C/C++ that defines the number of clock ticks per second used by the clock() function. It is used to convert the processor time returned by clock() into seconds. For example, dividing the value returned by clock() by CLOCKS_PER_SEC gives the CPU time in seconds
-
+                CLOCKS_PER_SEC is a constant in C/C++ that defines the "number of clock ticks per second" used by the clock() function. 
+                    It is used to convert the "PROCESSOR time" returned by clock() into seconds. 
+                    For example, dividing the value returned by clock() by CLOCKS_PER_SEC gives the "CPU time" in seconds
 
 */
 
@@ -387,9 +385,9 @@ stopwatch :: stopwatch(){
 }
 
 // constructor 2: with parameter
-stopwatch :: stopwatch( clock_t t){
-begin = ( double ) t / CLOCKS_PER_SEC ;
-end = 0.0;
+stopwatch :: stopwatch(clock_t t){
+    begin = (double) t / CLOCKS_PER_SEC ;
+    end = 0.0;
 }
 
 // Destructor
@@ -420,9 +418,15 @@ int main(){
     long i;
 
     watch.start();
-    for(i =0; i<320000; i++); // time a for loop
+    for(i=0; i<320000; i++); // time a for loop
     watch.stop();
     watch.show();
+
+    // create object using initial value
+    stopwatch s2(clock());
+    for(i=0; i<250000; i++);  // time a for loop
+    s2.stop();
+    s2.show();
 
     return 0;
 }
@@ -439,54 +443,4 @@ int main(){
 
 */
 
-
-
-
-
-
-stopwatch :: stopwatch ()
-{
-begin = end = 0.0;
-}
-
-stopwatch :: stopwatch ( clock_t t)
-{
-begin = ( double ) t / CLOCKS_PER_SEC ;
-end = 0.0;
-}
-
-stopwatch ::~ stopwatch ()
-{
-cout << " Stopwatch object being destroyed ... ";
-show ();
-}
-
-void stopwatch :: start ()
-{
-begin = ( double ) clock () / CLOCKS_PER_SEC ;
-}
-void stopwatch :: stop ()
-{
-end = ( double ) clock () / CLOCKS_PER_SEC ;
-}
-void stopwatch :: show ()
-{
-cout << " Elapsed time : " << end - begin ;
-cout << "\n";
-}
-int main ()
-{
-stopwatch watch ;
-long i;
-watch . start ();
-for (i =0; i <320000; i++); // time a for loop
-watch . stop ();
-watch . show ();
-// create object using initial value
-stopwatch s2( clock ());
-for (i =0; i <250000; i++); // time a for loop
-s2. stop ();
-s2. show ();
-return 0;
-}
 
