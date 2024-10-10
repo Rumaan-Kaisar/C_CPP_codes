bca71528b2b23bf4d7f1c4f8fc35361962642722
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-10 14:26:08 +0600

    Update ch11_02_default_args.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_02_default_args.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_02_default_args.cpp
@@ -1,11 +1,67 @@
 
 /*  ------------------------    Default Arguments    ------------------------
 
+    Default Arguments in Functions:
+        Default arguments allow you to assign a default value to a function parameter, 
+            so if a function is called "without specifying arguments" for those parameters, the default values are used. 
 
-11.5 Default arguments
-When a function (having one or more parameter) is called without specifying corresponding arguments the default arguments allows us to give default value to a parameter/s. Using default arguments is essentially a shorthand form of function overloading.
-	To give a parameter a default argument, simply follow that parameter with an equal sign and the value you want it to default to if no corresponding argument is present. Example: Following function gives its two parameters default values of 0:
-void f(int a=0, int b=0) ;
+        Using default arguments is essentially a "shorthand form" of function overloading.
+
+
+    Assign a default argument:
+        To assign a default argument, follow the parameter with an equal sign (=) and specify the default value. 
+        For example, the function below provides default values of 0 for both of its parameters:
+
+                void f(int a = 0, int b = 0);
+
+        If the function f() is called without arguments, it will use the default values a = 0 and b = 0.
+
+
+
+
+----  GPT more  ----
+here are a few important points to know about default arguments in C++:
+
+Order of Default Arguments: When using default arguments, any parameter with a default value must be to the right of those without. For example, this is correct:
+
+cpp
+Copy code
+void f(int a, int b = 0);
+But this is incorrect:
+
+cpp
+Copy code
+void f(int a = 0, int b);
+Omission of Arguments: When calling a function with default arguments, you can omit only the arguments from the rightmost side. For example, in the function:
+
+cpp
+Copy code
+void f(int a = 0, int b = 1, int c = 2);
+You can call:
+
+cpp
+Copy code
+f();          // uses a=0, b=1, c=2
+f(10);        // uses a=10, b=1, c=2
+f(10, 20);    // uses a=10, b=20, c=2
+f(10, 20, 30); // uses a=10, b=20, c=30
+But you cannot skip middle arguments without skipping the ones after them.
+
+Declaration in Function Prototype: Default arguments should typically be placed in the function declaration (prototype) and not in the function definition itself. For instance:
+
+cpp
+Copy code
+void f(int a = 0, int b = 0);  // Default values in the declaration
+void f(int a, int b) {
+    // function body
+}
+Mixing with Function Overloading: You can mix default arguments and function overloading. However, care should be taken because they might conflict if multiple overloaded functions match a call due to default arguments.
+
+Defaults Across Multiple Files: If you declare a function with default arguments in a header file and define it in a source file, place the default values only in the header file to avoid redefinition errors.
+
+
+
+
 	Notice that this syntax is similar to variable initialization. This function can now be called three different ways. 
 [1]	First, it can be called with both arguments specified. Example: f(10, 99); 		/* a is 10, b is 99*/ 
 [2]	Second, it can be called with only the first argument specified. In this case, b will default to 0. Example: 
