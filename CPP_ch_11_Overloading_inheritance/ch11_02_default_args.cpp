02bb3fbe18e8a3fc487ed6bc9aac26dd9f393964
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-10-11 10:12:40 +0600

    Update ch11_02_default_args.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_02_default_args.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_02_default_args.cpp
@@ -50,44 +50,34 @@
                     void example(int a = 5, int b)      // Incorrect
 
 
-
-	Notice that this syntax is similar to variable initialization. This function can now be called three different ways. 
-[1]	First, it can be called with both arguments specified. Example: f(10, 99); 		/* a is 10, b is 99*/ 
-[2]	Second, it can be called with only the first argument specified. In this case, b will default to 0. Example: 
-f(10); 	/* a is 10, b defaults to 0 */
-[3]	Finally, f() can be called with no arguments, causing both a and b to default to 0. Example: 
-f(); 		/* a and b default to 0*/
+        Also specified parameters can't stay left side of the default parameters
+            i.e. Once you begin to define default parameters, you cannot specify any parameters that have no defaults
 
 
+        Once the "first default argument" is specified, all following arguments must also have default values. 
+            You can't skip some arguments. 
+            For example, following will cause a compile-time error because b doesn't have a default:
 
+                    void f(int a = 0, int b)  // wrong! b must have a default too
+
+
+        You can "omit" only the arguments from the "rightmost side". 
+        You cannot skip middle arguments without skipping the ones after them.
+            
+            For example, in the function:
+                            void f(int a = 0, int b = 1, int c = 2);
+
+            You can call:
+                            f();          // uses a=0, b=1, c=2
+                            f(10);        // uses a=10, b=1, c=2
+                            f(10, 20);    // uses a=10, b=20, c=2
+                            f(10, 20, 30); // uses a=10, b=20, c=30
+
+
+
+// --------    rev point 1: [11-Oct-2024]    --------
 
 ----  GPT more  ----
-here are a few important points to know about default arguments in C++:
-
-Order of Default Arguments: When using default arguments, any parameter with a default value must be to the right of those without. For example, this is correct:
-
-cpp
-Copy code
-void f(int a, int b = 0);
-But this is incorrect:
-
-cpp
-Copy code
-void f(int a = 0, int b);
-Omission of Arguments: When calling a function with default arguments, you can omit only the arguments from the rightmost side. For example, in the function:
-
-cpp
-Copy code
-void f(int a = 0, int b = 1, int c = 2);
-You can call:
-
-cpp
-Copy code
-f();          // uses a=0, b=1, c=2
-f(10);        // uses a=10, b=1, c=2
-f(10, 20);    // uses a=10, b=20, c=2
-f(10, 20, 30); // uses a=10, b=20, c=30
-But you cannot skip middle arguments without skipping the ones after them.
 
 Declaration in Function Prototype: Default arguments should typically be placed in the function declaration (prototype) and not in the function definition itself. For instance:
 
@@ -107,11 +97,8 @@ Defaults Across Multiple Files: If you declare a function with default arguments
 // --------    rev point 2: [10-Oct-2024]    --------
 
 
-	It is clear that there is no way to default a and specify b.
-	There are several rules to specify default arguments :
-	Default arguments must be specified only once: either in the function's prototype or in its definition if the definition precedes the function's first use. The defaults cannot be specified in both the prototype and the definition. 
-	All default parameters must be to the right side of any parameters that don't have defaults.  Once you begin to define default parameters, you cannot specify any parameters that have no defaults (i.e. specified parameters can't stay left side of the default parameters).
-	Default arguments must be constants or global variables. They cannot be local variables or other parameters.
+
+
 Here is a program that illustrates the example described in the preceding discussion:
 
 void f(int a=0, int b=0){
@@ -123,8 +110,11 @@ cout <<'\n';}	int main(){	f();
 	a: 0, b: 0
 	a: 10, b: 0
 	a: 10, b: 99
-	All arguments must specified as default:  We can't make any specific argument to default. Once the first default argument is specified, all following parameters must have defaults as well. For example, this slightly different version of f() causes a compile- time error:
+
+	All arguments must specified as default:  We can't just make any specific argument to default. Once the first default argument is specified, all following parameters must have defaults as well. For example, this slightly different version of f() causes a compile- time error:
 void f(int a=0, int b) 		/* wrong! b must have default , too */ 
+
+
 Many times a constructor is overloaded simply to allow both initialized and uninitialized objects to be created. In many cases, you can avoid overloading a constructor by giving it one or more default arguments. For example,
 
 myclass(int n = 0) { x = n; } 	/* default argument instead of overloading constructor */
@@ -148,4 +138,6 @@ Note
 
 
 
-*/  
+*/
+
+
