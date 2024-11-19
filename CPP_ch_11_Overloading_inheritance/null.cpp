082df378264f301f345121901c12fd5eec53f093
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-11-19 13:03:30 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -197,5 +197,206 @@ Example 6: What is a copy constructor and under what circumstances is it called?
 
 // ------------    Cumulative/Masery/Review skill check    ------------
 
+1. // Overload date () for time_t .
+# include <iostream >
+# include <cstdio > // included for sscanf ()
+# include <ctime >
+using namespace std ;
+class date
+{
+int day , month , year ;
+public :
+date ( char * str );
+date ( int m, int d, int y)
+{
+day = d;
+month = m;
+year = y;
+}
+// overload for parameter of type time_t
+date ( time_t t);
+void show ()
+{
+cout << month << ’/’ << day << ’/’;
+cout << year << ’\n’;
+}
+};
+date :: date ( char * str )
+{
+sscanf (str , "%d%*c%d%*c%d", &month , &day , & year );
+}
+date :: date ( time_t t)
+{
+struct tm *p;
+p = localtime (&t); // convert to broken down time
+day = p-> tm_mday ;
+month = p-> tm_mon ;
+year = p-> tm_year ;
+}
+int main ()
+{
+471TEACH YOURSELF
+C++
+// construct date object using string
+date sdate (" 12/31/99 ");
+// construct date object using integers
+date idate (12 , 31, 99) ;
+/*
+construct date object using time_t - this
+creates an object using the system date
+*/
+date tdate ( time ( NULL ));
+sdate . show ();
+idate . show ();
+tdate . show ();
+return 0;
+}
+2. The class samp defines only one constructor, and this constructor requires an initializer.
+Therefore, it is improper to declare an object of type samp without one. (That is, samp
+x is an invalid declaration.)
+3. One reason to overload a constructor is to provide flexibility, allowing you to choose the
+most appropriate constructor in the specific instance. Another is to allow both initialized
+and uninitialized objects to be declared. You might want to overload a constructor so
+that dynamic arrays can be allocated.
+4. The most common general form of a copy constructor is shown here:
+classname ( const classname &obj)
+{
+// body of constructor
+}
+5. A copy constructor is called when an initialization takes place-specifically, when one object
+is explicitly used to initialize another, when an object is passed as a parameter to a
+function, and when a temporary object is created when an object is returned by a function.
+6. The overload keyword is obsolete. In early versions of C++ it was used to tell the
+compiler that a function will be overloaded. It is not supported by modern compilers.
+7. A default argument is a value that is given to a function parameter when no corresponding
+argument appears when the function is called.
+8. # include <iostream >
+# include <cstring >
+using namespace std ;
+void reverse ( char *str , int count = 0);
+int main ()
+{
+char s1 [] = " This is a test .";
+472ANSWERS
+CUMULATIVE SKILLS CHECK: Chapter 5
+char s2 [] = "I like C++. ";
+reverse (s1); // reverses entire string
+reverse (s2 , 7); // reverse first 7 chars
+cout << s1 << ’\n’;
+cout << s2 << ’\n’;
+return 0;
+}
+void reverse ( char *str , int count )
+{
+int i, j;
+char temp ;
+if (! count )
+count = strlen (str) - 1;
+for (i=0, j= count ; i<j; i++, j --)
+{
+temp = str [i];
+str [i] = str [j];
+str [j] = temp ;
+}
+}
+9. All parameters receiving default arguments must appear to the right of those that do not.
+10. Ambiguity can be introduced by default type conversions, reference parameters, and default arguments.
+11. It is ambiguous because the compiler cannot know which version of compute() to call.
+Is it the first version, with divisor defaulting? Or is it the second version, which takes
+only one parameter?
+12. When you are obtaining the address of an overloaded function, it is the type specification
+of the pointer that determines which function is used.
 
+
+
+
+
+
+CUMULATIVE SKILLS CHECK: Chapter 5
+1. # include <iostream >
+using namespace std ;
+void order ( int &a, int &b)
+{
+int t;
+if(a<b)
+return ;
+else // swap a and b
+{
+t = a;
+473TEACH YOURSELF
+C++
+a = b;
+b = t;
+}
+}
+int main ()
+{
+int x=10 , y =5;
+cout << "x: " << x << ", y: " << y << ’\n’;
+order (x, y);
+cout << "x: " << x << ", y: " << y << ’\n’;
+return 0;
+}
+2. The syntax for calling a function that takes a reference parameter is identical to the syntax
+for calling a function that takes a value parameter.
+3. A default argument is essentially a shorthand approach to function overloading because
+the net result is the same.
+For example,
+int f( int a, int b = 0);
+is functionally equivalent to these two overloaded functions:
+int f( int a);
+int f( int a, int b);
+4. # include <iostream >
+using namespace std ;
+class samp
+{
+int a;
+public :
+samp () { a = 0; }
+samp ( int n) { a = n; }
+int get_a () { return a; }
+};
+int main ()
+{
+samp ob (88) ;
+samp obarray [10];
+// ...
+}
+474ANSWERS
+REVIEW SKILLS CHECK: Chapter 6
+5. Copy constructors are needed when you, the programmer, must control precisely how a
+copy of an object is made. This is important only when the default bitwise copy creates
+undesired side effects.
+
+
+
+
+
+
+
+REVIEW SKILLS CHECK: Chapter 6
+1. class myclass
+{
+int x, y;
+public :
+myclass ( int i, int j) { x=i; y=j; }
+myclass () { x =0; y =0; }
+};
+2. class myclass
+{
+int x, y;
+public :
+myclass ( int i=0, int j=0) { x=i; y=j; }
+};
+3. Once default arguments have begun, a non-defaulting parameter cannot occur.
+4. A function cannot be overloaded when the only difference is that one version takes a value
+parameter and the other takes a reference parameter. (The compiler cannot tell them
+apart.)
+5. It is appropriate to use default arguments when there are one or more values that will
+occur frequently. Is is inappropriate when there is no value or values that have a greater
+likelihood of occurring.
+6. No, because there is no way to initialize a dynamic array. This class has only one constructor, and it requires initializers.
+7. A copy constructor is a special constructor that is called when one object initializes another. This circumstance can occur in any of the following three ways: When one object
+is explicitly used to initialize another, when an object is passed to a function, or when a
+temporary object is created as a function return value.
 
