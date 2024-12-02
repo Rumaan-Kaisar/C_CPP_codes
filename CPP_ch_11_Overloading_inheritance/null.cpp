9cc679121cfe3bd186529045437463c8f863f3e6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-02 20:33:37 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -10,52 +10,6 @@
 // -=-=-=-=-=-=-=-    Review Skills Check    -=-=-=-=-=-=-=-
 
 
-
-/* Example 1: Show how to overload the constructor for the following class so that uninitialized objects
-can also be created. (When creating uninitialized objects, give x and y the value 0.)
-class myclass
-{
-int x, y;
-public :
-myclass ( int i, int j) { x=i; y=j; }
-//
-};
- */
-1. class myclass
-{
-int x, y;
-public :
-myclass ( int i, int j) { x=i; y=j; }
-myclass () { x =0; y =0; }
-};
-
-
-
-/* Example 2: Using the class from Question 1, show how you can avoid overloading myclass() by using
-default arguments. */
-2. class myclass
-{
-int x, y;
-public :
-myclass ( int i=0, int j=0) { x=i; y=j; }
-};
-
-
-
-/* Example 3: What is wrong with these two overloaded functions?
-void f( int a);
-void f( int &a); */
-3. Once default arguments have begun, a non-defaulting parameter cannot occur.
-
-
-
-/* Example 4: When is it appropriate to use default arguments? When is it probably a bad idea? */
-4. A function cannot be overloaded when the only difference is that one version takes a value
-parameter and the other takes a reference parameter. (The compiler cannot tell them
-apart.)
-
-
-
 /* Example 5: Given the following class definition, is it possible to dynamically allocate an array of these
 objects?
 class test
