fed078d5f7de644cb7ddd944c545ffc82f3af381
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-03 12:05:02 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -10,39 +10,7 @@
 // -=-=-=-=-=-=-=-    Review Skills Check    -=-=-=-=-=-=-=-
 
 
-/* Example 5: Given the following class definition, is it possible to dynamically allocate an array of these
-objects?
-class test
-{
-char *p;
-int *q;
-int count ;
-public :
-test ( char *x, int *y, int c)
-{
-p = x;
-q = y;
-count = c;
-}
-// ...
-};
- */
-5. It is appropriate to use default arguments when there are one or more values that will
-occur frequently. Is is inappropriate when there is no value or values that have a greater
-likelihood of occurring.
-
-
-
-/* Example 6: What is a copy constructor and under what circumstances is it called? */
-6. No, because there is no way to initialize a dynamic array. This class has only one constructor, and it requires initializers.
-7. A copy constructor is a special constructor that is called when one object initializes another. This circumstance can occur in any of the following three ways: When one object
-is explicitly used to initialize another, when an object is passed to a function, or when a
-temporary object is created as a function return value.
-
-
-
-
-
-
 // ------------    Cumulative/Masery/Review skill check    ------------
+
+
 
