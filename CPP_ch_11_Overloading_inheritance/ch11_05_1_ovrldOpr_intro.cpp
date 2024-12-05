e996567e077ef953f64da68752d68afbfdc9f52e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2024-12-05 09:25:35 +0600

    Update ch11_05_1_ovrldOpr_intro.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_05_1_ovrldOpr_intro.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_05_1_ovrldOpr_intro.cpp
@@ -1,19 +1,41 @@
 
 /*  ------------------------    operator overloading    ------------------------
+    
+    ------------    The basics of "operator overloading"    ------------
 
-The basics of operator overloading
+    Overloading "Member Operator Functions":
+        Operator overloading is a form of function overloading with extra rules.
+        Operators are overloaded specifically for a "class" or user-defined type.
 
-11.8 Overloading MEMBER OPERATOR FUNCTIONS
-	Operator overloading is really just a type of function overloading with some additional rules apply. 
-	When an operator is overloaded, that operator loses none of its original meaning. Instead, it gains additional meaning relative to the class for which it is defined.
-	For example, an operator is always overloaded relative to a user-defined type, such as a class. 
-	To overload an operator, you create an operator function. Most often an operator function is :
-1.	Member operator function 
-2.	Friend operator function 
-	However, there is a slight difference between a member operator function and a friend operator function. 
+        To overload an operator, you write an operator function, which is often defined as a "member" of the class.
+        Most often an operator function is:
+
+                1. Member operator function
+                2. Friend operator function
+
+
+        Overloading an operator doesn't change its "original meaning".
+        Instead, it gains additional meaning relative to the class for which it is defined.
+
+
+
+    ------------    Member Operator Function and Friend Operator Function    ------------
+
+
+
+
+
+
+
+
+
+
+there is a slight difference between a member operator function and a friend operator function. 
 	The general form of a member operator function is, 
 return_type class_name::operator#( arg_list ) {
 /* operation to be performed */   }
+
+
 	The return type of an operator function is often the class for which it is defined. (operator function may free to return any type.) 
 	The operator being overloaded is substituted for the #. For example, to overloaded + , the function name would be operator+.
 	The contents of arg-list vary depending upon how the operator function is implemented and the type of operator being overloaded.
