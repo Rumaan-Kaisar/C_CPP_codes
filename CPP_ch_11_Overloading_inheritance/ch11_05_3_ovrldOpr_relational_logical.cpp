053ed4ce4884ceb4f3a724a7849dcdae848fe640
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-02 09:50:22 +0600

    Update ch11_05_3_ovrldOpr_relational_logical.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_05_3_ovrldOpr_relational_logical.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_05_3_ovrldOpr_relational_logical.cpp
@@ -78,45 +78,47 @@ int main(){
 
 
 
+/* Example 2: Overload < and > operator relative to the coord class. 
+                Notice the return types of overloaded operators 
+*/
+#include <iostream>
 
-/* 1. Overload < and > operator relative to the coord class */
-1. // Overload the < and > relative to coord class .
-# include <iostream >
-using namespace std ;
+class coord{
+        int x, y;   // coordinate values
+    public:
+        coord(){ x =0; y =0; }
+        coord(int i, int j){ x=i; y=j; }
+        void get_xy(int &i, int &j){ i=x; j=y; }
 
-class coord
-{
-int x, y; // coordinate values
-public :
-coord () { x =0; y =0; }
-coord ( int i, int j) { x=i; y=j; }
-void get_xy ( int &i, int &j) { i=x; j=y; }
-int operator <( coord ob2);
-int operator >( coord ob2);
+        // operator overload
+        int operator <( coord ob2);
+        int operator >( coord ob2);
 };
-// Overload < relative to coord class .
-int coord :: operator <( coord ob2)
-{
-return x< ob2 .x && y< ob2.y;
+
+
+// Overload < relative to coord class
+int coord::operator<(coord ob2){
+    return (x < ob2.x) && (y < ob2.y);
 }
+
+
 // Overload > relative to coord class .
-int coord :: operator >( coord ob2)
-{
-return x> ob2 .x && y> ob2.y;
+int coord :: operator >( coord ob2){
+    return (x > ob2.x) && (y > ob2.y);
 }
-int main ()
-{
-coord o1 (10 , 10) , o2 (5, 3);
-int x, y;
-if(o1 >o2)
-cout << "o1 > o2\n";
-else
-cout << "o1 <= o2\n";
-if(o1 <o2)
-cout << "o1 < o2\n";
-else
-cout << "o1 >= o2\n";
-return 0;
+
+
+int main(){
+    coord o1(10,10), o2(5,3);
+    int x, y;
+
+    if(o1 > o2) std::cout << "o1 > o2\n";
+    else std::cout << "o1 <= o2\n";
+
+    if(o1 < o2) std::cout << "o1 < o2\n";
+    else std::cout << "o1 >= o2\n";
+
+    return 0;
 }
 
 
