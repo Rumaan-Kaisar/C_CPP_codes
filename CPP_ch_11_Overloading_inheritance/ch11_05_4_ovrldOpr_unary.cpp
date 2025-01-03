536e50971f4a0fff3f97dcf17c82cc321f0b23ad
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-03 13:44:22 +0600

    Update ch11_05_4_ovrldOpr_unary.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_05_4_ovrldOpr_unary.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_05_4_ovrldOpr_unary.cpp
@@ -10,7 +10,14 @@
 
 
 
-/* Example 1:  The following program overloads the increment operator (++) relative to the coord class */
+/* Example 1: The following program overloads the increment operator (++) relative to the coord class 
+                The overloaded ++ operator increases the object by 1.
+                It modifies the object it operates on and returns the incremented object.
+                This allows usage in larger statements like o2 = ++o1;.
+
+                While it's not mandatory to overload a unary operator to match its usual behavior, 
+                    it is generally recommended.
+*/
 
 #include <iostream>
 
@@ -44,20 +51,15 @@ int main(){
 }
 
 
-Since the increment operator is designed to increase its operand by 1, the overloaded
-++ modifies the object it operates upon. The function also returns the object that it
-increments. This allows the increment operator to be used as part of a larger statement,
-such as this:
-o2 = ++ o1;
-As with the binary operators, there is no rule that says you must overload a unary operator
-so that it reflects its normal meaning. However, most of the time this is what you will
-want to do
+
+
+
+
+
 
 
 /*  
-	++ is designed to increase its operand by 1, the overloaded ++ modifies the object it operates upon. 
-	The function returns the object that it increments allowing ++ to be used as part of a larger statement, such as: o2 = ++o1;
-	There is no rule that says we must overload a unary operator so that it reflects its normal meaning. 
+
 	In early version of C++ there was no way to determine whether an overloaded ++ or --  preceded or followed its operand. That is these two statements would have been identical:  o1++;  and   ++o1;
 	In modern C++ to distinguish between these two statements we declare following by which the compiler can distinguish.
 coord coord :: operator++( int notused );
@@ -68,8 +70,25 @@ coord coord :: operator++();
 coord coord :: operator++( int notused );
 
 
+In early version of C++, when an increment or decrement operator was overloaded, there
+was no way to determine whether an overloaded ++ or { preceded or followed its operand.
+That is, assuming the preceding program, these two statements would have been identical:
+o1 ++;
+++ o1;
+However, the modern specification for C++ has defined a way by which the compiler can
+distinguish between these two statements. To accomplish this, create two versions of the
+operator++() function. The first is defined as shown in the preceding example. The
+second is declared like this:
+coord coord :: operator ++( int notused );
+If the ++ operator precedes its operand, the operator++() function is called. However,
+if the ++ follows its operand, the operator++(int notused) function is used. In this
+case, notused will always be passed the value 0. Therefor, if the difference between prefix
+and postfix increment or decrement is important to your class objects, you will need to
+implement both operator functions.
 
-	Example 2 (Use – as "unary" and "binary" both in a program):  The solution is: you simply overload it twice, once as a binary operator and once as a unary operator. This program shows how:
+*/
+
+/* 	Example 2 (Use – as "unary" and "binary" both in a program):  The solution is: you simply overload it twice, once as a binary operator and once as a unary operator. This program shows how: */
 
 class coord { 
 	public: 	
@@ -88,6 +107,9 @@ int main() { coord o1(10, 10), o2(5, 7);	  int x, y;
 	o1=-o1;  	o1.get_xy(x, y);  cout<< "(-o1) X:" << x << ", Y: " << y << "\n"; // negation 
 	return 0; }
 
+
+
+/*  
 	This difference in the number of parameters is what makes it possible for the minus to be overloaded for both operations. 
 	When the minus is overloaded as a binary operator, it takes one parameter. When the minus sign is used as a binary operator, the operator-(coord ob2) function is called. 
 	When it is overloaded as a unary operator, it takes no parameter. When it is used as a unary minus, the operator-() function is called.
