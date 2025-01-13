b5773810c8f9808a1cffab64148d6b9689b77259
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-01-13 15:15:20 +0600

    Update ch11_06_ovrldOpr_friend.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_06_ovrldOpr_friend.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_06_ovrldOpr_friend.cpp
@@ -64,7 +64,7 @@ int main(){
 // rev[10-Jan-2025]
 
 /* Example 2: All else being equal, there is no reason to use a "friend" function instead of a "member" operator function, 
-                except for one important exception discussed in the following programs. 
+                except for one important exception discussed in the following program. 
 */
 
 
@@ -97,6 +97,82 @@ int main() { coord o1(10, 10); int x, y;
   o1=o1+10;  	o1.get_xy(x, y); 	cout<< "(o1 +10) X:"<< x <<", Y:"<< y <<"\n"; /* object + integer */ 
   o1=99+o1;  	o1.get_xy(x, y); 	cout<< "(99+ o1) X:"<< x <<", Y:"<< y <<"\n"; /* integer + object  */
 return 0; }
+
+
+
+2. Overloading an operator by using a friend provides one very important feature that member functions do not. Using a friend operator function, you can allow objects to be used
+in operations involving built-in types in which the built-in type is on the left side of the
+operator. As you saw earlier in this chapter, it is possible to overload a binary member
+operator function such that the left operand is an object and the right operand is a built-in
+type. But it is not possible to use a member function to allow the built-in type to occur
+on the left side of the operator. For example, assuming an overload member operator
+function, the first statement shown here is legal; the second is not:
+ob1 = ob2 + 10; // legal
+ob1 = 10 + ob2 ; // illegal
+While it is possible to organize such statements like the first, always having to make sure
+that the object is on the left side of the operand and the built-in type on the right can be
+cumbersome restriction. The solution to this problem is to make the overloaded operator
+functions friends and define both possible situations.
+As you know, a friend operator function is explicitly passed both operands. Thus, it is
+possible to define one overloaded friend function so that the left operand is an object and
+the right operand is the other type. Then you could overload the operator again with the
+left operand being the built-in type and the right operand being the object. The following
+program illustrates this method:
+// Use friend operator functions to add flexibility .
+# include <iostream >
+using namespace std ;
+class coord
+{
+int x, y; // coordinate values
+public :
+coord () { x =0; y =0; };
+coord ( int i, int j) { x=i; y=j; }
+void get_xy ( int &i, int &j) { i=x; j=y; }
+friend coord operator +( coord ob1 , int i);
+friend coord operator +( int i, coord ob1 );
+};
+// Overload + for ob + int
+coord operator +( coord ob1 , int i)
+{
+coord temp ;
+temp .x = ob1 .x + i;
+temp .y = ob1 .y + i;
+return temp ;
+}
+// Overload + for int + ob
+coord operator +( int i, coord ob1)
+{
+coord temp ;
+161TEACH YOURSELF
+C++
+temp .x = ob1 .x + i;
+temp .y = ob1 .y + i;
+return temp ;
+}
+int main ()
+{
+coord o1 (10 , 10) ;
+int x, y;
+o1 = o1 + 10; // object + integer
+o1. get_xy (x, y);
+cout << "(o1 +10) X: " << x << ", Y: " << y << "\n";
+o1 = 99 + o1; // integer + object
+o1. get_xy (x, y);
+cout << " (99+ o1) X: " << x << ", Y: " << y << "\n";
+return 0;
+}
+As a result of overloading friend operator functions for both situations, both of these
+statements are now valid:
+o1 = o1 + 10;
+o1 = 99 + o1;
+3.
+
+
+
+
+
+
+
 
 
 
