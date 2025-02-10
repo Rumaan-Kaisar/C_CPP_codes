5f2363def6cb75a5a8338c87857d143ef7071bc5
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-02-10 16:59:58 +0600

    Update ch11_08_ovrldOpr_subscript_sqrBrc.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_08_ovrldOpr_subscript_sqrBrc.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_08_ovrldOpr_subscript_sqrBrc.cpp
@@ -156,35 +156,31 @@ int main() {
 
 
 
-
+// rev[10-Feb-2025]
 
 /* Example 2: It is possible to design the operator[]() function in such a way that 
                 the [] can be used on both the left and right sides of an assignment statement. 
                 
                 To do this, return a "reference" to the element being indexed. 
+                Just as we did to fix the limitations in the previous example.
                 
                 For example, following program makes this change and illustrates its use 
 */
-# include <iostream >
-using namespace std ;
+#include <iostream>
+
 const int SIZE = 5;
-167TEACH YOURSELF
-C++
-class arraytype
-{
-int a[ SIZE ];
-public :
-arraytype ()
-{
-int i;
-for (i =0; i< SIZE ; i++)
-a[i] = i;
-}
-int & operator []( int i)
-{
-return a[i];
-}
+
+class arraytype{
+        int a[SIZE];
+    public:
+        arraytype(){
+            int i;
+            for(i=0; i<SIZE; i++) a[i] = i;
+        }
+        // overload [], with referance
+        int &operator []( int i){ return a[i]; }
 };
+
 int main ()
 {
 arraytype ob;
@@ -199,13 +195,18 @@ for (i =0; i< SIZE ; i ++)
 cout << ob[i] << " ";
 return 0;
 }
-This program displays the following output:
-0 1 2 3 4
-10 11 12 13 14
-Because the operator[ ]() function now returns a reference to they array element indexed
-by i, it can be used on the left side of an assignment to modify an element of the array.
-(Of course, it can still be used on the right side as well.) As you can see, this makes
-objects of arraytype act like normal arrays.
+
+
+/*  This program outputs:
+            0 1 2 3 4
+            10 11 12 13 14
+
+        Since the operator[]() returns a "reference" to the array element, 
+            it allows modification of the array elements, just like normal arrays. 
+        It can still be used on the right side of an assignment as well.
+*/
+
+
 
 
 /*  
