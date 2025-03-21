21678c27d00953d57941f9bceec8e84c171bb107
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-21 19:02:44 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -1,6 +1,6 @@
 
 
-// --------    rev[17-Mar-25]    --------
+// --------    rev[21-Mar-25]    --------
 
 
 // -=-=-=-=-=-=-    Mastery Skills Check    -=-=-=-=-=-=-
@@ -18,90 +18,103 @@
 
 /* Example 4: Given the following partially completed program, fill in the "needed operator functions":
 
-note:
+                Have the "overloaded +" add each element of each operand. 
+                Have the "overloaded -" subtract each element of the "right operand" from the left. 
+                Have the "overloaded ==" return 'true' if each element of each operand is the same 
+                    and return 'false' otherwise.
 
-is array::array(){
-    for(int i=0; i<10; i ++) nums[i] = 0;
-}
+            note:
 
-is same as
+                In following context
 
-array::array(){ int i;
+                        is array::array(){
+                            for(int i=0; i<10; i ++) nums[i] = 0;      
+                            // Scope of i exists only inside the loop.
+                        }
 
-    for(i=0; i<10; i ++) nums[i] = 0;
-}
+                is same as
 
-#include <iostream>
+                        array::array(){ 
+                            int i;      
+                            // Scope of i exists throughout the constructor but is still local to the function.
+                            for(i=0; i<10; i ++) nums[i] = 0;
+                        }
 
-class array{
-        int nums[10];
-    public:
-        array();
-        void set(int n[10]);
-        void show();
-        
-        // overload +, -, ==
-        array operator+(array ob2);
-        array operator-(array ob2);
-        array operator==(array ob2);
-};
-
-
-array::array(){
-    for(int i=0; i<10; i ++) nums[i] = 0;
-}
-
-
-void array :: set ( int *n)
-{
-int i;
-for (i =0; i <10; i ++)
-nums [i] = n[i];
-}
-
-
-void array :: show ()
-
-{
-int i;
-for (i =0; i <10; i ++)
-cout << nums [i] << ’ ’;
-cout << "\n";
-}
-
-
-// Fill in operator functions.
+                However, the scope of i is different.
+                The first one is generally preferred for better scoping and readability.
 
 
 
-int main ()
-{
-array o1 , o2 , o3;
-int i [10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
-o1. set (i);
-o2. set (i);
-o3 = o1 + o2;
-o3. show ();
-o3 = o1 - o3;
-o3. show ();
-if(o1 == o2)
-cout << "o1 equals o2\n";
-else
-cout << "o1 does not equal o2\n";
-if(o1 == o3)
-cout << "o1 equals o3\n";
-else
-cout << "o1 does not equal o3\n";
-return 0;
-}
-Have the overloaded + add each element of each operand. Have the overloaded - subtract
-each element of the right operand from the left. Have the overloaded == return true if
-each element of each operand is the same and return false otherwise.
- */
+                // overload +, -, == for array objects
+                #include <iostream>
+
+                class array{
+                        int nums[10];
+                    public:
+                        array();
+                        void set(int n[10]);
+                        void show();
+                        
+                        // overload +, -, ==
+                        array operator+(array ob2);
+                        array operator-(array ob2);
+                        array operator==(array ob2);
+                };
+
+
+                array::array(){
+                    int i;
+                    for(i=0; i<10; i ++) nums[i] = 0;
+                }
+
+
+                void array::set(int *n){
+                    int i;
+                    for(i=0; i<10; i++) nums[i] = n[i];
+                }
+
+
+                void array::show(){
+                    int i;
+                    for (i =0; i<10; i++) std::cout << nums[i] << ' ';
+                    std::cout << "\n";
+                }
+
+
+                // Fill in operator functions.
+
+
+                int main(){
+                    array o1, o2, o3;
+                    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+                    o1.set(i);
+                    o2.set(i);
+
+                    o3 = o1 + o2;
+                    o3.show();
+
+                    o3 = o1 - o3;
+                    o3.show();
+
+                    if(o1 == o2) 
+                        std::cout << "o1 equals o2\n";
+                    else 
+                        std::cout << "o1 does not equal o2\n";
+
+                    if(o1 == o3) 
+                        std::cout << "o1 equals o3\n";
+                    else
+                        std::cout << "o1 does not equal o3\n";
+
+                    return 0;
+                }
+
+*/
 
 
 
-4. # include <iostream >
+# include <iostream >
 using namespace std ;
 class array
 {
