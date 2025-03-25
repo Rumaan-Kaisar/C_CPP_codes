4f45dbd79f0ae69f67996afbb672a2da248ce293
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-25 14:32:46 +0600

    Update ch11_05_3_ovrldOpr_relational_logical.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_05_3_ovrldOpr_relational_logical.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_05_3_ovrldOpr_relational_logical.cpp
@@ -123,3 +123,192 @@ int main(){
 
 
 
+
+/* Example 3: Given the following partially completed program, fill in the "needed operator functions":
+
+                Have the "overloaded +" add each element of each operand. 
+                Have the "overloaded -" subtract each element of the "right operand" from the left. 
+                Have the "overloaded ==" return 'true' if each element of each operand is the same 
+                    and return 'false' otherwise.
+
+                note 1:
+
+                    In following context
+
+                            is array::array(){
+                                for(int i=0; i<10; i ++) nums[i] = 0;      
+                                // Scope of i exists only inside the loop.
+                            }
+
+                    is same as
+
+                            array::array(){ 
+                                int i;      
+                                // Scope of i exists throughout the constructor but is still local to the function.
+                                for(i=0; i<10; i ++) nums[i] = 0;
+                            }
+
+                    However, the scope of i is different.
+                    The first one is generally preferred for better scoping and readability.
+
+
+                note 2:
+                    In C++, you can change the return type of an overloaded operator to a basic data type instead of a user-defined type. 
+                    This allows operators to return either basic or user-defined types based on the requirements.
+                    So we can change "array operator==(array ob2);"  to  "int operator==(array ob2);"
+
+
+
+                // overload +, -, == for array objects
+                #include <iostream>
+
+                class array{
+                        int nums[10];
+                    public:
+                        array();
+                        void set(int n[10]);
+                        void show();
+                        
+                        // overload +, -, ==
+                        array operator+(array ob2);
+                        array operator-(array ob2);
+                        array operator==(array ob2);
+                };
+
+
+                array::array(){
+                    int i;
+                    for(i=0; i<10; i ++) nums[i] = 0;
+                }
+
+
+                void array::set(int *n){
+                    int i;
+                    for(i=0; i<10; i++) nums[i] = n[i];
+                }
+
+
+                void array::show(){
+                    int i;
+                    for(i =0; i<10; i++) std::cout << nums[i] << ' ';
+                    std::cout << "\n";
+                }
+
+
+                // Fill in operator functions.
+
+
+                int main(){
+                    array o1, o2, o3;
+                    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+                    o1.set(i);
+                    o2.set(i);
+
+                    o3 = o1 + o2;
+                    o3.show();
+
+                    o3 = o1 - o3;
+                    o3.show();
+
+                    if(o1 == o2) 
+                        std::cout << "o1 equals o2\n";
+                    else 
+                        std::cout << "o1 does not equal o2\n";
+
+                    if(o1 == o3) 
+                        std::cout << "o1 equals o3\n";
+                    else
+                        std::cout << "o1 does not equal o3\n";
+
+                    return 0;
+                }
+
+*/
+
+// overload +, -, == for array objects
+#include <iostream>
+
+class array{
+        int nums[10];
+    public:
+        array();
+        void set(int n[10]);
+        void show();
+        
+        // overload +, -, ==
+        array operator+(array ob2);
+        array operator-(array ob2);
+        int operator==(array ob2);
+};
+
+
+array::array(){
+    int i;
+    for(i=0; i<10; i ++) nums[i] = 0;
+}
+
+
+void array::set(int *n){
+    int i;
+    for(i=0; i<10; i++) nums[i] = n[i];
+}
+
+
+void array::show(){
+    int i;
+    for(i =0; i<10; i++) std::cout << nums[i] << ' ';
+    std::cout << "\n";
+}
+
+
+// implement overloading: Fill in operator functions.
+array array::operator+(array ob2){
+    int i;
+    array temp;
+    for(i=0; i<10; i++) temp.nums[i] = nums[i] + ob2.nums[i];
+    return temp;
+}
+
+
+array array::operator-(array ob2){
+    int i;
+    array temp;
+    for(i=0; i<10; i++) temp.nums[i] = nums[i] - ob2.nums[i];
+    return temp;
+}
+
+
+int array::operator==(array ob2){
+    int i;
+    for(i=0; i<10; i++) if(nums[i] != ob2.nums[i]) return 0;
+    return 1;
+}
+
+
+int main(){
+    array o1, o2, o3;
+    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+    o1.set(i);
+    o2.set(i);
+
+    o3 = o1 + o2;
+    o3.show();
+
+    o3 = o1 - o3;
+    o3.show();
+
+    if(o1 == o2) 
+        std::cout << "o1 equals o2\n";
+    else 
+        std::cout << "o1 does not equal o2\n";
+
+    if(o1 == o3) 
+        std::cout << "o1 equals o3\n";
+    else
+        std::cout << "o1 does not equal o3\n";
+
+    return 0;
+}
+
