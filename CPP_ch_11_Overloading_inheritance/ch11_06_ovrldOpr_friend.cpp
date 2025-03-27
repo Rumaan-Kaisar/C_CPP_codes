7e08043cbf11af99eed188a05e41214ac0be077e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-27 12:36:30 +0600

    Update ch11_06_ovrldOpr_friend.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_06_ovrldOpr_friend.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_06_ovrldOpr_friend.cpp
@@ -832,3 +832,183 @@ int main() {
 }
 
 
+
+
+/* Example 14: Convert the solution from "Example 3" from "ch11_05_3_ovrldOpr_relational_logical.cpp" 
+                so it overloads the operators by using friend functions. 
+
+
+                // overload +, -, == for array objects
+                #include <iostream>
+
+                class array{
+                        int nums[10];
+                    public:
+                        array();
+                        void set(int n[10]);
+                        void show();
+                        
+                        // overload +, -, ==
+                        array operator+(array ob2);
+                        array operator-(array ob2);
+                        int operator==(array ob2);
+                };
+
+
+                array::array(){
+                    int i;
+                    for(i=0; i<10; i ++) nums[i] = 0;
+                }
+
+
+                void array::set(int *n){
+                    int i;
+                    for(i=0; i<10; i++) nums[i] = n[i];
+                }
+
+
+                void array::show(){
+                    int i;
+                    for(i =0; i<10; i++) std::cout << nums[i] << ' ';
+                    std::cout << "\n";
+                }
+
+
+                // implement overloading: Fill in operator functions.
+                array array::operator+(array ob2){
+                    int i;
+                    array temp;
+                    for(i=0; i<10; i++) temp.nums[i] = nums[i] + ob2.nums[i];
+                    return temp;
+                }
+
+
+                array array::operator-(array ob2){
+                    int i;
+                    array temp;
+                    for(i=0; i<10; i++) temp.nums[i] = nums[i] - ob2.nums[i];
+                    return temp;
+                }
+
+
+                int array::operator==(array ob2){
+                    int i;
+                    for(i=0; i<10; i++) if(nums[i] != ob2.nums[i]) return 0;
+                    return 1;
+                }
+
+
+                int main(){
+                    array o1, o2, o3;
+                    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+                    o1.set(i);
+                    o2.set(i);
+
+                    o3 = o1 + o2;
+                    o3.show();
+
+                    o3 = o1 - o3;
+                    o3.show();
+
+                    if(o1 == o2) 
+                        std::cout << "o1 equals o2\n";
+                    else 
+                        std::cout << "o1 does not equal o2\n";
+
+                    if(o1 == o3) 
+                        std::cout << "o1 equals o3\n";
+                    else
+                        std::cout << "o1 does not equal o3\n";
+
+                    return 0;
+                }
+*/
+
+// overload +, -, == for array objects using friend
+#include <iostream>
+
+class array{
+        int nums[10];
+    public:
+        array();
+        void set(int n[10]);
+        void show();
+        
+        // overload +, -, == using friend
+        friend array operator+(array ob1, array ob2);
+        friend array operator-(array ob1, array ob2);
+        friend int operator==(array ob1, array ob2);
+};
+
+
+array::array(){
+    int i;
+    for(i=0; i<10; i ++) nums[i] = 0;
+}
+
+
+void array::set(int *n){
+    int i;
+    for(i=0; i<10; i++) nums[i] = n[i];
+}
+
+
+void array::show(){
+    int i;
+    for(i =0; i<10; i++) std::cout << nums[i] << ' ';
+    std::cout << "\n";
+}
+
+
+// implement overloading: Fill in operator functions. notice no "::array" used
+array operator+(array ob1, array ob2){
+    int i;
+    array temp;
+    for(i=0; i<10; i++) temp.nums[i] = ob1.nums[i] + ob2.nums[i];
+    return temp;
+}
+
+
+array operator-(array ob1, array ob2){
+    int i;
+    array temp;
+    for(i=0; i<10; i++) temp.nums[i] = ob1.nums[i] - ob2.nums[i];
+    return temp;
+}
+
+
+int operator==(array ob1, array ob2){
+    int i;
+    for(i=0; i<10; i++) if(ob1.nums[i] != ob2.nums[i]) return 0;
+    return 1;
+}
+
+
+int main(){
+    array o1, o2, o3;
+    int i[10] = {1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10 };
+
+    o1.set(i);
+    o2.set(i);
+
+    o3 = o1 + o2;
+    o3.show();
+
+    o3 = o1 - o3;
+    o3.show();
+
+    if(o1 == o2) 
+        std::cout << "o1 equals o2\n";
+    else 
+        std::cout << "o1 does not equal o2\n";
+
+    if(o1 == o3) 
+        std::cout << "o1 equals o3\n";
+    else
+        std::cout << "o1 does not equal o3\n";
+
+    return 0;
+}
+
+
