06a2d9b51343e2eae72c477556db783e2c7cab6b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-03-31 15:23:48 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -1,15 +1,32 @@
 
 /*  ------------------------    BASE class access    ------------------------
 
-INHERITANCE basics: 
-    previously discussed in "ch10_02_inheritance_intro.cpp"
+    INHERITANCE basics: 
+        previously discussed in "ch10_02_inheritance_intro.cpp"
+
 
 --------    rev[28-Mar-25]    --------
 
-11.15 INHERITANCE: access control of base class
-	When one class inherits another, it uses this general form:
-class derived_class_name : access base_class_name { // ... }
-	Here access is one of three keywords: public, private, or protected. 
+INHERITANCE: 
+    When one class inherits another, it uses this general form:
+
+                class derived_class_name : access_specifier base_class_name { 
+                    // ... 
+                    // ... 
+                }
+
+        Here access_specifier is one of three keywords: 
+            public, 
+            private, or 
+            protected.
+
+
+// correct GPT ----------------
+    Access specifiers:
+Access specifiers controlls Control ACCESS of base class from derived class
+	
+
+
 	The access specifier determines how elements of the base class are inherited by the derived class. 
 	When the access specifer for the inherited base class is public, all public members of the base become public members of the derived class. 
 	If the access specifier is private, all public members of the base class become private members of the derived class. 
@@ -18,6 +35,8 @@ class derived_class_name : access base_class_name { // ... }
 	Technically, access is optional:		If the specifier is not present,  it is private by default if the derived class is a class. 
 	If the derived class is a struct, public is the default in the absence of an explicit access specifier. 
 	However, we explicitly specify access for the sake of clarity.
+
+
 	Example 1: Here because base is inherited as public, the public members of base- setx() and showx() - become public members of derived and are, therefore, accessible by any other part of the program. Specifically, they are legally called within main().
 class base { int x;
     	public :
