74668af052904abe02c9525b89591f75232bf7e9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-01 21:29:56 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -85,29 +85,29 @@
 class base { int x;
     	public :
 	void setx (int n) { x = n; }
-	void showx() {cout<< x <<'\n';}     };	/* Inherit as public .*/
+	void showx() {cout<< x <<'\n';}     };	// Inherit as public .
 class derived : public base { int y;
   public : void sety (int n) { y = n; }
            void showy() {cout<< y <<'\n';}  };
-	int main() { 	derived ob; 		/* derived type object */
-			ob.setx(10) ; 	/* access member of base class through the derived class's object */ 
-			ob.sety(20) ; 	/* access member of derived class */ 
-			ob.showx(); 		/* access member of base class through the derived class's object */ 
-			ob.showy(); 		/* access member of derived class */ 
+	int main() { 	derived ob; 		// derived type object 
+			ob.setx(10) ; 	// access member of base class through the derived class's object 
+			ob.sety(20) ; 	// access member of derived class 
+			ob.showx(); 		// access member of base class through the derived class's object 
+			ob.showy(); 		// access member of derived class 
 	return 0; }
 	Example 2: It is important to understand that just because a derived class inherits a base as public, it does not mean that the derived class has access to the base's private members. For example, in previous example 1 : 
 
 class derived : public base { int y;
-  			      public:  /* Error! : x is a private member of base and not available within derived .  */
+  			      public:  // Error! : x is a private member of base and not available within derived . 
    void show_sum() {cout<< x+y << '\n';}  
 	In this example, the derived class attempts to access x, which is a private member of base. This is an error because the private parts of a base class remain private to it no matter how it is inherited.
 	Example 3 (with private specifier, public member of base become private to derived): this time derived inherits base as private. 
 
 class base { all same as Example 1     };
-/* Inherit as private .*/
-class derived : private base { same inside };	int main(){ derived ob; 	/* derived type object */
-	ob.setx(10) ; /* ERROR - now setx() private to derived class */ 
-	ob.showx(); /* ERROR - now showx() private to derived class */ 
+// Inherit as private .
+class derived : private base { same inside };	int main(){ derived ob; 	// derived type object 
+	ob.setx(10) ; // ERROR - now setx() private to derived class 
+	ob.showx(); // ERROR - now showx() private to derived class 
 return 0; }
 	Both showx() and setx() become private to derived and are not accessible outside of it. Relative to objects of type derived, they become private.
 	Keep in mind that showx() and setx() are still public within base no matter how they are inherited by some derived class. This means that an object of type base could access these functions anywhere. For example, given this fragment:
@@ -118,11 +118,15 @@ Is legal because base_ob is of type base  and the call to setx() is legal becaus
 class derived : private base { 
 int y;
 public :
-void setxy(int n, int m) { setx(n); y=m; }  /* setx is accessible from within derived */ 
-void showxy() { showx(); cout<< y; }  };    /* show is accessible from within derived */	int main(){
+void setxy(int n, int m) { setx(n); y=m; }  // setx is accessible from within derived  
+void showxy() { showx(); cout<< y; }  };    // show is accessible from within derived 	
+int main(){
 derived ob; // derived type
     ob.setxy(10, 20);
     ob.showxy();
 return 0; }
 
-*/  
+*/
+
+
+
