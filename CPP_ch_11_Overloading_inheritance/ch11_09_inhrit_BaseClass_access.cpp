9d34c7a21f9106ece5a3019c0fd8e904c075cebd
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-08 13:37:54 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -175,6 +175,64 @@ class derived : public base {
 
 
 
+/* Example 3: Here is a variation of  Example 1; this time 'derived' inherits 'base' as "private". 
+                This change causes the program to be in error.
+*/
+
+
+// This program contains an error
+#include <iostream>
+
+
+class base {
+        int x;
+    public :
+        void setx(int n){ x = n; }
+        void showx(){ std::cout<< x <<'\n'; }
+};  
+
+
+// Notice, now "base" is derived as private
+class derived : private base { 
+        int y;
+    public:
+        void sety(int n){ y = n; }
+        void showy(){ std::cout<< y <<'\n'; }
+}
+
+
+
+
+// Inherit base as private .
+class derived : private base
+{
+int y;
+public :
+void sety ( int n) { y = n; }
+void showy () { cout << y << ’\n’; }
+};
+int main ()
+{
+derived ob;
+ob. setx (10) ; // ERROR - now private to derived class
+ob. sety (20) ; // access member of derived class - OK
+ob. showx (); // ERROR - now private to derived class
+ob. showy (); // access member of derived class - OK
+return 0;
+}
+As the comments in this (incorrect) program illustrate, both showx() and setx() become
+private to derived and are not accessible outside of it.
+Keep in mind that showx() and setx() are still public within base no matter how they
+are inherited by some derived class. This means that an object of type base could access
+these functions anywhere. However, relative to objects of type derived, they become
+private. For example, given this fragment:
+base base_ob ;
+base_ob . setx (1) ; // is legal because base_ob is of type base
+the call to setx() is legal because setx() is public within base.
+
+
+
+
 // -----------  rev[07-apr-25]  -----------
 
 /*
