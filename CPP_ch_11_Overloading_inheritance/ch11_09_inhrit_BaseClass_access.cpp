888b506439043c2853d142e29b454150e2df1ebe
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-14 17:20:44 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -281,3 +281,43 @@ int main(){
 
 
 
+
+/* Example 5: Examine this skeleton:
+# include <iostream >
+using namespace std ;
+class mybase
+{
+int a, b;
+public :
+int c;
+void setab ( int i, int j) { a = i; b = j; }
+void getab ( int &i, int &j) { i = a; j = b; }
+};
+class derived1 : public mybase
+{
+// ...
+};
+class derived2 : private mybase
+{
+179TEACH YOURSELF
+C++
+// ...
+};
+int main ()
+{
+derived1 o1;
+derived2 o2;
+int i, j;
+// ...
+}
+Within main(), which of the following statements are legal?
+A. o1. getab (i, j);
+B. o2. getab (i, j);
+C. o1.c = 10;
+D. o2.c = 10; 
+*/
+
+
+
+2. What happens when a public member is inherited as public? What happens when it is
+inherited as private?
