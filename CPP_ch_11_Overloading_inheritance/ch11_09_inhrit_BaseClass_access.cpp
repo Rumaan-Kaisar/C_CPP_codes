d567ad4b14373783641a9f1c1f305b46fef876f4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-14 17:30:05 +0600

    Update ch11_09_inhrit_BaseClass_access.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_09_inhrit_BaseClass_access.cpp
@@ -282,42 +282,54 @@ int main(){
 
 
 
-/* Example 5: Examine this skeleton:
-# include <iostream >
-using namespace std ;
-class mybase
-{
-int a, b;
-public :
-int c;
-void setab ( int i, int j) { a = i; b = j; }
-void getab ( int &i, int &j) { i = a; j = b; }
+/* Example 5: Examine following skeleton-code:
+
+                Within main(), which of the following statements are legal?
+
+                    A. o1.getab(i, j);
+                    B. o2.getab(i, j);
+                    C. o1.c = 10;
+                    D. o2.c = 10; 
+
+            ans:
+                A and C are valid.
+*/
+
+#include <iostream>
+
+class mybase{
+        int a, b;
+    public:
+        int c;
+        void setab(int i, int j){ a = i; b = j; }
+        void getab(int &i, int &j){ i = a; j = b; }
 };
-class derived1 : public mybase
-{
-// ...
+
+class derived1 : public mybase{
+    // ...
 };
-class derived2 : private mybase
-{
-179TEACH YOURSELF
-C++
-// ...
+
+class derived2 : private mybase{
+    // ...
 };
-int main ()
-{
-derived1 o1;
-derived2 o2;
-int i, j;
-// ...
+
+
+int main(){
+    derived1 o1;
+    derived2 o2;
+    int i, j;
+    // ...
 }
-Within main(), which of the following statements are legal?
-A. o1. getab (i, j);
-B. o2. getab (i, j);
-C. o1.c = 10;
-D. o2.c = 10; 
+
+
+
+
+/* Example 6:   What happens when a public member is inherited as public? 
+                What happens when it is inherited as private? 
+
+            ans:
+                A public member of a base remains public in derived class when it is inherited as public. 
+                But a public member of a base becomes a private in derived class if inherited privately.
 */
 
 
-
-2. What happens when a public member is inherited as public? What happens when it is
-inherited as private?
