6e832e8da234c005316605eb402711e13f6ae1c8
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-17 16:31:16 +0600

    Update ch11_10_inhrit_protected_members.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_10_inhrit_protected_members.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_10_inhrit_protected_members.cpp
@@ -13,23 +13,45 @@
         They're usually placed after private (default) members and before public ones in a class.
 
 
+    General form of a CLASS declaration with all ACCESS specifiers:
+
+                class ClassName {  
+                        // private members (default)
+
+                    protected:  
+                        // protected members (optional)
+
+                    public:  
+                        // public members
+                };
+
+
+        A "protected member" in a BASE class:
+            Becomes protected in the derived class if "inherited as public".
+            Becomes private in the derived class if "inherited as private".
+
+        If a base class is "inherited as protected":
+            Its public and protected members become protected in the derived class.
+
+        Private members of the base class always stay private and are not accessible in the derived class.
+
+        protected can also be used in "structs".
+
+*/  
+
+
 // ----  rev[15-Apr-2025]  ----
 
-General form of a class declaration:
 
 
 
+/* 	Example1: This program illustrates how public, private, and protected members of a class can be accessed 
+It also shows: what occurs when protected members are inherited as public: 
 
-class class_name { 		//  private members *
-protected: 	//  protected members (optional)*
-public: 	//  public members *
-};
-	When a protected member of a base class is inherited: 
-	as public by the derived class, it becomes a protected member of the derived class. 
-	as private by the derived class, it becomes a private member of the derived class.
-	When a base inherited as protected: public and protected members of the base class become protected members of the derived class. (Of course, private members of the base class remain private to it and not acessable.)
-	The protected access specifier can also be used with structures.
-	Example1: This program illustrates how public, private, and protected members of a class can be accessed also what occurs when protected members are inherited as public:
+	Because a and b are protected in base and inherited as public by derived, they are available for use by member functions of derived as we used to showabc(). 
+	However, outside of these two classes, a and b are effectively private and inaccessible.
+*/
+
 class base { int t;  //  private member
 protected : 
    int a, b;   //  private but accessible by derived *
@@ -53,16 +75,21 @@ int main(){ base ob_bs; 		//  base type object
 	ob_drv.setc(3) ; 		// ok: c is public member of derived
 	ob_drv.a=3; ob_drv.b=4;   	//  error: a,b is protected member of base but accessible inside derived
 	ob_drv.showabc(); 	return 0;} 		//  accessing a,b inside of derived class
-	Because a and b are protected in base and inherited as public by derived, they are available for use by member functions of derived as we used to showabc(). 
-	However, outside of these two classes, a and b are effectively private and inaccessible.
 
-	Example 2: When a base class is inherited as protected, public and protected members of the base class become protected members of the derived class. For example following statement returns error because of inheriting base as protected instead of public in the class declaration of derived in the preceding program, 
+
+
+
+
+
+
+
+
+
+/* 	Example 2: When a base class is inherited as protected, public and protected members of the base class become protected members of the derived class. 
+For example following statement returns error because of inheriting base as protected instead of public in the class declaration of derived in the preceding program,  */
 
 class derived : protected base { . . . . . .  same . . .  . }
 int main(){ derived ob_drv; 		// derived type object 
 		ob_drv.setab (1, 2);  // ERROR : setab() is now a protected member of base.  setab() is not accessible here . 
 
 Because base is inherited as protected, its public and protected elements become protected members of derived and are therefore inaccessible within main().
-
-
-*/  
