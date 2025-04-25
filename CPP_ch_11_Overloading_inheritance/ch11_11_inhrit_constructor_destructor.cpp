45a444ed07c35ee628d4268c5da80172e7d0dedc
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-04-25 16:43:39 +0600

    Update ch11_11_inhrit_constructor_destructor.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_11_inhrit_constructor_destructor.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_11_inhrit_constructor_destructor.cpp
@@ -1,12 +1,19 @@
 
 /*  ------------------------    Inherit constructors, destructors    ------------------------
+    
+    Constructors, Destructors, and INHERITANCE
+        The BASE class, the DERIVED class, or both may have constructor and/or destructor functions.
+ 
+    When both BASE and DERIVED classes have their own constructors and destructors:
 
-Constructors, Destructors, and INHERITANCE
+Constructor (Base-class first): 
+    The constructor functions are executed in order of derivation. That is, the base class constructor is executed before the constructor in the derived class. Because a base class has no knowledge of any derived class, any initialization it performs is separate from.
 
-11.17 INHERITANCE with Constructors-Destructors
-	The base class, the derived class, or both may have constructor and/or destructor functions. When a base class and a derived class both have constructor and destructor functions: 
-	Constructor (Base-class first): The constructor functions are executed in order of derivation. That is, the base class constructor is executed before the constructor in the derived class. Because a base class has no knowledge of any derived class, any initialization it performs is separate from.
-	Destructor (Derived-class first): The destructor functions are executed in reverse order.  That is, the derived class's destructor is executed before the base class's destructor because the base class underlies the derived class. If the base class's destructor were executed first, it would imply the destruction of the derived class. 
+Destructor (Derived-class first): 
+    The destructor functions are executed in reverse order.  That is, the derived class's destructor is executed before the base class's destructor because the base class underlies the derived class. If the base class's destructor were executed first, it would imply the destruction of the derived class. 
+
+
+
 	Passing arguments: It is possible to passed arguments to either a derived or base class constructor. There are two cases: 
 	Only derived class takes arguments: When only the derived class takes an initialization, arguments are passed to the derived class's constructor in the normal fashion (as we did before).
 	Base class takes arguments along with derived class: to pass an argument to the constructor of the base class, a chain of argument passing is established. 
