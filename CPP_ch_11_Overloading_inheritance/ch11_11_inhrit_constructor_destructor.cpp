ba5bbecebd35bcea0a34b5682418d51e0143227c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-05-06 13:41:10 +0600

    Update ch11_11_inhrit_constructor_destructor.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_11_inhrit_constructor_destructor.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_11_inhrit_constructor_destructor.cpp
@@ -260,29 +260,50 @@ int main(){
 
 
 
-// --------rev[05-May-2025]--------
 
-/* Example 5: ( Base uses the arguments and derived just pass these to base without using  ):   
-	If the derived class does not need an argument, it ignores the argument and simply passes it along. For example, 
-in this fragment, parameter n is not used by derived(). Instead, it is simply passed to base().
+/* Example 5: (Base uses the arguments and derived just pass these to base without using):   
+                If the derived class does not need an argument, it ignores the argument and simply passes it along.
+                
+                For example, in this fragment, parameter n is not used by derived(). Instead, it is simply passed to base().
 */
-class base { int i;
-public :
-base(int n){ cout<< "Constructing base \n"; i=n;}
-~base() { cout<< "Destructing base \n"; }
-void showi() { cout<< i << '\n'; } 
-};	class derived : public base { int j;
-	public :
-derived(int n) : base(n) 	   // pass arg to base 
- 	{cout<< "Constructing derived \n"; j=0; }
-~derived() { cout<< "Destructing derived \n"; }
-void showj() { cout << j << '\n'; } 	};
+#include <iostream>
+
+class base{
+        int i;
+    public:
+        base(int n){
+            std::cout << " Constructing base class \n";
+            i = n;
+        } 
+        ~base(){ std::cout << " Destructing base class \n"; }
+        void showi(){ std::cout << i << '\n';}
+};
+
+
+class derived : public base{
+        int j;
+    public:
+        derived(int n) : base(n){  // pass arg to base class.
+            std::cout << " Constructing derived class \n";
+            j = 0;  // no use of n
+        }
+        ~derived(){ std::cout << " Destructing derived class \n";}
+        void showj(){ std::cout << j << '\n'; }
+};
+
+int main(){
+    derived o(10);
+    
+    o.showi();
+    o.showj();
+    
+    return 0;
+}
 
 
 
 
-
-
+// --------rev[06-May-2025]--------
 
 /* Example 6: Here is an example demonstrating the case where only the base class takes arguments, and the derived class does not need any additional arguments: 
 Explanation:
