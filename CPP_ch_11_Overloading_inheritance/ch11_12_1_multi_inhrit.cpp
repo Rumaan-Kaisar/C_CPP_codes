d06e1e0f523b72d8b002c891a59f11a646e61936
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-05-26 15:41:44 +0600

    Update ch11_12_1_multi_inhrit.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_12_1_multi_inhrit.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_12_1_multi_inhrit.cpp
@@ -289,31 +289,49 @@ int main() {
 
 
 /* Example 5: Using the following class hierarchy, create C's constructor 
-                so that it initializes k and passes on argument to A() and B(). */
-# include <iostream >
-using namespace std ;
-class A
-{
-int i;
-public :
-A( int a) { i = a; }
+                so that it initializes k and passes on argument to A() and B(). 
+
+                    #include <iostream>
+
+                    class A {
+                            int i;
+                        public :
+                            A( int a) { i = a; }
+                    };
+
+                    class B {
+                            int j;
+                        public :
+                            B( int a) { j = a; }
+                    };
+
+                    class C : public A, public B {
+                        int k;
+                            public :
+                            // Create C() so that it initializes k and passes arguments to both A() and B()
+                    };
+*/
+
+#include <iostream>
+
+class A {
+        int i;
+    public :
+        A( int a) { i = a; }
 };
 
-class B
-{
-int j;
-public :
-B( int a) { j = a; }
+class B {
+        int j;
+    public :
+        B( int a) { j = a; }
 };
-class C : public A, public B
-{
-int k;
-public :
-/*
-Create C() so that it initializes k
-and passes arguments to both A() and B()
-*/
+
+class C : public A, public B {
+    int k;
+        public :
+        // Create C() so that it initializes k and passes arguments to both A() and B()
 };
+
 
 
 2. # include <iostream >
