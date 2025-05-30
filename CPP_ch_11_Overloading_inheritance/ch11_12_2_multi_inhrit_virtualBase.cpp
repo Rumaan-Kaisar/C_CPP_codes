ebe72d8700476e27a49a30bbdc11637e953b846b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-05-30 16:51:27 +0600

    Update ch11_12_2_multi_inhrit_virtualBase.cpp

--- CPP_ch_11_Overloading_inheritance/ch11_12_2_multi_inhrit_virtualBase.cpp
+++ CPP_ch_11_Overloading_inheritance/ch11_12_2_multi_inhrit_virtualBase.cpp
@@ -34,58 +34,76 @@
         This ensures only one shared copy of Base exists in "Derived3", no matter how many paths inherit it.
         The "virtual" keyword precedes the base class "access specifier" when it is inherited by a derived class.
 
+        How to declare:
+
+                class Derived1 : virtual public Base { };
+                class Derived2 : virtual public Base { };
+
+            The keyword "virtual" before the access specifier (public) handles this.
 
 
-// rev[29-May-2025]
+        It is important to understand that when a base is inherited as "virtual" by a derived, 
+            that base still exists within that derived. 
 
-Virtual Base Classes
+        The only difference between a "normal base" and a "virtual base" occurs 
+            when an object inherits the base more than once. 
 
-
-
------------------------------------
-
-📌 Example Scenario:
+*/  
 
 
+/* Example 1:  Here virtual base class prevents two copies of base from being present in derived3.
+
+                If derived1 and derived2 had not inherited base as virtual, the statement 
+                            ob.i = 10; 
+                would have been ambiguous and a "compile-time error" would have resulted.
+*/
+
+// This program uses a virtual base class .
+# include <iostream >
+using namespace std ;
+class base
+{
+public :
+int i;
+};
+// Inherit base as virtual .
+class derived1 : virtual public base
+{
+public :
+int j;
+};
+// Inherit base as virtual here , too .
+class derived2 : virtual public base
+{
+public :
+int k;
+};
+/*
+Here , derived3 inherits both derived1 and derived2 .
+However , only one copy of base is present .
+*/
+class derived3 : public derived1 , public derived2
+{
+public :
+int product () { return i * j * k; }
+};
+int main ()
+{
+derived3 ob;
+ob.i = 10; // unambiguous because only one copy present
+ob.j = 3;
+ob.k = 5;
+cout << " Product is " << ob. product () << ’\n’;
+return 0;
+}
+If derived1 and derived2 had not inherited base as virtual, the statement
+ob.i = 10;
+would have been ambiguous and a compile-time error would have resulted. (See Exercise
+1, below.)
+197TEACH YOURSELF
+C++
 
 
-
-📖 How to declare:
-cpp
-Copy
-Edit
-class Derived1 : virtual public Base { };
-class Derived2 : virtual public Base { };
-The keyword virtual before the access specifier (public) handles this.
-
-📌 Summary:
-Problem	Solution
-Multiple indirect copies of a base class	Use virtual base classes to share one copy
-Ambiguity accessing base members	Virtual inheritance prevents this issue
-
-Would you like a full code example for this too?
-
------------------------------------
-
-
-
-
-
-
-Virtual base classes
-
-
-
-
-
-
-	The only difference between a normal base and a virtual base occurs when an object inherits the base more than once. 
-
-
-
-
-
-	Example 1:  Here virtual base class prevents two copies of base from being present in derived3.
 class base {
    public :
        int i;
@@ -109,13 +127,29 @@ return 0; }
 
 
 
-	If derived1 and derived2 had not inherited base as virtual, the statement ob.i = 10; would have been ambiguous and a compile-time error would have resulted.
-	It is important to understand that when a base is inherited as virtual by a derived, that base still exists within that derived. For example, this fragment is perfectly valid:
+
+
+
+
+
+/* 
+
+	It is important to understand that when a base is inherited as "virtual" by a derived, 
+that base still exists within that derived. For example, this fragment is perfectly valid:
 derived1 ob_drv1;  	// we've used derived3 type object in the above
 ob_drv1.i = 100;
 
 
-	The only difference between a normal base and a virtual base occurs when an object inherits the base more than once. 
+2. It is important to understand that when a base class is inherited as virtual by a derived
+class, that base class still exists within that derived class. For example, assuming the
+preceding program, this fragment is perfectly valid:
+derived1 ob;
+ob.i = 100;
+The only difference between a normal base class and a virtual one occurs when an object
+inherits the base more than once. If virtual base classes are used, only one base class is
+present in the object. Otherwise, multiple copies will be found.
+EXERCISES
+1. Using the program in Example 1, remove the virtual keyword and try to compile the
+program. See what type of errors result.
+2. Explain why a virtual base class might be necessary */
 
-
-*/  
