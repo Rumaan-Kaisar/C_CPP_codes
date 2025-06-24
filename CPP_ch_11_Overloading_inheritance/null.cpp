2eaa485c259a677820326db37d386535468b8a9c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-06-24 17:51:38 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -488,88 +488,118 @@ int main() {
 
 // ----  rev[23-Jun-2025]  ----
 
-// cp 1/2
 
 
+/* Example 3: Following is a reworked version of the coord class from the previous 
+                operator overloading codes (eg: ch11_06_ovrldOpr_friend.cpp)
 
+                This time it is used as a "base" for another class called "quad", 
+                    which also maintains the quadrant the specific point is in. 
 
+                So we Overload the +, -, and = relative to "coord" class.
+                    Then use coord as a base for quad.
+                    (recall Example 2 of ch11_05_2_ovrldOpr_binary.cpp)
 
-
-/* Example 3: Following is a reworked version of the coord class from the previous chapter. This time
-it is used as a base for another class called quad, which also maintains the quadrant the
-specific point is in. On your own, run this program and try to understand its output. */
-
-/*
-Overload the +, -, and = relative to coord class . Then
-use coord as a base for quad .
+                On your own, run this program and try to understand its output. 
 */
-# include <iostream >
-using namespace std ;
-201TEACH YOURSELF
-C++
-class coord
-{
-public :
-int x, y; // coordinate values
-coord () { x =0; y =0; }
-coord ( int i, int j) { x=i; y=j; }
-void get_xy ( int &i, int &j) { i=x; j=y; }
-coord operator +( coord ob2);
-coord operator -( coord ob2);
-coord operator =( coord ob2);
+
+
+#include <iostream>
+
+class coord{
+        int x, y; // coordinate values
+    public:
+        coord() { x =0; y =0; }
+        coord(int i, int j) { x=i; y=j; }
+        void get_xy(int &i, int &j) { i=x; j=y; }
+
+        // OPERATOR OVERLOADING: coord type used for overloaded operator
+        coord operator+(coord ob2);
+        coord operator-(coord ob2);
+        coord operator=(coord ob2);
 };
-// Overload + relative to coord class .
-coord coord :: operator +( coord ob2)
-{
-coord temp ;
-cout << " Using coord operator +() \n";
-temp .x = x + ob2 .x;
-temp .y = y + ob2 .y;
-return temp ;
+
+
+// Overload '+' relative to coord class.
+coord coord::operator+(coord ob2) {
+    coord temp;
+    std::cout << " Using coord operator +() \n";
+    temp.x = x + ob2.x;
+    temp.y = y + ob2.y;
+    return temp;
 }
-// Overload - relative to coord class .
-coord coord :: operator -( coord ob2)
-{
-coord temp ;
-cout << " Using coord operator -() \n";
-temp .x = x - ob2 .x;
-temp .y = y - ob2 .y;
-return temp ;
+
+// Overload '-' relative to coord class.
+coord coord::operator-(coord ob2) {
+    coord temp;
+    std::cout << " Using coord operator -() \n";
+    temp.x = x - ob2.x;
+    temp.y = y - ob2.y;
+    return temp;
 }
-// Overload = relative to coord .
-coord coord :: operator =( coord ob2)
-{
-cout << " Using coord operator =() \n";
-x = ob2 .x;
-y = ob2 .y;
-return * this ; // return the object that is assigned to
+
+// Overload '=' relative to coord class.
+coord coord::operator=(coord ob2) {
+    std::cout << " Using coord operator =() \n";
+    x = ob2.x;
+    y = ob2.y;
+    return *this;
+    // return the object that is assigned. Noticem, no "temp" is used
 }
-class quad : public coord
-{
-202INHERITANCE
-SKILLS CHECK
-int quadrant ;
-public :
-quad ()
-{
-x = 0;
-y = 0;
-quadrant = 0;
+
+
+int main(){
+    coord o1(10,10), o2(5,3), o3;
+    int x, y;
+
+    o3 = o1 + o2;   // add two objects - this calls operator +
+    o3.get_xy(x,y);
+    std::cout << "(o1+o2) X: " << x << ", Y: " << y << "\n";
+
+    o3 = o1 - o2;   // subtract two objects
+    o3.get_xy(x,y);
+    std::cout << "(o1-o2) X: " << x << ", Y: " << y << "\n";
+
+    o3 = o1;        // assign an object
+    o3.get_xy(x,y);
+    std::cout << "(o3=o1) X: " << x << ", Y: " << y << "\n";
+
+    return 0;
 }
-quad ( int x, int y) : coord (x, y)
-{
-if(x >=0 && y >= 0)
-quadrant = 1;
-else if(x <0 && y >=0)
-quadrant = 2;
-else if(x <0 && y <0)
-quadrant = 3;
-else
-quadrant = 4;
-}
-void showq ()
-{
-cout << " Point in Quadrant : " << quadrant << ’\n’;
+
+
+
+
+// ----  raw  ----
+
+#include <iostream>
+
+
+
+
+// "quad" class inheriting "coord"
+class quad : public coord {
+        int quadrant;
+    public:
+        quad() {
+            x = 0;
+            y = 0;
+            quadrant = 0;
+        }
+        quad(int x, int y) : coord (x, y) {
+            if(x >=0 && y >= 0) 
+                quadrant = 1;
+            else if(x <0 && y >=0) 
+                quadrant = 2;
+            else if(x <0 && y <0) 
+                quadrant = 3;
+            else 
+                quadrant = 4;
+        }
+
+// ---- rev[]24-Jun-2025 ----
+void showq() {
+    std::cout << " Point in Quadrant : " << quadrant << ’\n’;
 }
 quad operator =( coord ob2 );
 };
@@ -611,8 +641,7 @@ return 0;
 
 
 
-/* Example 4: Again on your own, convert the program shown in Exercise 3 so that it uses friend operator
-functions. */
+/* Example 4: Convert the previous program so that it uses friend operator functions. */
 
 
 
