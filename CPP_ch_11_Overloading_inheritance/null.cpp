3008d90df0212ef46d03a5e565d4883922a2f070
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-06-26 15:02:59 +0600

    Update null.cpp

--- CPP_ch_11_Overloading_inheritance/null.cpp
+++ CPP_ch_11_Overloading_inheritance/null.cpp
@@ -621,7 +621,117 @@ int main() {
 
 
 /* Example 4: Convert the previous program so that it uses friend operator functions. */
+#include <iostream>
 
+class coord{
+        int x, y; // coordinate values
+    public:
+        coord() { x =0; y =0; }
+        coord(int i, int j) { x=i; y=j; }
+        void get_xy(int &i, int &j) { i=x; j=y; }
+
+        // OPERATOR OVERLOADING: coord type used for overloaded operator
+        coord operator+(coord ob2);
+        coord operator-(coord ob2);
+        coord operator=(coord ob2);
+};
+
+
+// Overload '+' relative to coord class.
+coord coord::operator+(coord ob2) {
+    coord temp;
+    std::cout << " Using coord operator +() \n";
+    temp.x = x + ob2.x;
+    temp.y = y + ob2.y;
+    return temp;
+}
+
+// Overload '-' relative to coord class.
+coord coord::operator-(coord ob2) {
+    coord temp;
+    std::cout << " Using coord operator -() \n";
+    temp.x = x - ob2.x;
+    temp.y = y - ob2.y;
+    return temp;
+}
+
+// Overload '=' relative to coord class.
+coord coord::operator=(coord ob2) {
+    std::cout << " Using coord operator =() \n";
+    x = ob2.x;
+    y = ob2.y;
+    return *this;
+    // return the object that is assigned. Noticem, no "temp" is used
+}
+
+// "quad" class inheriting "coord"
+class quad : public coord {
+        int quadrant;
+    public:
+        quad() {
+            x = 0;
+            y = 0;
+            quadrant = 0;
+        }
+        quad(int x, int y) : coord (x, y) {
+            if(x >=0 && y >= 0) 
+                quadrant = 1;
+            else if(x <0 && y >=0) 
+                quadrant = 2;
+            else if(x <0 && y <0) 
+                quadrant = 3;
+            else 
+                quadrant = 4;
+        }
+        void showq() {
+            std::cout << " Point in Quadrant : " << quadrant << '\n';
+        }
+        // quad's own overladed '=' operator
+        quad operator=( coord ob2 );
+};
+
+
+// Overload '=' relative to 'quad' class again
+quad quad :: operator=( coord ob2 ) {
+    std::cout << " Using quad operator =() \n";
+
+    x = ob2.x;
+    y = ob2.y;
+
+    if(x >=0 && y >= 0)
+        quadrant = 1;
+    else if(x <0 && y >=0)
+        quadrant = 2;
+    else if(x <0 && y <0)
+        quadrant = 3;
+    else
+        quadrant = 4;
+
+    return * this;
+}
+
+
+int main() {
+    quad o1(10, 10), o2(15, 3), o3;
+    int x, y;
+
+    o3 = o1 + o2; // add two objects - this calls operator +()
+    o3.get_xy(x, y);
+    o3.showq();
+    std::cout << "(o1+o2) X: " << x << ", Y: " << y << "\n";
+
+    o3 = o1 - o2; // subtract two objects
+    o3.get_xy(x, y);
+    o3.showq();
+    std::cout << "(o1 -o2) X: " << x << ", Y: " << y << "\n";
+
+    o3 = o1; // assign an object
+    o3.get_xy(x, y);
+    o3.showq();
+    std::cout << "(o3=o1) X: " << x << ", Y: " << y << "\n";
+
+    return 0;
+}
 
 
 
