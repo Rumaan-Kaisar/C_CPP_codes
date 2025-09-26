abfdc11186de153bd86b3a40e76256988ec61387
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-09-26 08:49:46 +0600

    Update ch12_05_inserter_extractor.cpp

--- CPP_ch_12_Cpp_conio_fileio/ch12_05_inserter_extractor.cpp
+++ CPP_ch_12_Cpp_conio_fileio/ch12_05_inserter_extractor.cpp
@@ -7,7 +7,7 @@
 	Insertion and inserter: In C++, the output operation is called an insertion and the << is called the insertion operator. (The reason for this term: an output operator inserts information into a stream).
 	Overloading the << for output, creates an inserter function, or inserter for short.  All inserted functions have this general form:
 
-ostream &operator<<(ostream &stream, class_name obj) {  /* body of inserter */ 
+ostream &operator<<(ostream &stream, class_name obj) {  // body of inserter 
     return stream;	}
 	The inserted function "ostream &operator<<" returns a reference to stream, which is of type ostream. This is required if the overloaded << is going to be used in a series of I/O expressions, such as cout<< ob1 << ob2 << ob3 ;
 	The first parameter is a reference to an object of type ostream. This means that stream must be an output stream. (ostream is derived form the ios class.) 
@@ -19,7 +19,7 @@ ostream &operator<<(ostream &stream, class_name obj) {  /* body of inserter */
 	Extraction and extractor:  In C++, the >> is referred to as the extraction operator. The reason for this term is that the act of inputting information from a stream removes (that is, extracts) data from it.
 	A function that overloads >> is called an extractor.  The general form of an extractor function is:
 
-istream &operator>>(istream &stream, class_name &ob) { /* body of extractor */ 
+istream &operator>>(istream &stream, class_name &ob) { // body of extractor
     return stream; 	}
 	Extractors return a reference to stream " istream &operator>>", which is of type istream an input stream. 
 	The first parameter must be a reference to an input stream. 
@@ -32,8 +32,8 @@ class coord {	int x, y;
 	public:
 		coord() { x = 0; y = 0; }
 		coord(int i, int j) { x = i; y = j; }
-		friend ostream & operator <<( ostream &stream , coord ob); 		/* inserter */
-		friend istream & operator >>( istream &stream , coord &ob); 		/* extractor */
+		friend ostream & operator <<( ostream &stream , coord ob); 		// inserter 
+		friend istream & operator >>( istream &stream , coord &ob); 		// extractor
 		};
 ostream &operator<<(ostream &stream, coord ob){
 		stream << ob.x << ", " << ob.y << '\n';
@@ -50,7 +50,7 @@ istream &operator>>(istream &stream, coord &ob){
 	However the following is not for general streaming. In this case, the output statement is hard-coded to display information on the standard output device linked to cout. This prevents the inserter from being used by other streams. 
 
 ostream &operator<<(ostream &stream, coord ob){
-cout << ob.x << ", " << ob.y << '\n'; 	/* using "cout" instead of  "stream" */
+cout << ob.x << ", " << ob.y << '\n'; 	// using "cout" instead of  "stream"
 return stream ;}
 	Non-Friend inserter/extractor: If inserter/extractor are not friends to any class, they cannot use the private members of any class. However all public members are accessible.
 
@@ -60,7 +60,7 @@ return stream ;}
 */  
 
 
-*/  
+
 
 /* 
 
