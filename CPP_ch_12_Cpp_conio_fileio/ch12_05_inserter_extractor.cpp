48b6f7d969cc6c3af95aec82611fef1db39e1b82
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-10-10 15:30:22 +0600

    Update ch12_05_inserter_extractor.cpp

--- CPP_ch_12_Cpp_conio_fileio/ch12_05_inserter_extractor.cpp
+++ CPP_ch_12_Cpp_conio_fileio/ch12_05_inserter_extractor.cpp
@@ -376,7 +376,10 @@ int main() {
                     strcpy(p, ptr);
                 }
 
-                // Create operator << inserter function here .
+
+                // ----  Create operator << inserter function here.  ----
+
+
                 int main() {
                     strtype s1(" This is a test ."), s2("I like C++. ");
                     std::cout << s1 << '\n' << s2;
@@ -384,46 +387,51 @@ int main() {
                 }
 */
 
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
 
-
-# include <iostream >
-# include <cstring >
-# include <cstdlib >
-using namespace std ;
-class strtype
-{
-char *p;
-int len ;
-public :
-strtype ( char * ptr );
-~ strtype () { delete [] p; }
-friend ostream & operator <<( ostream &stream , strtype &ob)
-;
+class strtype {
+        char *p;
+        int len ;
+    public:
+        strtype( char *ptr );
+        ~strtype() { delete [] p; }
+        friend std::ostream &operator<<( std::ostream &stream, strtype &ob);
 };
-strtype :: strtype ( char *ptr )
-{
-len = strlen ( ptr ) +1;
-p = new char ( len );
-if (!p)
-{
-cout << " Allocation error \n";
-exit (1) ;
-513TEACH YOURSELF
-C++
+
+strtype :: strtype( char *ptr ) {
+    len = strlen(ptr) +1;
+    p = new char(len);
+    if(!p) {
+        std::cout << " Allocation error \n";
+        exit(1);
+    }
+    strcpy(p, ptr);
 }
-strcpy (p, ptr );
+
+// operator << inserter function here.
+
+
+int main() {
+    strtype s1(" This is a test ."), s2("I like C++. ");
+    std::cout << s1 << '\n' << s2 << '\n' << std::endl;
+    return 0;
 }
-ostream & operator <<( ostream &stream , strtype &ob)
+
+
+
+
+
+
+
+
+std::ostream &operator<<( std::ostream &stream , strtype &ob)
 {
 stream << ob.p;
 return stream ;
 }
-int main ()
-{
-strtype s1(" This is a test ."), s2("I like C++. ");
-cout << s1 << ’\n’ << s2 << ’\n’;
-return 0;
-}
+
 
 
 
