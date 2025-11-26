689d86306b1250743a40210648e099364ff95c77
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-11-26 16:02:34 +0600

    Update null.cpp

--- CPP_ch_12_Cpp_conio_fileio/null.cpp
+++ CPP_ch_12_Cpp_conio_fileio/null.cpp
@@ -16,71 +16,12 @@
 
 
 
-// rev[20-Nov-2025]
-
-
-
 
 // -=-=-=-=-=-=-=-    Review Skills Check    -=-=-=-=-=-=-=-
 
-Before proceeding, you should be able to correctly answer the following questions and do the
-exercises.
 
+// rev[26-Nov-2025]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// rev[21-Nov-2025]
-
-
-
-
-/* Example 5: Given the following class, create an INSERTER and an EXTRACTOR for it.
-                class date {
-                        char d[9]; // store date as string : mm/dd/yy
-                    public :
-                        // add inserter and extractor
-                };
-*/
-
-#include <iostream>
-
-class date {
-        char d[9]; // store date as string : mm/dd/yy
-    public :
-        friend std::ostream &operator<<( std::ostream &stream , date ob);   // inserter
-        friend std::istream &operator>>( std::istream &stream , date &ob);  // extractor
-};
-
-// inserter
-std::ostream &operator<<(std::ostream &stream, date ob) {
-    stream << ob.d << '\n';
-    return stream;
-}
-
-// extractor
-std::istream &operator>>(std::istream &stream, date &ob) {
-    std::cout << " Enter date (mm/dd/yy): ";
-    stream >> ob.d;
-    return stream;
-}
-
-int main() {
-    date ob;
-    std::cin >> ob;
-    std::cout << ob;
-    return 0;
-}
 
 
 
