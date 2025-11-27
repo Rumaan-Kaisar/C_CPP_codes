eabaaffb0e8be5911bc9a257b7cc2f4b6d9f9a8e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-11-27 09:23:08 +0600

    Update null.cpp

--- CPP_ch_12_Cpp_conio_fileio/null.cpp
+++ CPP_ch_12_Cpp_conio_fileio/null.cpp
@@ -1,5 +1,5 @@
 
-// ----  rev[31-Oct-2025]  ----
+// ----  rev[27-Nov-2025]  ----
 
 // -=-=-=-=-=-=-    Mastery Skills Check    -=-=-=-=-=-=-
 
@@ -11,36 +11,8 @@
 
 
 
-// -=-=-=-=-=-=-    Mastery Skills Check    -=-=-=-=-=-=-
 
 
-
-
-
-// -=-=-=-=-=-=-=-    Review Skills Check    -=-=-=-=-=-=-=-
-
-
-// rev[26-Nov-2025]
-
-
-
-
-
-/* Example 6: What header must be included if your program is to use "I/O manipulators" that take parameters? 
-
-            ans:
-                To use a parameterized manipulator, we must include <iomanip> in our program.
-*/
-
-
-
-
-
-/* Example 7: What predefined streams are created when a C++ program begins execution? 
-
-            ans:
-                The predefined streams are cin, cout, cerr, and clog.
-*/
 
 
 
