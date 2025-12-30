0c0105e46290abea3186984edb702b90a9ac1abb
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2025-12-30 10:18:18 +0600

    Update ch12_07_file_io_basics.cpp

--- CPP_ch_12_Cpp_conio_fileio/ch12_07_file_io_basics.cpp
+++ CPP_ch_12_Cpp_conio_fileio/ch12_07_file_io_basics.cpp
@@ -562,18 +562,21 @@ int main() {
 #include <cctype>
 
 int main(int argc, char *argv[]) {
-if(argc !=2) {
-std::cout << " Usage : COUNT <input >\n";
-return 1;
-}
-ifstream in( argv [1]) ;
-if (! in)
-{
-cout << " Cannot open input file .\n";
-return 1;
-}
+    if(argc !=2) {
+        std::cout << " Usage : COUNT <input >\n";
+        return 1;
+    }
+
+    std::ifstream in(argv [1]);
+
+    if(!in) {
+        std::cout << " Cannot open input file .\n";
+        return 1;
+    }
+
 int count = 0;
 char ch;
+
 in >> ch; // find first non - space char
 // after first non - space found , do not skip spaces
 in. unsetf ( ios :: skipws ); // do not skip spaces
