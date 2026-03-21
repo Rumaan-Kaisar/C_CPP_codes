e4fd2bd9b0635dd9ee09b30bdd0092d93ab3f159
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-03-21 20:10:29 +0600

    Update ch12_11_custom_io_files.cpp

--- CPP_ch_12_Cpp_conio_fileio/ch12_11_custom_io_files.cpp
+++ CPP_ch_12_Cpp_conio_fileio/ch12_11_custom_io_files.cpp
@@ -222,3 +222,94 @@ File Input: The ifstream object in uses the overloaded >> operator to load data 
 Screen Output: The standard cout stream uses the same overloaded << operator to display o3 and o4, proving the function's generality.
 
 */  
+
+
+1. In the following program, the coord class overloads the << and >> operators. Notice
+that you can use the operator functions to write both to the screen and to a file.
+# include <iostream >
+# include <fstream >
+using namespace std ;
+class coord
+{
+int x, y;
+public :
+coord ( int i, int j) { x = i; y = j; }
+friend ostream & operator <<( ostream &stream , coord ob);
+friend istream & operator >>( istream &stream , coord &ob);
+};
+ostream & operator <<( ostream &stream , coord ob)
+{
+stream << ob.x << ’ ’ << ob.y << ’\n’;
+return stream ;
+}
+istream & operator >>( istream &stream , coord &ob)
+{
+stream >> ob.x >> ob.y;
+return stream ;
+}
+int main ()
+{
+coord o1 (1, 2) , o2 (3, 4);
+ofstream out (" test ");
+if (! out )
+{
+cout << " Cannot open output file .\n";
+256ADVANCED C++ I/O
+9.7. CUSTOMIZED I/O AND FILES
+return 1;
+}
+out << o1 << o2;
+out . close ();
+ifstream in(" test ");
+if (! in)
+{
+cout << " Cannot open input file .\n";
+return 1;
+}
+coord o3 (0, 0) , o4 (0, 0);
+in >> o3 >> o4;
+cout << o3 << o4;
+in. close ();
+return 0;
+}
+
+
+2. All of the I/O manipulators can be used with files. For example, in this reworked version
+of a program presented earlier in this chapter, the same manipulator that writes to the
+screen will also write to a file:
+# include <iostream >
+# include <fstream >
+# include <iomanip >
+using namespace std ;
+// Attention :
+ostream & atn ( ostream & stream )
+{
+stream << " Attention : ";
+return stream ;
+}
+// Please note :
+ostream & note ( ostream & stream )
+{
+stream << " Please Note : ";
+return stream ;
+}
+int main ()
+{
+ofstream out (" test ");
+257TEACH YOURSELF
+C++
+if (! out )
+{
+cout << " Cannot open output file .\n";
+return 1;
+}
+// write to screen
+cout << atn << " High voltage circuit \n";
+cout << note << " Turn off all lights \n";
+// write to file
+out << atn << " High voltage circuit \n";
+out << note << " Turn off all lights \n";
+out . close ();
+return 0;
+}
+
