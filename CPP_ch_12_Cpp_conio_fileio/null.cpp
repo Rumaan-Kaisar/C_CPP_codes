35e7a342e029938839a1c308a58b86729f8f5f5d
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-04-01 14:29:07 +0600

    Update null.cpp

--- CPP_ch_12_Cpp_conio_fileio/null.cpp
+++ CPP_ch_12_Cpp_conio_fileio/null.cpp
@@ -531,51 +531,49 @@ int main() {
 
 
 
+/* Example 2: Following program copies a text file. During the copy process, 
+                it convert all "TABS" into the correct number of "SPACES".
+*/
 
-/* Example 2: Write a program that copies a text file. During the copy process, convert all tabs into the
-correct number of spaces. */
+#include <iostream>
+#include <fstream>
 
+int main(int argc, char *argv[]) {
+    if(argc!=3) {
+        std::cout << " Usage : CPY <int > <out >\n";
+        return 1;
+    }
 
-2. // Copy and convert tabs to spaces .
-# include <iostream >
-# include <fstream >
-using namespace std ;
-int main ( int argc , char * argv [])
-{
-if( argc !=3)
-{
-cout << " Usage : CPY <int > <out >\n";
-return 1;
-}
-ifstream in( argv [1]) ;
-if (! in)
-{
-cout << " Cannot open input file .\n";
-return 1;
-}
-ofstream out ( argv [2]) ;
-if (! out )
-{
-cout << " Cannot open output file .\n";
-return 1;
-}
-char ch;
-int i = 8;
-while (! in. eof ())
-{
-in. get (ch);
-if(ch == ’\t’)
-for ( ; i >0; i --)
-out . put (’ ’);
-else
-out . put (ch);
-if(i == -1 || ch == ’\n’)
-i = 8;
-i --;
-}
-in. close ();
-out . close ();
-return 0;
+    std::ifstream in(argv[1]);
+
+    if(!in) {
+        std::cout << " Cannot open input file .\n";
+        return 1;
+    }
+
+    std::ofstream out(argv[2]);
+
+    if(!out) {
+        std::cout << " Cannot open output file .\n";
+        return 1;
+    }
+
+    char ch;
+    int i = 8;  // 1 tab = 8 spaces
+
+    while(!in.eof()) {
+        in.get(ch);
+        if(ch == '\t') for( ; i>0; i--) out.put(' ');
+        else out.put(ch);
+
+        if(i == -1 || ch == '\n') i = 8;
+        i--;
+    }
+
+    in.close();
+    out.close();
+
+    return 0;
 }
 
 
