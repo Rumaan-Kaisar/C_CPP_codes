7f7fc2dd4b9a2efdbd143f830f0eae0a0e0a36b1
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-04-06 18:50:56 +0600

    Update ch12_10_io_status_check.cpp

--- CPP_ch_12_Cpp_conio_fileio/ch12_10_io_status_check.cpp
+++ CPP_ch_12_Cpp_conio_fileio/ch12_10_io_status_check.cpp
@@ -332,3 +332,55 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+
+
+
+/* Example 6: Following program reads a text file and then 
+                counts how many times each letter appears in that file.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+int alpha[26];
+
+int main(int argc, char *argv[]) {
+    char ch;
+
+    if(argc!=2) {
+        std::cout << " Usage : COUNT <source>\n";
+        return 1;
+    }
+
+    std::ifstream in(argv[1]);
+
+    if(!in) {
+        std::cout << " Cannot open input file.\n";
+        return 1;
+    }
+
+    // init alpha []
+    int i;
+
+    for(i=0; i<26; i++) alpha[i] = 0;
+
+    while(!in.eof()) {
+        ch = in.get();
+
+        // if letter found , count it
+        if(isalpha(ch)) {
+            ch = toupper (ch); // normalize
+            alpha [ch -'A']++; // 'A'-'A' == 0, 'B'-'A' == 1, etc.
+        }
+    }
+
+    // display count
+    for (i =0; i <26; i ++) std::cout << (char)('A'+i) << ": " << alpha[i] << '\n';
+    
+    in.close();
+
+    return 0;
+}
+
+
