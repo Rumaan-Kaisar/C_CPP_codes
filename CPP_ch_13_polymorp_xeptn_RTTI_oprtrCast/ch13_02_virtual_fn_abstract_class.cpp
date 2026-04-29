e9e19a84b1568c18d310543b2570b8dff4f9b65c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-04-29 08:29:20 +0600

    Update ch13_02_virtual_fn_abstract_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
@@ -381,7 +381,7 @@ int main(){
 
 
 
-// ----  rev [28-Apr-2026]  ----
+// ----  rev [29-Apr-2026]  ----
 
 
 
@@ -395,57 +395,45 @@ int main(){
                 Derived classes override it to provide the actual implementation.
 */
 
-
-// Use virtual function to define interface .
-
 #include <iostream>
 
-class area
-{
-double dim1 , dim2 ; // dimensions of figure
-public :
-void setarea ( double d1 , double d2)
-{
-dim1 = d1;
-dim2 = d2;
-}
-void getdim ( double &d1 , double &d2)
-{
-d1 = dim1 ;
-d2 = dim2 ;
-}
-virtual double getarea ()
-{
-std::cout << " You must override this function \n";
-return 0.0;
-}
+class area {
+        double dim1 , dim2 ; // dimensions of figure
+    public:
+        void setarea(double d1, double d2) {
+            dim1 = d1;
+            dim2 = d2;
+        }
+        void getdim(double &d1, double &d2) {
+            d1 = dim1;
+            d2 = dim2;
+        }
+        // virtual function that define interface
+        virtual double getarea() {
+            std::cout << " You must override this function \n";
+            return 0.0;
+        }
 };
 
 
-class rectangle : public area
-{
-public :
-double getarea ()
-{
-double d1 , d2;
-getdim (d1 , d2);
-return d1 * d2;
-}
+class rectangle : public area {
+    public:
+        double getarea() {
+            double d1, d2;
+            getdim(d1 , d2);
+            return d1*d2;
+        }
 };
 
 
-
-class triangle : public area
-{
-public :
-double getarea ()
-{
-double d1 , d2;
-getdim (d1 , d2);
-return 0.5 * d1 * d2;
-}
+class triangle : public area {
+    public:
+        double getarea() {
+            double d1, d2;
+            getdim(d1 , d2);
+            return 0.5*d1*d2;
+        }
 };
-
 
 
 int main() {
@@ -456,14 +444,16 @@ int main() {
     r.setarea(3.3, 4.5);
     t.setarea(4.0, 5.0);
 
-p = &r;
-std::cout << " Rectangle has area : " << p-> getarea () << '\n';
-p = &t;
+    p = &r;
+    std::cout << "Rectangle has area : " << p-> getarea () << '\n';
 
-std::cout << " Triangle has area : " << p-> getarea () << '\n';
+    p = &t;
+    std::cout << "Triangle has area : " << p-> getarea () << '\n';
 
-return 0;
+    return 0;
 }
+
+
 
 /* Notice that the definition of getarea() inside area is just a placeholder and performs
 no real function. Because area is not linked to any specific type of figure, there is no
