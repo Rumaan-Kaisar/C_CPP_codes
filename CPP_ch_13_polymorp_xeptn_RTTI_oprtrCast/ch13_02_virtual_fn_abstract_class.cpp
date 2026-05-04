6733abb11ec67ee5080cb80eae92a01b252a2bfb
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-05-04 17:44:58 +0300

    Update ch13_02_virtual_fn_abstract_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
@@ -439,10 +439,10 @@ int main() {
     t.setarea(4.0, 5.0);
 
     p = &r;
-    std::cout << "Rectangle has area : " << p-> getarea () << '\n';
+    std::cout << "Rectangle has area : " << p-> getarea() << '\n';
 
     p = &t;
-    std::cout << "Triangle has area : " << p-> getarea () << '\n';
+    std::cout << "Triangle has area : " << p-> getarea() << '\n';
 
     return 0;
 }
@@ -665,6 +665,8 @@ class area {
         }
         // pure virtual function
         virtual double getarea() = 0
+        // Now this getarea() is pure, it ensures that each derived class will override it.
+
 };
 
 
@@ -697,10 +699,10 @@ int main() {
     t.setarea(4.0, 5.0);
 
     p = &r;
-    std::cout << "Rectangle has area : " << p-> getarea () << '\n';
+    std::cout << "Rectangle has area : " << p-> getarea() << '\n';
 
     p = &t;
-    std::cout << "Triangle has area : " << p-> getarea () << '\n';
+    std::cout << "Triangle has area : " << p-> getarea() << '\n';
 
     return 0;
 }
@@ -887,63 +889,11 @@ Let me know if you'd like this converted into flashcards, a diagram, or a conden
 
 // ABS:
 
-1. Here is an improved version of the program shown in Example 4 in the predeing section.
-In this version, the function getarea() is declared as pure in the base class area.
-// Create an abstract class
-# include <iostream >
-using namespace std ;
-class area
-{
-double dim1 , dim2 ; // dimensions of figure
-public :
-void setarea ( double d1 , double d2)
-{
-dim1 = d1;
-dim2 = d2;
-}
-void getdim ( double &d1 , double &d2)
-{
-d1 = dim1 ;
-271TEACH YOURSELF
-C++
-d2 = dim2 ;
-}
-virtual double getarea () = 0; // pure virtual function
-};
-class rectangle : public area
-{
-public :
-double getarea ()
-{
-double d1 , d2;
-getdim (d1 , d2);
-return d1 * d2;
-}
-};
-class triangle : public area
-{
-public :
-double getarea ()
-{
-double d1 , d2;
-getdim (d1 , d2);
-return 0.5 * d1 * d2;
-}
-};
-int main ()
-{
-area *p;
-rectangle r;
-triangle t;
-r. setarea (3.3 , 4.5) ;
-t. setarea (4.0 , 5.0) ;
-p = &r;
-cout << " Rectangle has area : " << p-> getarea () << ’\n’;
-p = &t;
-cout << " Triangle has area : " << p-> getarea () << ’\n’;
-return 0;
-}
-Now that getarea() is pure, it ensures that each derived class will override it.
+
+
+
+
+
 2. The following program illustrates how a function’s virtual nature is preserved when it is
 inherited:
 // Virtual functions retain their virtual nature when
