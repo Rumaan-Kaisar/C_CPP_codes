8b93a8398516d0989d3afbfd25c295db7648b187
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-05-04 19:58:58 +0600

    Update ch13_02_virtual_fn_abstract_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_02_virtual_fn_abstract_class.cpp
@@ -635,8 +635,115 @@ int main() {
 
 
 
-// ----  rev[03-May-2026]  ----
+// ----  rev[04-May-2026]  ----
 
+
+
+
+
+
+/* Example 7: This program creates a base called "area" that holds two dimensions of a figure.
+
+                It also declares a VF called getarea() that, when overridden by derived classes, 
+                    "returns the area" of the type of figure defined by the derived. 
+
+                Notice the PVF
+                        class area{ virtual double getarea() = 0; };
+                
+                the two classes "rectangle" and "triangle" inherit the base "area" and 
+                    must implement the PVF getarea().
+*/
+
+
+
+
+#include <iostream>
+
+class area {
+        double dim1 , dim2 ; // dimensions of figure
+    public:
+        void setarea(double d1, double d2) {
+            dim1 = d1;
+            dim2 = d2;
+        }
+        void getdim(double &d1, double &d2) {
+            d1 = dim1;
+            d2 = dim2;
+        }
+        // virtual function that define interface
+        virtual double getarea() {
+            std::cout << " You must override this function \n";
+            return 0.0;
+        }
+};
+
+
+class rectangle : public area {
+    public:
+        double getarea() {
+            double d1, d2;
+            getdim(d1 , d2);
+            return d1*d2;
+        }
+};
+
+
+class triangle : public area {
+    public:
+        double getarea() {
+            double d1, d2;
+            getdim(d1 , d2);
+            return 0.5*d1*d2;
+        }
+};
+
+
+int main() {
+    area *p;
+    rectangle r;
+    triangle t;
+
+    r.setarea(3.3, 4.5);
+    t.setarea(4.0, 5.0);
+
+    p = &r;
+    std::cout << "Rectangle has area : " << p-> getarea () << '\n';
+
+    p = &t;
+    std::cout << "Triangle has area : " << p-> getarea () << '\n';
+
+    return 0;
+}
+
+
+
+ 
+
+    class area{     double dim1 , dim2 ;    // dimensions of figure
+        public :    void setarea(double d1, double d2){ dim1 = d1; dim2 = d2;}
+        void getdim(double &d1 , double &d2) { d1 = dim1 ; d2 = dim2 ; }
+            virtual double getarea(){   cout << "You must override this function \n";
+                            return 0.0; }
+        };
+class rectangle : public area {
+    public :
+        double getarea(){double d1, d2;
+                    getdim(d1, d2);
+                    return d1*d2;  }
+                };  class triangle : public area {
+    public :
+        double getarea() { double d1, d2;
+                     getdim(d1, d2);
+                  return 0.5*d1*d2;}
+                };
+int main(){     area *p;
+        rectangle r;
+        triangle t;
+            r.setarea(3.3, 4.5) ;
+            t.setarea(4.0, 5.0) ;
+            p = &r; cout << " Rectangle has area : " << p-> getarea() << '\n';
+            p = &t; cout << " Triangle has area : " << p-> getarea() << '\n';
+        return 0; }
 
 
 
