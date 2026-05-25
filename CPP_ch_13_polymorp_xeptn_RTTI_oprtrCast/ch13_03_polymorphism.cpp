02052146e954af778179c1a9eb5e3f421fb41d0a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-05-25 14:58:16 +0600

    Update ch13_03_polymorphism.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_03_polymorphism.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_03_polymorphism.cpp
@@ -151,27 +151,6 @@
 
 
 
-/*  
-
-Difference between Stack and Queue Data Structures :
-
-Stack: A stack is a linear data structure in which elements can be inserted and deleted only from one side of the list, called the top. 
-   A stack follows the LIFO (Last In First Out) principle, i.e., the element inserted at the last is the first element to come out. 
-   The insertion of an element into stack is called push operation, and deletion of an element from the stack is called pop operation. 
-   In stack we always keep track of the last element present in the list with a pointer called top.    Queue: A queue is a linear data structure in which elements can be inserted only from one side of the list called rear, and the elements can be deleted only from the other side called the front.
-
-   The queue follows the FIFO (First In First Out) principle, i.e. the element inserted at first in the list, is the first element to be removed from the list. 
-   The insertion of an element in a queue is called an enqueue operation and the deletion of an element is called a dequeue operation. 
-   In queue we always maintain two pointers, one pointing to the element which was inserted at the first and still present in the list with the front pointer and the second pointer pointing to the element inserted at the last with the rear pointer.
-
-
-
-
-*/
-
-
-
-
 /* Example 2: This program shows the idea of “one interface, multiple methods.” 
                 It creates an abstract "list" class for storing integers.
 
@@ -539,20 +518,36 @@ int main(){
 
 
 
+/*  ----------------    Stack and Queue Data Structures    ----------------
+    Stack: 
+        A stack is a linear data structure in which 
+        elements can be inserted and deleted only from one side of the list, called the top. 
 
+        A stack follows the LIFO (Last In First Out) principle, 
+            i.e., the element inserted at the last is the first element to come out. 
 
+        The insertion of an element into stack is called "push" operation, and 
+            deletion of an element from the stack is called "pop" operation. 
 
+        In stack we always "keep track of the last element" present in the list with a pointer called top.
+        
 
+    Queue:
+        A queue is a linear data structure in which 
+            elements can be inserted only from one side of the list called "rear", and 
+            the elements can be deleted only from the other side called the "front".
 
+        The queue follows the FIFO (First In First Out) principle, 
+            i.e. the element inserted at first in the list, is the first element to be removed from the list. 
 
+        The "insertion" of an element in a queue is called an "enqueue" operation and 
+            the "deletion" of an element is called a "dequeue" operation. 
 
-
-
-/* Example 1: .
+        In queue we always maintain "two pointers", 
+            one pointing to the element which was inserted at the first 
+                and still present in the list with the front pointer and 
+            the second pointer pointing to the element inserted at the last with the rear pointer.
 */
-
-// Demonstrate virtual functions .
-
 
 
 
