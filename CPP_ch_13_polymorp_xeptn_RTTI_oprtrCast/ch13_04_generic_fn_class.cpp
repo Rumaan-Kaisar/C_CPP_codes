52253b091588df9200ee44fd11dad22f81344bf1
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-05 09:17:46 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -577,5 +577,272 @@ int main(){
 // ----  rev[04-Jun-2026]  ----
 
 
+
+// --------  Generic-Functions  --------
+
+
+
+
+
+Example 1: The following program creates a generic function that swaps the values of the two variables
+it is called with. Because the general process of exchanging two values is independent of
+the type of the variables, this process is a good choice to be made into a generic function.
+
+// Function template example .
+# include <iostream >
+using namespace std ;
+// This is a function template .
+template <class X> void swapargs (X &a, X &b)
+{
+X temp ;
+temp = a;
+a = b;
+b= temp ;
+}
+int main ()
+{
+int i=10 , j =20;
+float x=10 , y =23.3;
+cout << " Original i, j: " << i << ’ ’ << j << endl ;
+cout << " Original x, y: " << x << ’ ’ << y << endl ;
+swapargs (i, j); // swap integers
+swapargs (x, y); // swap floats
+cout << " Swapped i, j: " << i << ’ ’ << j << endl ;
+cout << " Swapped x, y: " << x << ’ ’ << y << endl ;
+return 0;
+}
+The keyword template is used to define a generic function. The line:
+template <class X> void swapargs (X &a, X &b)
+
+tells the compiler two things: that a template is being created and that a generic definition
+is beginning. Here X is a generic type that is used as a placeholder. After the template
+portion, the function swapargs() is declared, using X as the data type of the values that
+will be swapped. In main(), the swapargs() function is called using two different types
+of data: integers and floats. Because swapargs() is a generic function, the compiler
+automatically creates two versions of swapargs()-one that will exchange integer values
+and one that will exchange floating-point values. You should compile and try this program
+now.
+Here are some other terms that are sometimes used when templates are discussed and
+that you might encounter in other C++ literature. First, a generic function (that is, a
+function definition preceded by a template statement) is also called a template function.
+When the compiler creates a specific version of this function, it is said to have created a
+generated function. The act of generating a function is referred to as instantiating it. Put
+differently, a generated function is a specific instance of a template function.
+
+
+
+
+Example 2: The template portion of a generic function definition does not have to be on the same
+line as the function’s name. For example, the following is also a common way to format
+the swapargs() function:
+template <class X>
+void swapargs (X &a, X &b)
+{
+X temp ;
+temp = a;
+a = b;
+b= temp ;
+}
+If you use this form, it is important to understand that no other statements can occur
+between the template statement and the start of the generic function definition. For
+example, the following fragment will not compile:
+// This will not compile .
+template <class X>
+int i; // this is an error
+void swapargs (X &a, X &b)
+{
+X temp ;
+temp = a;
+a = b;
+b= temp ;
+}
+As the comments imply, the template specification must directly precede the rest of the
+function definition.
+
+
+
+
+Example 3: As mentioned, instead of using the keyword class, you can use the keyword typename
+to specify a generic type in a template definition. For example, here is another way to
+declare the swapargs() function.
+// Use typename
+template <typename X> void swapargs (X &a, X &b)
+284TEMPLATES AND EXCEPTION HANDLING
+11.1. GENERIC FUNCTIONS
+{
+X temp ;
+temp = a;
+a = b;
+b= temp ;
+}
+The typename keyword can also be used to specify an unknown type within a template,
+but this use is beyond the scope of this book.
+
+
+
+
+Example 4: You can define more than one generic data type with the template statement, using a
+comma-separated list. For example, this program creates a generic function that has two
+generic types:
+# include <iostream >
+using namespace std ;
+template <class type1 , class type2 >
+void myfunc ( type1 x, type2 y)
+{
+cout << x << ’ ’ << y << endl ;
+}
+int main ()
+{
+myfunc (10 , "hi");
+myfunc (0.23 , 10L);
+return 0;
+}
+In this example, the placeholder types type1 and type2 are replaced by the compiler with
+the data types int and char * and double and long, respectively, when the compiler
+generates the specific instances of myfunc().
+Remember: When you create a generic function, you are, in essence, allowing the
+compiler to generate as many different versions of that function as necessary to handle
+the various ways that your program calls that function.
+
+
+
+
+Example 5: Generic functions are similar to overloaded functions except that they are more restrictive.
+When functions are overloaded, you can have different actions performed within the body
+of each function. But a generic function must perform the same general action for all
+versions. For example, the following overloaded functions cannot be replaced by a generic
+function because they do not do the same thing:
+void outdata ( int i)
+{
+cout << i;
+}
+void outdata ( double d)
+{
+cout << setprecision (10) << setfill (’#’);
+
+cout << d;
+cout << setprecision (6) << setfill (’ ’);
+}
+
+
+
+
+Example 6: Even though a template function overloads itself as needed, you can explicitly overload
+one, too. If you overload a generic function, that overloaded function overrides (or *hides*)
+the generic function relative to that specific version. For example, consider this version of
+Example 1:
+// Overriding a template function .
+# include <iostream >
+using namespace std ;
+// This is a function template .
+template <class X> void swapargs (X &a, X &b)
+{
+X temp ;
+temp = a;
+a = b;
+b= temp ;
+}
+// This overrides the generic version of swapargs ().
+void swapargs ( int a, int b)
+{
+cout << " this is inside swapargs (int ,int )\n";
+}
+int main ()
+{
+int i=10 , j =20;
+float x=10 , y =23.3;
+cout << " Original i, j: " << i << ’ ’ << j << endl ;
+cout << " Original x, y: " << x << ’ ’ << y << endl ;
+swapargs (i, j); // calls overloaded swapargs ()
+swapargs (x, y); // swap floats
+cout << " Swapped i, j: " << i << ’ ’ << j << endl ;
+cout << " Swapped x, y: " << x << ’ ’ << y << endl ;
+return 0;
+}
+As the comments indicate, when swapargs(i,j) is called, it invokes the explicitly overloaded version of swapargs() defined in the program. Thus, the compiler does not
+generate this version of the generic swapargs() function because the generic function is
+overridden by the explicit overloading.
+Manual overloading of a template, as shown in this example, allows you to tailor a version
+of a generic function to accommodate a special situation. However, in general, if you need
+
+to have different versions of a function for different data types, you should use overloaded
+functions rather than templates.
+
+
+
+
+
+
+1. If you have not done so, try each of the preceding examples.
+2. Write a generic function, called min(), that returns the lesser of its two arguments. For
+example, min(3, 4) will return 3 and min(’c’, ’a’ will return a. Demonstrate your
+function in a program.
+3. A good candidate for a template function is called find(). This function searches an array
+for an object. It returns either the index of the matching object (if one is found) or -1 if no
+match is found. Here is the prototype for a specific version of find(). Convert find() into
+a generic function and demonstrate your solution within a program. (The size parameter
+specifies the number of elements in the array.)
+int find ( int object , int *list , int size )
+{
+// ...
+}
+4. In your own words, explain why generic functions are valuable and may help simplify the
+source code to program that you create.
+
+
+2. # include <iostream >
+using namespace std ;
+template <class X> X min (X a, X b)
+{
+if(a <=b)
+return a;
+else
+return b;
+}
+int main ()
+{
+cout << min (12.2 , 2.0) ;
+cout << endl ;
+cout << min (3, 4);
+cout << endl ;
+cout << min (’c’, ’a’);
+return 0;
+}
+/*
+This min (X, X) function might cause ambiguity
+because of STL function min ( const _Tp &, const _Tp &).
+Try a different name .
+*/
+3. # include <iostream >
+# include <cstring >
+using namespace std ;
+template <class X> int find (X object , X *list , int size )
+{
+int i;
+for (i =0; i< size ; i ++)
+if( object == list [i])
+return i;
+return -1;
+}
+int main ()
+{
+int a[] = {1, 2, 3, 4};
+char *c = " this is a test ";
+
+double d[] = {1.1 , 2.2 , 3.3};
+cout << find (3, a, 4);
+cout << endl ;
+cout << find (’a’, c, (int ) strlen (c));
+cout << endl ;
+cout << find (0.0 , d, 3);
+return 0;
+}
+4. Generic functions are valuable because they allow you to define a general algorithm that
+can be applied to various types of data. (That is, specific versions of the algorithm need
+not be explicitly created by you.) Generic functions further help implement the concept
+of "one interface, multiple methods," which is a common theme in C++ programming.
+
+
+
 */  
 
