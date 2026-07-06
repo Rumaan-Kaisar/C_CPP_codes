dd07152fa2127ac1560463668b0f44bc0a527984
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-07 00:45:31 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -586,39 +586,6 @@ int main(){
 
 
 
-
-/* Example 2: The template portion of a generic function definition does not have to be on the same
-line as the function’s name. For example, the following is also a common way to format
-the swapargs() function: */
-template <class X>
-void swapargs (X &a, X &b)
-{
-X temp ;
-temp = a;
-a = b;
-b= temp ;
-}
-
-/* If you use this form, it is important to understand that no other statements can occur
-between the template statement and the start of the generic function definition. For
-example, the following fragment will not compile: */
-
-// This will not compile .
-template <class X>
-int i; // this is an error
-void swapargs (X &a, X &b)
-{
-X temp ;
-temp = a;
-a = b;
-b= temp ;
-}
-/* As the comments imply, the template specification must directly precede the rest of the
-function definition. */
-
-
-
-
 /* Example 3: As mentioned, instead of using the keyword class, you can use the keyword typename
 to specify a generic type in a template definition. For example, here is another way to
 declare the swapargs() function. */
