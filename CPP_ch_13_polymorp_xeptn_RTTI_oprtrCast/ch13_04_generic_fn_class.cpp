82b6956684f210a0583472e732cd786a5b8b0983
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-07 00:35:22 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -582,21 +582,6 @@ int main(){
 
 
 
-
-
-
-int main ()
-{
-int i=10 , j =20;
-float x=10 , y =23.3;
-cout << " Original i, j: " << i << ’ ’ << j << endl ;
-cout << " Original x, y: " << x << ’ ’ << y << endl ;
-swapargs (i, j); // swap integers
-swapargs (x, y); // swap floats
-cout << " Swapped i, j: " << i << ’ ’ << j << endl ;
-cout << " Swapped x, y: " << x << ’ ’ << y << endl ;
-return 0;
-}
 /* 
 The keyword template is used to define a generic function. The line:
 template <class X> void swapargs (X &a, X &b)
