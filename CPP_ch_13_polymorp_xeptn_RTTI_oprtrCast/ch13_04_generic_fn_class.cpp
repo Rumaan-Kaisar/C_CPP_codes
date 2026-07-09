05d86214298fe14eb8aa178d41fcb79ca232d493
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-09 10:42:53 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -604,17 +604,7 @@ When functions are overloaded, you can have different actions performed within t
 of each function. But a generic function must perform the same general action for all
 versions. For example, the following overloaded functions cannot be replaced by a generic
 function because they do not do the same thing: */
-void outdata ( int i)
-{
-cout << i;
-}
-void outdata ( double d)
-{
-cout << setprecision (10) << setfill (’#’);
 
-cout << d;
-cout << setprecision (6) << setfill (’ ’);
-}
 
 
 
