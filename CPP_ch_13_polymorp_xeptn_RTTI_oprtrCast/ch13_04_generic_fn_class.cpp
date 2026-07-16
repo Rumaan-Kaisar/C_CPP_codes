101656a07250b4c19f7338535cb341812acfd4d3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-16 14:24:24 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -706,38 +706,6 @@ int main() {
 
 // --------  Generic-Classes  --------
 
-1. This program creates a very simple generic singly linked list class. It then demonstrates
-the class by creating a linked list that stores characters.
-
-
-
-As you can see, the declaration of a generic class is similar to that of a generic function.
-The actual type of data stored by the list is generic in the class declaration. It is not until
-an object of the list is declared that the actual data type is determined. In this example,
-objects and pointers are created inside main() that specify that the data type of the list
-will be char. Pay special attention to this declaration:
-list <char > start (’a’);
-Notice how the desired data type is passed inside the angle brackets.
-You should enter and execute this program. It builds a linked list that contains the
-characters of the alphabet and then displays them. However, by simply changing the type
-of data specified when list objects are created, you can change the type of data stored by
-the list. For example, you could create another object that stores integers by using this
-declaration:
-list <int > int_start (1) ;
-You can also use list to store data types that you create. For example, if you want to
-store address information, use this structure:
-struct addr
-{
-char name [40];
-char street [40];
-char city [30];
-char state [3];
-char zip [12];
-}
-Then, to use list to generate objects that will store objects of type addr, use a declaration
-like this (assuming that structvar contains a valid addr structure):
-list <addr > obj ( structvar );
-
 
 
 2. Here is another example of a generic class. It is a reworking of the stack class first
