a4b9aeffc016eada6d65b9b40c9f1246a16585ab
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-07-22 15:19:18 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -857,19 +857,19 @@ int main() {
         q2.q(i -1 + 'A');
     }
 
-for (i =1; i <=10; i ++)
-{
-cout << " Dequeue 1: " << q1.deq () << ’\n’;
-cout << " Dequeue 2: " << q2.deq () << ’\n’;
+    for(i =1; i<=10; i++) {
+        std::cout << " Dequeue 1: " << q1.deq() << '\n';
+        std::cout << " Dequeue 2: " << q2.deq() << '\n';
+    }
+
+    return 0;
 }
-return 0;
-}
 
 
 
 
 
-3. Create a generic class, called input, that does the following when its constructor is called:
+/* Example 12: Create a generic class, called input, that does the following when its constructor is called:
 ➤ prompts the user for input,
 ➤ inputs the data entered by the user, and
 ➤ reprompts if the data is not within a predetermined range.
@@ -877,7 +877,8 @@ Objects of type input should be declared like this:
 input ob(" prompt message ", min_value , max_value )
 Here prompt message is the message that prompt for input. The minimum and maximum
 acceptable values are specified by min-value and max-value, respectively. (Note: the type
-of data entered by the user will be the same as the type of min-valueand max-value.)
+of data entered by the user will be the same as the type of min-valueand max-value.) 
+*/
 
 
 #include <iostream>
