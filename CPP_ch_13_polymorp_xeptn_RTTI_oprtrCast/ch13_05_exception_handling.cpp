58fad8c162074297ac8051133667eede40e754ae
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-08-19 08:33:36 +0600

    Update ch13_05_exception_handling.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
@@ -579,25 +579,21 @@ int main(){
 */
 
 void Xhandler(int test){
-
-        try { 
+    try { 
         if(test==0) throw test;     // throw int 
         if(test==1) throw 'a';      // throw char 
         if(test==2) throw 123.23;   // throw double 
     }
-    catch(...){                     // catch all exceptions 
+    catch (int i){                  // catch an int exception
+        std::cout << " Caught" << i << '\n';    
+    }
+    catch(...){                     // catch all other exceptions 
         std::cout << " Caught One !\n";
     }
-    try {
-        if(test ==0) throw test ;      // throw int
-                      if( test ==1) throw 'a';           // throw char
-                      if( test ==2) throw 123.23;  // throw double
-                       }
-            catch (int i){                   // catch an int exception
-                    cout << " Caught" << i << '\n';    }
-            catch (...){                   // all other  exceptions
-                    cout << " Caught One !\n";    }
-            }   
+    
+    
+            
+
 int main(){ cout << " start \n";
         Xhandler(0);
         Xhandler(1);
