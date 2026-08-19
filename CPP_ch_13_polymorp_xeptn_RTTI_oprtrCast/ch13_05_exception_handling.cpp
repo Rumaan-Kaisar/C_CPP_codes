d17afa6b5ebebc7552f3f42e92cb2c6a1f8b44c2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-08-19 08:29:14 +0600

    Update ch13_05_exception_handling.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
@@ -578,7 +578,18 @@ int main(){
                 By catching all exceptions, you prevent an unhandled exception from causing an abnormal program termination.
 */
 
-void Xhandler (int test ) {     try { if( test ==0) throw test ;      // throw int
+void Xhandler(int test){
+
+        try { 
+        if(test==0) throw test;     // throw int 
+        if(test==1) throw 'a';      // throw char 
+        if(test==2) throw 123.23;   // throw double 
+    }
+    catch(...){                     // catch all exceptions 
+        std::cout << " Caught One !\n";
+    }
+    try {
+        if(test ==0) throw test ;      // throw int
                       if( test ==1) throw 'a';           // throw char
                       if( test ==2) throw 123.23;  // throw double
                        }
