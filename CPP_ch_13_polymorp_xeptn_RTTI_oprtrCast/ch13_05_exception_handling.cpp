f0b3b8c09e8521333b7e8c1846e0f6bf06d7f239
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-08-20 11:57:42 +0600

    Update ch13_05_exception_handling.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_05_exception_handling.cpp
@@ -621,18 +621,22 @@ int main(){
                 }
 */
 
-# include <iostream >
-using namespace std ;
-// This function can only throw ints , chars , and doubles .
-void Xhandler ( int test ) throw (int , char , double )
-{
-if( test ==0)
-throw test ; // throw int
-if( test ==1)
-throw ’a’; // throw char
-if( test ==2)
-throw 123.23; // throw double
-}
+#include <iostream>
+
+
+
+// This function can only throw ints, chars, and doubles
+void Xhandler(int test) throw(int , char , double) {
+    try { 
+        if(test==0) throw test;     // throw int 
+        if(test==1) throw 'a';      // throw char 
+        if(test==2) throw 123.23;   // throw double 
+    }
+    // no "catch" is used here
+}    
+
+
+
 int main ()
 
 {
