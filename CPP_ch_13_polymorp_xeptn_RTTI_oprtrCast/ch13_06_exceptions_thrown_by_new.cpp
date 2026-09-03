7b5665468b3d679deb7042fcaf16243a442ab9ed
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-09-03 12:12:10 +0600

    Update ch13_06_exceptions_thrown_by_new.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_06_exceptions_thrown_by_new.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_06_exceptions_thrown_by_new.cpp
@@ -130,13 +130,13 @@ int main() {
 
 /* Example 4:   1. Explain the difference between the behavior of "new" and "new(nothrow)" when an allocation failure occurs.
                 2. Given the following fragment, show two ways to convert it into modern C++-style code.
-p = malloc ( sizeof ( int ));
-if (!p)
-{
-cout << " Allocation error .\n";
-exit (1) ;
-}
- */
+
+                        p = malloc( sizeof(int) );
+                        if(!p){
+                            cout << " Allocation error .\n";
+                            exit(1);
+                        }
+*/
 
 
 
