3a1c5daa870cbd77fc69f0913846e2416c6060b2
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-09-05 08:15:30 +0600

    Update null.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/null.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/null.cpp
@@ -22,31 +22,31 @@ At this point you should be able to perform the following exercises and answer t
 #include <iostream>
 #include <cstring>
 
-// A generic mode - fining function .
-template <class X> X mode (X *data , int size) {
-register int t, w;
-X md , oldmd ;
-int count , oldcount ;
-oldmd = 0;
-oldcount = 0;
-for (t =0; t< size ; t ++)
-{
-md = data [t];
-count = 1;
-for (w = t +1; w< size ; w++)
-{
+// A generic mode - finding function.
+template <class X> X mode(X *data, int size) {
+    register int t, w;
+    X md , oldmd;
+    int count, oldcount;
 
-if(md == data [w])
-count ++;
-if( count > oldcount )
-{
-oldmd = md;
-oldcount = count ;
+    oldmd = 0;
+    oldcount = 0;
+
+    for(t =0; t < size; t++) {
+        md = data [t];
+        count = 1;
+        for(w = t +1; w < size; w++) {
+            if(md == data[w]) count++;
+            if(count > oldcount){
+                oldmd = md;
+                oldcount = count;
+            }
+        }
+    }
+
+    return oldmd;
 }
-}
-}
-return oldmd ;
-}
+
+
 int main ()
 {
 int i[] = {1, 2, 3, 4, 2, 3, 2, 2, 1, 5};
