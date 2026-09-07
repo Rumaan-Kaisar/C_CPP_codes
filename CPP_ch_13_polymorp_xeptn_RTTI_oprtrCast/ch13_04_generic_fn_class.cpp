85940fd6e120ed563dfddd9cb5eecfa486193c8c
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-09-07 10:07:26 +0600

    Update ch13_04_generic_fn_class.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/ch13_04_generic_fn_class.cpp
@@ -953,3 +953,30 @@ int main() {
     return 0;
 }
 
+
+
+
+/* Example 14: Create a generic function that returns the summation of an array of values. */
+
+#include <iostream>
+
+template <class X> X sum (X *data, int size) {
+    int i;
+
+    X result = 0;
+
+    for(i=0; i < size; i++) result += data[i];
+    return result;
+}
+
+
+int main() {
+    int i[] = {1, 2, 3, 4};
+    double d[] = {1.1 , 2.2 , 3.3 , 4.4};
+
+    std::cout << sum(i, 4) << std::endl;
+    std::cout << sum(d, 4) << std::endl;
+
+    return 0;
+}
+
