450620daa9353640cf04f9169523abd794239b6b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2026-09-12 09:39:45 +0600

    Update null.cpp

--- CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/null.cpp
+++ CPP_ch_13_polymorp_xeptn_RTTI_oprtrCast/null.cpp
@@ -65,7 +65,7 @@ int main() {
 
 
 /* Example 4: Rework the stack class so that it can store pairs of different-type objects on the stack.
-                (rework version of Example 10 of 'ch13_04_generic_fn_class.cpp') 
+                (rework version of Example 10 of 'ch13_04_generic_fn_class.cpp', introduced in "ch10_01_1_class_intro.cpp") 
 */
 
 
@@ -167,7 +167,7 @@ return 0;
 
 
 
-/* Example 10: Following is a rewoked version of the "stack" class introduced in "ch10_01_1_class_intro.cpp".  
+/* Example 10: Following is a rewoked version of the "stack" class .  
                 However, in this case, stack has been made into a template class (i.e a generic stack). 
                 Thus, it can be used to store any type of object. 
                 In this example, a character stack and a floating-point stack are created.
